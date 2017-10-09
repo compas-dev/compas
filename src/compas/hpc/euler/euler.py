847ffd220e515f5b27504a8851ac1dde549be649
Andrew Liew <liew@arch.ethz.ch>
2017-10-09 15:07:37 +0200

    - show_euler_jobs(), show_euler_resources(), submit_job(), kill_job() functions to hpc.euler.

--- src/compas/hpc/euler/euler.py
+++ src/compas/hpc/euler/euler.py
@@ -14,9 +14,13 @@ __all__ = [
     'recieve_file_from_euler',
     'send_file_to_euler',
     'send_folder_to_euler',
+    'show_euler_jobs',
     'show_euler_quotas',
     'show_euler_modules',
     'show_euler_module_info',
+    'show_euler_resources',
+    'submit_job',
+    'kill_job',
     'sync_folder_to_euler',
     # 'unload_euler_modules',
 ]
@@ -36,6 +40,19 @@ def connect_to_euler(username):
     """
     client = ssh.connect_to_server(username=username, server=server)
     return client
+
+
+def kill_job(client, job):
+    """ Kill a specific submitted job on Euler.
+
+    Parameters:
+        client (obj): Connected ssh client object to Euler.
+        job (int): Euler job number.
+
+    Returns:
+        None
+    """
+    ssh.server_command(client=client, command='bkill {0}'.format(job))
 
 
 def load_euler_module(client, module):
@@ -142,6 +159,56 @@ def show_euler_module_info(client, module):
     ssh.server_command(client=client, command='module show {0}'.format(module))
 
 
+def show_euler_resources(client):
+    """ Show the available Euler resources for user.
+
+    Parameters:
+        client (obj): Connected ssh client object to Euler.
+
+    Returns:
+        None
+    """
+    ssh.server_command(client=client, command='busers')
+
+
+def show_euler_jobs(client, type='user'):
+    """ Show the jobs in queue on Euler.
+
+    Parameters:
+        client (obj): Connected ssh client object to Euler.
+        type (str): 'all' cluster jobs or those submitted by 'user'.
+
+    Returns:
+        None
+    """
+    if type == 'all':
+        ssh.server_command(client=client, command='bqueues')
+    elif type == 'user':
+        ssh.server_command(client=client, command='bbjobs')
+
+
+def submit_job(client, command, time='60', output='output.txt', cpus=1):
+    """ Submit a job to Euler.
+
+    Note:
+        - If the output file already exists, it will be overwritten.
+        - STRICTLY do not request more than 24 CPUs, make sure the job can use all those requested.
+
+    Parameters:
+        client (obj): Connected ssh client object to Euler.
+        command (str): Command to execute.
+        time (str): Requested amount of CPU time '1:30' (hrs:mins) or '60' (mins).
+        output (str): Name of output summary file.
+        cpus (int): Number of CPUs to request.
+
+    Returns:
+        None
+    """
+    n = min([24, int(cpus)])
+    cmd = 'export OMP_NUM_THREADS={0}; bsub -n {0} -W {1} -oo {2} {3}; bbjobs'.format(n, int(time), output, command)
+    ssh.server_command(client=client, command=cmd)
+
+
 def sync_folder_to_euler(username, local_folder, remote_folder):
     """ Sync (rsync) a local folder to the ETHZ Euler cluster (home folder).
 
@@ -178,9 +245,14 @@ def sync_folder_to_euler(username, local_folder, remote_folder):
 if __name__ == '__main__':
 
     client = connect_to_euler(username='liewa')
-    sync_folder_to_euler(username='liewa', local_folder='/home/al/Dropbox/compas_core/', remote_folder='compas_core/')
+    # sync_folder_to_euler(username='liewa', local_folder='/home/al/Dropbox/compas_core/', remote_folder='compas_core/')
     # show_euler_quotas(username='liewa', client=client)
     # show_euler_modules(client=client)
     # show_euler_module_info(client=client, module='python')
     # load_euler_module(client=client, module='python')
+    show_euler_resources(client=client)
+    show_euler_jobs(client=client, type='all')
+    submit_job(client=client, command='./test', time='5', output='output.txt', cpus=2)
     client.close()
+
+    # -n 24 python script.py
