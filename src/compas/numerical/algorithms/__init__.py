2c07eda2a29fd8a8d4531453b3d713a22fb64701
Tom Van Mele <vanmelet@ethz.ch>
2017-11-20 22:23:14 +0100

    fd in cpp (not tested in rhino)

--- src/compas/numerical/algorithms/__init__.py
+++ src/compas/numerical/algorithms/__init__.py
@@ -2,12 +2,14 @@ from .dr import *
 from .dr_numpy import *
 from .drx_numpy import *
 from .fd_numpy import *
+from .fd_cpp import *
 from .pca_numpy import *
 
 from .dr import __all__ as a
 from .dr_numpy import __all__ as b
 from .drx_numpy import __all__ as c
 from .fd_numpy import __all__ as d
-from .pca_numpy import __all__ as e
+from .fd_cpp import __all__ as e
+from .pca_numpy import __all__ as f
 
-__all__ = a + b + c + d + e
+__all__ = a + b + c + d + e + f
