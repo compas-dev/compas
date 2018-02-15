8238e48ff50d20dd4040a3ebbd5fdf3c3047ed34
Tom Van Mele <vanmelet@ethz.ch>
2018-02-15 18:22:29 +0100

    more elegant solution to the same problem as outlined in issue #44

--- src/compas/topology/duality.py
+++ src/compas/topology/duality.py
@@ -2,7 +2,8 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
-from compas.geometry import circle_from_points_xy
+from math import pi
+
 from compas.geometry import angle_vectors
 from compas.geometry import is_ccw_xy
 
@@ -18,6 +19,9 @@ __all__ = [
     'network_dual',
     'network_find_faces',
 ]
+
+
+PI2 = 2.0 * pi
 
 
 def mesh_dual(mesh, cls=None):
@@ -93,144 +97,6 @@ def mesh_dual(mesh, cls=None):
         dual.add_face(vertices, fkey=fkey)
 
     return dual
-
-
-# def mesh_voronoi(mesh, cls=None, update_coordinates=True):
-#     """Construct the Voronoi dual of the triangulation of a set of points.
-
-#     Parameters
-#     ----------
-#     mesh : Mesh
-#         A mesh object.
-#     cls : Mesh, optional
-#         The type of the dual mesh.
-#         Defaults to the type of the provided mesh object.
-#     update_coordinates : bool, optional
-#         Update the vertex coordinates of the voronoi.
-#         Defaults to true.
-
-#     Note
-#     ----
-#     This function produces a mesh with faces that have the oposite cycle direction
-#     of the original mesh.
-
-#     Example
-#     -------
-#     .. plot::
-#         :include-source:
-
-#         from numpy import random
-#         from numpy import hstack
-#         from numpy import zeros
-
-#         from compas.datastructures import Mesh
-#         from compas.topology import mesh_dual
-#         from compas.topology import mesh_voronoi
-#         from compas.topology import delaunay_from_points
-#         from compas.topology import trimesh_remesh
-
-#         from compas.plotters import MeshPlotter
-
-#         points = hstack((10.0 * random.random_sample((10, 2)), zeros((10, 1)))).tolist()
-#         mesh = Mesh.from_vertices_and_faces(points, delaunay_from_points(points))
-
-#         trimesh_remesh(mesh, 1.0, allow_boundary_split=True)
-
-#         points = [mesh.vertex_coordinates(key) for key in mesh.vertices()]
-#         mesh = Mesh.from_vertices_and_faces(points, delaunay_from_points(points))
-
-#         dual = mesh_voronoi(mesh)
-
-#         lines = []
-#         for u, v in mesh.edges():
-#             lines.append({
-#                 'start': mesh.vertex_coordinates(u, 'xy'),
-#                 'end'  : mesh.vertex_coordinates(v, 'xy'),
-#                 'color': '#cccccc',
-#                 'width': 0.5
-#             })
-
-#         plotter = MeshPlotter(dual, figsize=(10, 7))
-
-#         plotter.draw_lines(lines)
-#         plotter.draw_vertices(facecolor='#eeeeee', edgecolor='#000000', radius=0.05)
-#         plotter.draw_faces(facecolor='#eeeeee', edgecolor='#eeeeee', text='key')
-#         plotter.draw_edges(keys=[(u, v) for u, v in dual.edges() if not dual.is_edge_naked(u, v)])
-
-#         plotter.show()
-
-#     """
-#     if not cls:
-#         cls = type(mesh)
-
-#     fkey_centroid = {fkey: mesh.face_centroid(fkey) for fkey in mesh.faces()}
-#     uv_fkey = {}
-
-#     outer = mesh.vertices_on_boundary(ordered=True)[::-1]
-#     inner = list(set(mesh.vertices()) - set(outer))
-
-#     f = mesh._get_face_key(None)
-
-#     for i in range(-1, len(outer) - 1):
-#         u = outer[i]
-#         v = outer[i + 1]
-#         uv_fkey[(u, v)] = f
-#         fkey_centroid[f] = mesh.edge_midpoint(u, v)
-#         f += 1
-
-#     vertices = {}
-#     faces = {}
-
-#     for key in inner:
-#         fkeys = mesh.vertex_faces(key, ordered=True)
-#         for fkey in fkeys:
-#             if fkey not in vertices:
-#                 vertices[fkey] = fkey_centroid[fkey]
-#         faces[key] = fkeys
-
-#     for key in outer:
-#         nbrs = mesh.vertex_neighbours(key, ordered=True)
-
-#         if len(nbrs) < 2:
-#             continue
-
-#         # the first boundary edge
-#         fkey = uv_fkey[(nbrs[0], key)]
-#         fkeys = [fkey]
-#         if fkey not in vertices:
-#             vertices[fkey] = fkey_centroid[fkey]
-
-#         # all internal edges
-#         for nbr in nbrs[0:-1]:
-#             fkey = mesh.halfedge[nbr][key]
-#             fkeys.append(fkey)
-#             if fkey not in vertices:
-#                 vertices[fkey] = fkey_centroid[fkey]
-
-#         # the last boundary edge
-#         fkey = uv_fkey[(key, nbrs[-1])]
-#         fkeys.append(fkey)
-#         if fkey not in vertices:
-#             vertices[fkey] = fkey_centroid[fkey]
-#         faces[key] = fkeys
-
-#     voronoi = cls()
-
-#     for key, (x, y, z) in vertices.items():
-#         voronoi.add_vertex(key, x=x, y=y, z=z)
-
-#     for fkey, vertices in faces.items():
-#         voronoi.add_face(vertices, fkey=fkey)
-
-#     if update_coordinates:
-#         for key in mesh.faces():
-#             a, b, c = mesh.face_coordinates(key)
-#             center, radius, normal = circle_from_points_xy(a, b, c)
-#             voronoi.vertex[key]['x'] = center[0]
-#             voronoi.vertex[key]['y'] = center[1]
-#             voronoi.vertex[key]['z'] = center[2]
-
-#     return voronoi
 
 
 def network_dual(network, cls=None):
@@ -451,45 +317,22 @@ def network_find_faces(network, breakpoints=None):
     return network.face
 
 
-# def _find_first_neighbour(key, network):
-#     angles = []
-#     nbrs = list(network.halfedge[key].keys())
-#     if len(nbrs) == 1:
-#         return nbrs[0]
-#     # this is a bit fragile
-#     # it assumes quite a few things
-#     vu = [-1, -0.5, 0]
-#     for nbr in nbrs:
-#         w = [network.vertex[nbr][_] for _ in 'xyz']
-#         v = [network.vertex[key][_] for _ in 'xyz']
-#         vw = [w[0] - v[0], w[1] - v[1], 0]
-#         angles.append(angle_vectors(vu, vw))
-#     return nbrs[angles.index(min(angles))]
-
-
 def _find_first_neighbour(key, network):
     nbrs = list(network.halfedge[key].keys())
     if len(nbrs) == 1:
         return nbrs[0]
-    vu = [-1.0, -1.0, 0.0]
+    ab = [-1.0, -1.0, 0.0]
     a = network.vertex_coordinates(key, 'xyz')
-    b = [a[0] + vu[0], a[1] + vu[1], 0]
-    cw = []
+    b = [a[0] + ab[0], a[1] + ab[1], 0]
+    angles = []
     for nbr in nbrs:
         c = network.vertex_coordinates(nbr, 'xyz')
-        if not is_ccw_xy(a, b, c, True):
-            cw.append(nbr)
-    if cw:
-        nbrs = cw
-    angles = []
-    v = [network.vertex[key][_] for _ in 'xyz']
-    for nbr in nbrs:
-        w = [network.vertex[nbr][_] for _ in 'xyz']
-        vw = [w[0] - v[0], w[1] - v[1], 0]
-        angles.append(angle_vectors(vu, vw))
-    if cw:
-        return nbrs[angles.index(min(angles))]
-    return nbrs[angles.index(max(angles))]
+        ac = [c[0] - a[0], c[1] - a[1], 0]
+        alpha = angle_vectors(ab, ac)
+        if is_ccw_xy(a, b, c, True):
+            alpha = PI2 - alpha
+        angles.append(alpha)
+    return nbrs[angles.index(min(angles))]
 
 
 def _sort_neighbours(network, ccw=True):
