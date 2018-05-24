c44b55238b0ed7b4376de9aa86ae838863be352d
Gonzalo Casas <casas@arch.ethz.ch>
2018-05-24 16:59:01 +0200

    Add sample/simple test and pin pytest version to one that is not buggy as hell

---
+++ tests/test_network.py
@@ -0,0 +1,8 @@
+from compas.datastructures import Network
+
+def test_add_vertex():
+    network = Network()
+    assert network.add_vertex() == 0
+    assert network.add_vertex(x=0, y=0, z=0) == 1
+    assert network.add_vertex(key=2) == 2
+    assert network.add_vertex(key=0, x=1) == 0
