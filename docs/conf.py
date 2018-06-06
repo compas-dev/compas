9c859281e6dbc54e84778968f05ad9bf24d24e25
Tom Van Mele <vanmelet@ethz.ch>
2018-06-06 12:50:39 +0200

    removing latex stuff from config

--- docs/conf.py
+++ docs/conf.py
@@ -176,83 +176,82 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ReadtheDocsTemplatedoc'
+htmlhelp_basename = 'COMPASmaindoc'
 
 
-# -- Options for LaTeX output ---------------------------------------------
+# # -- Options for LaTeX output ---------------------------------------------
 
-latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+# latex_elements = {
+# # The paper size ('letterpaper' or 'a4paper').
+# #'papersize': 'letterpaper',
 
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+# # The font size ('10pt', '11pt' or '12pt').
+# #'pointsize': '10pt',
 
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
-}
+# # Additional stuff for the LaTeX preamble.
+# #'preamble': '',
+# }
 
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-  ('index', 'ReadtheDocsTemplate.tex', u'Read the Docs Template Documentation',
-   u'Read the Docs', 'manual'),
-]
+# # Grouping the document tree into LaTeX files. List of tuples
+# # (source start file, target name, title,
+# #  author, documentclass [howto, manual, or own class]).
+# latex_documents = [
+#   ('index', 'COMPASmaindoc.tex', u'COMPAS main library documentation', u'Tom Van Mele', 'manual'),
+# ]
 
-# The name of an image file (relative to this directory) to place at the top of
-# the title page.
-#latex_logo = None
+# # The name of an image file (relative to this directory) to place at the top of
+# # the title page.
+# #latex_logo = None
 
-# For "manual" documents, if this is true, then toplevel headings are parts,
-# not chapters.
-#latex_use_parts = False
+# # For "manual" documents, if this is true, then toplevel headings are parts,
+# # not chapters.
+# #latex_use_parts = False
 
-# If true, show page references after internal links.
-#latex_show_pagerefs = False
+# # If true, show page references after internal links.
+# #latex_show_pagerefs = False
 
-# If true, show URL addresses after external links.
-#latex_show_urls = False
+# # If true, show URL addresses after external links.
+# #latex_show_urls = False
 
-# Documents to append as an appendix to all manuals.
-#latex_appendices = []
+# # Documents to append as an appendix to all manuals.
+# #latex_appendices = []
 
-# If false, no module index is generated.
-#latex_domain_indices = True
-
-
-# -- Options for manual page output ---------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    ('index', 'readthedocstemplate', u'Read the Docs Template Documentation',
-     [u'Read the Docs'], 1)
-]
-
-# If true, show URL addresses after external links.
-#man_show_urls = False
+# # If false, no module index is generated.
+# #latex_domain_indices = True
 
 
-# -- Options for Texinfo output -------------------------------------------
+# # -- Options for manual page output ---------------------------------------
 
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-  ('index', 'ReadtheDocsTemplate', u'Read the Docs Template Documentation',
-   u'Read the Docs', 'ReadtheDocsTemplate', 'One line description of project.',
-   'Miscellaneous'),
-]
+# # One entry per manual page. List of tuples
+# # (source start file, name, description, authors, manual section).
+# man_pages = [
+#     ('index', 'readthedocstemplate', u'Read the Docs Template Documentation',
+#      [u'Read the Docs'], 1)
+# ]
 
-# Documents to append as an appendix to all manuals.
-#texinfo_appendices = []
+# # If true, show URL addresses after external links.
+# #man_show_urls = False
 
-# If false, no module index is generated.
-#texinfo_domain_indices = True
 
-# How to display URL addresses: 'footnote', 'no', or 'inline'.
-#texinfo_show_urls = 'footnote'
+# # -- Options for Texinfo output -------------------------------------------
 
-# If true, do not generate a @detailmenu in the "Top" node's menu.
-#texinfo_no_detailmenu = False
+# # Grouping the document tree into Texinfo files. List of tuples
+# # (source start file, target name, title, author,
+# #  dir menu entry, description, category)
+# texinfo_documents = [
+#   ('index', 'ReadtheDocsTemplate', u'Read the Docs Template Documentation',
+#    u'Read the Docs', 'ReadtheDocsTemplate', 'One line description of project.',
+#    'Miscellaneous'),
+# ]
+
+# # Documents to append as an appendix to all manuals.
+# #texinfo_appendices = []
+
+# # If false, no module index is generated.
+# #texinfo_domain_indices = True
+
+# # How to display URL addresses: 'footnote', 'no', or 'inline'.
+# #texinfo_show_urls = 'footnote'
+
+# # If true, do not generate a @detailmenu in the "Top" node's menu.
+# #texinfo_no_detailmenu = False
