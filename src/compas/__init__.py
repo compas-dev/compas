f6f3fb36a4ea2e1c4830d4cf594239331fa6ebe0
freeeki <freeeki@gmail.com>
2018-07-14 11:17:02 +0200

    updated installer to include data

--- src/compas/__init__.py
+++ src/compas/__init__.py
@@ -130,18 +130,6 @@ def credits():
 
 
 def verify():
-    # import pkg_resources
-    # from pkg_resources import DistributionNotFound
-    # from pkg_resources import VersionConflict
-
-    # # dependencies can be any iterable with strings,
-    # # e.g. file line-by-line iterator
-    # # here, if a dependency is not met, a DistributionNotFound or VersionConflict
-    # # exception is thrown.
-    # try:
-    #     pkg_resources.require(dependencies)
-    # except DistributionNotFound as e:
-    #     print(e)
     requirements = [
         'numpy',
         'scipy',
