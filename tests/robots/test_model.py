d0afa41b4e093396b9d2d02d5ebdcb0a4d40ad4f
Gonzalo Casas <casas@arch.ethz.ch>
2018-07-27 15:10:31 +0200

    Re-arrange test folders

--- tests/robots/test_model.py
+++ tests/robots/test_model.py
@@ -1,8 +1,15 @@
 import os
 
+import pytest
+
 from compas.robots import Robot
 
 BASE_FOLDER = os.path.dirname(__file__)
+
+
+@pytest.fixture
+def urdf_file():
+    return os.path.join(BASE_FOLDER, 'fixtures', 'sample.urdf')
 
 def test_root_urdf_attributes():
     r = Robot.from_urdf_string("""<?xml version="1.0" encoding="UTF-8"?><robot xmlns:xacro="http://www.ros.org/wiki/xacro" name="panda"></robot>""")
@@ -14,14 +21,12 @@ def test_unknown_urdf_attributes():
     assert r.name == 'panda'
     assert r.attr['some_random_attr'] == '1337'
 
-def test_parse_from_file():
-    urdf_file = os.path.join(BASE_FOLDER, 'sample.urdf')
+def test_parse_from_file(urdf_file):
     r = Robot.from_urdf_file(urdf_file)
     assert r is not None
     assert r.name == 'panda'
 
-def test_inertial_values():
-    urdf_file = os.path.join(BASE_FOLDER, 'sample.urdf')
+def test_inertial_values(urdf_file):
     r = Robot.from_urdf_file(urdf_file)
     assert r.links[0].inertial.origin is not None
     assert r.links[0].inertial.mass.value == 1.0
