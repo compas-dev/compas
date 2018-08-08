f77400165ceba86621c37550496468f728fc1ce1
Romana Rust <rust@arch.ethz.ch>
2018-08-08 16:53:19 +0200

    fix #119 moved unload_modules into compas_rhino and keep it imported in compas_ghpython

--- src/compas_ghpython/utilities/misc.py
+++ src/compas_ghpython/utilities/misc.py
@@ -5,21 +5,4 @@ __all__ = [
     'unload_modules',
 ]
 
-def unload_modules(top_level_module_name):
-    """Unloads all modules named starting with the specified string.
-
-    This function eases the development workflow when editing a library that is
-    used from Rhino/Grasshopper.
-
-    Args:
-        top_level_module_name (:obj:`str`): Name of the top-level module to unload.
-
-    Returns:
-        list: List of unloaded module names.
-    """
-    modules = filter(lambda m: m.startswith(top_level_module_name), sys.modules)
-
-    for module in modules:
-        sys.modules.pop(module)
-
-    return modules
+from compas_rhino.utilities.misc import unload_modules
