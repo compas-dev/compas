712145874e237ba4557cf0094d736c928603ba4a
Tom Van Mele <vanmelet@ethz.ch>
2018-08-18 08:55:36 +0200

    no future_builtins in python3

--- src/compas/geometry/basic.py
+++ src/compas/geometry/basic.py
@@ -2,8 +2,6 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
-from future_builtins import zip
-
 from math import sqrt
 from math import fabs
 from random import sample
