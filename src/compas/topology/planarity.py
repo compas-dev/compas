954e9bfe27f261647076b5fb92b81eec97e8b62e
Tom Van Mele <vanmelet@ethz.ch>
2018-08-24 16:59:36 +0200

    removed planarity requirement

--- src/compas/topology/planarity.py
+++ src/compas/topology/planarity.py
@@ -192,33 +192,8 @@ def network_is_planar(network):
 
     Examples
     --------
-
-    .. plot::
-        :include-source:
-
-        import compas
-
-        from compas.datastructures import Network
-        from compas.topology import network_is_planar
-        from compas.topology import network_find_crossings
-        from compas.plotters import NetworkPlotter
-
-        network = Network.from_obj(compas.get('lines.obj'))
-
-        network.add_edge(21, 29)
-        network.add_edge(17, 28)
-
-        if not network_is_planar(network):
-            crossings = network_find_crossings(network)
-        else:
-            crossings = []
-
-        plotter = NetworkPlotter(network)
-
-        plotter.draw_vertices(radius=0.15, text={key: key for key in network.vertices()})
-        plotter.draw_edges(color={edge: '#ff0000' for edges in crossings for edge in edges})
-
-        plotter.show()
+    >>>
+        
 
     """
     try:
