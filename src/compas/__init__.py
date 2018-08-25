1a93883389ed3d6cd749ac5b3c107dc4b27424d6
Tom Van Mele <vanmelet@ethz.ch>
2018-08-25 14:48:36 +0200

    adding the convenience functions to the docs

--- src/compas/__init__.py
+++ src/compas/__init__.py
@@ -21,6 +21,24 @@ compas
 ..
     compas.viewers
 
+
+In addition to the above packages, :mod:`compas` provides the following convenience functions.
+
+.. autosummary::
+    :toctree: generated/
+
+    get
+    get_bunny
+    is_windows
+    is_linux
+    is_mono
+    is_ironpython
+    raise_if_not_ironpython
+    raise_if_ironpython
+    verify
+    installed
+    requirements
+
 """
 
 from __future__ import print_function
@@ -45,7 +63,47 @@ TEMP = os.path.abspath(os.path.join(HERE, '../../temp'))
 
 
 def get(filename):
-    """"""
+    """Get the full path to one of the sample data files.
+
+    Parameters
+    ----------
+    filename : str
+        The name of the data file.
+        The following are available.
+
+        * boxes.obj
+        * faces.obj
+        * fink.obj
+        * hypar.obj
+        * lines.obj
+        * saddle.obj
+
+    Returns
+    -------
+    str
+        The full path to the specified file.
+
+    Notes
+    -----
+    The file name should be specified relative to the **COMPAS** sample data folder.
+    This folder is only locally available if you installed **COMPAS** from source,
+    or if you are working directly with the source.
+    In all other cases, the function will get the corresponding files direcly from
+    the GitHub repo, at https://raw.githubusercontent.com/compas-dev/compas/master/data
+    
+    Examples
+    --------
+    The ``compas.get`` function is meant to be used in combination with the static
+    constructors of the data structures.
+
+    .. code-block:: python
+
+        import compas
+        from compas.datastructures import Mesh
+
+        mesh = Mesh.from_obj(compas.get('faces.obj'))
+
+    """
     filename = filename.strip('/')
     localpath = os.path.abspath(os.path.join(DATA, filename))
     if os.path.exists(localpath):
@@ -54,20 +112,62 @@ def get(filename):
         return "https://raw.githubusercontent.com/compas-dev/compas/master/data/{}".format(filename)
 
 
-def get_bunny():
+def get_bunny(localstorage=None):
+    """Get the *Stanford Bunny* directly from the Stanford repository.
+
+    Parameters
+    ----------
+    localstorage : str, optional
+        Path to a local storage folder for saving the downloaded data.
+        Default is ``None``, in which case the data will be stored in a local
+        user data directory. See https://pypi.org/project/appdirs/ for more info.
+
+    Returns
+    -------
+    str
+        Full path to the local file.
+
+    Examples
+    --------
+    The *Stanford Bunny* is a `PLY` file.
+    Therefore, the returned path should be used in combination with the ``PLY``
+    file reader, or with the ``from_ply`` constructor function for meshes.
+
+    .. code-block:: python
+
+        import compas
+        from compas.datastructures import Mesh
+
+        mesh = Mesh.from_ply(compas.get_bunny())
+
+    """
     import urllib
     import tarfile
-    bunny = os.path.abspath(os.path.join(DATA, 'bunny/reconstruction/bun_zipper.ply'))
+    import appdirs
+
+    # bunny.tar.gz, when extracted contains the folders bunny/reconstruction/bun_zipper.ply
+
+    if not localstorage:
+        localstorage = appdirs.user_data_dir('COMPAS', 'compas-dev', roaming=True)
+
+    bunny = os.path.abspath(os.path.join(localstorage, 'bunny/reconstruction/bun_zipper.ply'))
+    destination = os.path.abspath(os.path.join(localstorage, 'bunny.tar.gz'))
+
     if not os.path.exists(bunny):
         url = 'http://graphics.stanford.edu/pub/3Dscanrep/bunny.tar.gz'
+
         print('Getting the bunny from {} ...'.format(url))
         print('This will take a few seconds...')
-        destination = os.path.abspath(os.path.join(DATA, 'bunny.tar.gz'))
+
         urllib.urlretrieve(url, destination)
+
         with tarfile.open(destination) as file:
-            file.extractall(DATA)
+            file.extractall(localstorage)
+
         os.remove(destination)
+
         print('Got it!\n')
+
     return bunny
 
 
