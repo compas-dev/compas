fb4b9aab78dbcaaa114d8bab558956d02d88768a
Tom Van Mele <vanmelet@ethz.ch>
2018-08-30 12:50:38 +0200

    removed metadata from ui

--- src/compas_rhino/ui/rui.py
+++ src/compas_rhino/ui/rui.py
@@ -1,3 +1,7 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
 import os
 import json
 import inspect
@@ -7,12 +11,6 @@ import compas
 import uuid
 from xml.etree import ElementTree as ET
 from xml.dom import minidom
-
-
-__author__     = ['Tom Van Mele <vanmelet@ethz.ch>']
-__copyright__  = 'Copyright 2014, Block Research Group - ETH Zurich'
-__license__    = 'MIT License'
-__email__      = 'vanmelet@ethz.ch'
 
 
 __all__ = [
