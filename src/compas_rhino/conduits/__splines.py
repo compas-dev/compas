aaffd14d8c71f500248180059fe8bdba0cfb4cd3
Tom Van Mele <vanmelet@ethz.ch>
2018-08-30 11:53:24 +0200

    removed metadata from conduits

--- src/compas_rhino/conduits/__splines.py
+++ src/compas_rhino/conduits/__splines.py
@@ -17,12 +17,6 @@ except ImportError:
         raise
 
 
-__author__     = ['Tom Van Mele <vanmelet@ethz.ch>']
-__copyright__  = 'Copyright 2014, BLOCK Research Group - ETH Zurich'
-__license__    = 'MIT License'
-__email__      = 'vanmelet@ethz.ch'
-
-
 __all__ = ['SplinesConduit']
 
 
