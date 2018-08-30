3848a379b21a4a2e2fecbdb259f34d2bae293099
Tom Van Mele <vanmelet@ethz.ch>
2018-08-30 13:48:08 +0200

    updated attribute mixins docs

--- src/compas/datastructures/_mixins/geometry.py
+++ src/compas/datastructures/_mixins/geometry.py
@@ -1,3 +1,7 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
 from compas.geometry import distance_point_point
 from compas.geometry import midpoint_line
 from compas.geometry import normalize_vector
