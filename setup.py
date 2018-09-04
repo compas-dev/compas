4e438636a206d39461d946f9fa0584172966ba33
Tom Van Mele <vanmelet@ethz.ch>
2018-09-04 15:22:39 +0200

    Bump version to 0.3.2

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ optional_requirements = {
 
 setup(
     name='COMPAS',
-    version='0.3.1',
+    version='0.3.2',
     description='The COMPAS framework',
     long_description=long_description,
     long_description_content_type='text/markdown',
--- src/compas/__init__.py
+++ src/compas/__init__.py
@@ -35,7 +35,7 @@ __author__    = 'Tom Van Mele and many others (see CONTRIBUTORS.md)'
 __copyright__ = 'Copyright 2014-2018 - Block Research Group, ETH Zurich'
 __license__   = 'MIT License'
 __email__     = 'vanmelet@ethz.ch'
-__version__   = '0.3.1'
+__version__   = '0.3.2'
 
 
 HERE = os.path.dirname(__file__)
