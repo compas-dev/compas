4305b390df3ef8df6d4c48c7e7a90ff85917b857
Robin Oval <oval@arch.ethz.ch>
2018-11-02 14:47:28 +0100

    typo

--- src/compas/datastructures/mesh/mesh.py
+++ src/compas/datastructures/mesh/mesh.py
@@ -27,7 +27,7 @@ from compas.geometry import normal_polygon
 from compas.geometry import area_polygon
 
 from compas.geometry.algorithms.bestfit import bestfit_plane
-from compas.geometry.algorithms.palnarity import flatness
+from compas.geometry.algorithms.planarisation import flatness
 
 from compas.geometry import Polyhedron
 
