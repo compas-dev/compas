39da3cfa3434cb7ab609751cbfd81549d1ea4b79
andrewtliew <liew@arch.ethz.ch>
2018-12-04 19:20:52 +0100

    - More drawing.

--- src/compas_blender/_/drawing.py
+++ src/compas_blender/_/drawing.py
@@ -27,44 +27,6 @@
 #    'xdraw_points',
 #    'xdraw_spheres',
 #    'xdraw_texts',
-]
-
-
-# ==============================================================================
-# materials
-# ==============================================================================
-
-#def delete_all_materials():
-#    """ Delete all scene materials.
-
-#    Parameters:
-#        None
-
-#    Returns:
-#        None
-#    """
-#    materials = bpy.data.materials
-#    for material in materials:
-#        material.user_clear()
-#        materials.remove(material)
-
-
-
-
-
-#def draw_cubes(pos=[[0, 0, 0]], radius=1, layer=0, color=[1, 1, 1]):
-#    """ Draw multiple cubes.
-
-#    Parameters:
-#        pos (list): Centroid locations [[x, y, z], ..].
-#        radius (float): Radius of cubes.
-#        layer (int): Layer number.
-#        color (list): Material color.
-
-#    Returns:
-#        list: Created cube objects.
-#    """
-#    return xdraw_cubes([{'pos': i, 'radius': radius, 'layer': layer, 'color': color} for i in pos])
 
 
 #def draw_cuboid(Lx=1, Ly=1, Lz=1, pos=[0, 0, 0], layer=0, color=[1, 1, 1], wire=True):
@@ -141,24 +103,6 @@
 #    return bmesh
 
 
-#def draw_pipes(start=[[0, 0, 0]], end=[[1, 1, 1]], radius=1, layer=0, color=[1, 1, 1]):
-#    """ Draw multiple pipes.
-
-#    Parameters:
-#        start (list): Pipe start points.
-#        end (list): Pipe end points.
-#        radius (float): Radius of pipes.
-#        layer (int): Layer number.
-#        color (list): Material color.
-
-#    Returns:
-#        list: Created pipe objects.
-#    """
-#    return xdraw_pipes([{'start': u, 'end': v, 'radius': radius, 'layer': layer, 'color': color}
-#                        for u, v in zip(start, end)])
-
-
-
 
 #def xdraw_faces(faces, alpha=1):
 #    """ Draw a set of faces.
@@ -193,106 +137,7 @@
 #    return objects
 
 
-def xdraw_lines(lines):
 
-    objects = []
-
-    for line in lines:
-
-        start = line.get('start')
-        end   = line.get('end')
-        name  = line.get('name', 'line')
-        # color
-        # layer
-        # width
-
-        curve = bpy.data.curves.new(name, type='CURVE')
-        curve.dimensions = '3D'
-        object = bpy.data.objects.new(name, curve)
-        object.location = [0, 0, 0]
-
-        spline = curve.splines.new('NURBS')
-        spline.points.add(2)
-        spline.points[0].co = list(start) + [1]
-        spline.points[1].co = list(end) + [1]
-        spline.order_u = 1
-
-#        material = create_material(color=line.get('color', [1, 1, 1]))
-#        object.data.fill_mode = 'FULL'
-#        object.data.bevel_depth = line.get('width', 0.05)
-#        object.data.bevel_resolution = 0
-#        object.data.materials.append(material)
-#        set_object_layer(object=object, layer=line.get('layer', 0))
-
-        objects.append(object)
-
-#    deselect_all_objects()
-
-    return _link_objects(objects)
-
-
-#def xdraw_mesh(name, vertices=[], edges=[], faces=[], layer=0, color=[1, 1, 1], alpha=1, wire=True):
-#    """ Draws a Blender mesh.
-
-#    Parameters:
-#        name (str): Blender mesh name.
-#        vertices (list): Vertices co-ordinates.
-#        edges (list): Edge vertex indices.
-#        faces (list): Face vertex indices.
-#        layer (int): Layer number.
-#        color (list): Material color.
-#        alpha (float): Alpha [0, 1].
-#        wire (bool): Show wires for faces.
-
-#    Returns:
-#        obj: Created Blender mesh object.
-#    """
-#    mesh = bpy.data.meshes.new(name)
-#    mesh.from_pydata(vertices, edges, faces)
-#    mesh.update(calc_edges=True)
-#    bmesh = bpy.data.objects.new(name, mesh)
-#    bpy.context.scene.objects.link(bmesh)
-#    bmesh.show_wire = wire
-#    material = create_material(color=color, alpha=alpha)
-#    bmesh.data.materials.append(material)
-#    set_object_layer(object=bmesh, layer=layer)
-#    bmesh.select = False
-#    return bmesh
-
-
-#def xdraw_pipes(pipes, div=8):
-#    """ Draw a set of pipes.
-
-#    Parameters:
-#        pipes (list): {'radius':, 'start':, 'end':, 'color':, 'name':, 'layer':}.
-#        div (int): Divisions around cross-section.
-
-#    Returns:
-#        list: Created pipe objects.
-#    """
-#    bpy.ops.mesh.primitive_cylinder_add(radius=1, depth=1, vertices=div, location=[0, 0, 0])
-#    object = bpy.context.object
-#    objects = []
-#    for pipe in pipes:
-#        radius = pipe.get('radius', 1)
-#        start = pipe.get('start', [0, 0, 0])
-#        end = pipe.get('end', [0, 0, 1])
-#        L = distance_point_point(start, end)
-#        pos = centroid_points([start, end])
-#        copy = object.copy()
-#        copy.name = pipe.get('name', 'pipe')
-#        copy.rotation_euler[1] = acos((end[2] - start[2]) / L)
-#        copy.rotation_euler[2] = atan2(end[1] - start[1], end[0] - start[0])
-#        copy.location = Vector(pos)
-#        copy.data = copy.data.copy()
-#        copy.scale = ((radius, radius, L))
-#        copy.show_wire = True
-#        material = create_material(color=pipe.get('color', [1, 1, 1]))
-#        copy.data.materials.append(material)
-#        set_object_layer(object=copy, layer=pipe.get('layer', 0))
-#        objects.append(copy)
-#    delete_object(object=object)
-#    return _link_objects(objects)
 
 
 #def xdraw_pointcloud(points):
@@ -346,34 +191,11 @@ def xdraw_lines(lines):
 #    return _link_objects(objects)
 
 
-#    vertices = [[-1, 0, 0], [-2, 0, 0], [-2, 1, 0], [-1, 1, 0]]
-#    faces = [[0, 1, 2], [2, 3, 0]]
-#    bmesh = xdraw_mesh(name='bmesh', vertices=vertices, faces=faces, color=[1, 1, 1], alpha=0.5, layer=1)
-
-#    draw_cubes(pos=[[0, 3, 0]], radius=0.5, color=[1, 1, 0], layer=1)
-#    xdraw_cubes([{'radius': 0.5, 'pos': [0, 2, 0], 'color': [1, 0, 0], 'layer': 1}])
-
-#    draw_spheres(pos=[[-2, 2, 0]], radius=0.5, color=[0, 1, 0], layer=1)
-#    xdraw_spheres([{'radius': 0.5, 'pos': [-2, -1, 0], 'layer': 1}])
 
 #    xdraw_texts([{'radius': 0.5, 'pos': [5, 0, 0], 'color': [0, 0, 1], 'text': 'TEXT', 'layer': 1}])
 
 #    points = [{'pos': [i[0], i[1], i[2] + 2], 'radius': 0.01, 'layer': 1} for i in list(rand(10, 3))]
 #    xdraw_points(points)
-
-    xdraw_lines([{
-        'start': [3, 1, 0],
-        'end': [5, -1, 0],
-        'name': 'line-test',
-        'width': 0.1,
-        'collection': 'Collection 2',
-        'color': [0, 0.5, 1],
-    }])
-
-    print(dir(bpy.context.scene.objects))
-
-#    draw_pipes(start=[[0, -2, 0]], end=[[0, -2, 1]], radius=0.1, color=[0, 0, 1], layer=1)
-#    xdraw_pipes([{'radius': 0.1, 'start': [0, -1, 0], 'end': [0, -1, 1], 'color': [0, 1, 0], 'layer': 1}])
 
 #    draw_plane(Lx=1, Ly=1, dx=0.5, dy=0.5, bracing='cross', color=[1, 0, 1], layer=1)
 
--- src/compas_blender/utilities/drawing.py
+++ src/compas_blender/utilities/drawing.py
@@ -5,11 +5,17 @@ from __future__ import print_function
 
 from compas_blender.utilities import set_deselect
 
+from compas.geometry import distance_point_point
+from compas.geometry import centroid_points
+
 try:
     import bpy
     from mathutils import Vector
 except ImportError:
     pass
+
+from math import acos
+from math import atan2
 
 
 __author__    = ['Andrew Liew <liew@arch.ethz.ch>']
@@ -24,7 +30,6 @@ __all__ = [
     'xdraw_points',
     'xdraw_lines',
     'xdraw_geodesics',
-    'xdraw_polylines',
     'xdraw_faces',
     'xdraw_cylinders',
     'xdraw_pipes',
@@ -33,11 +38,12 @@ __all__ = [
 ]
 
 
-#def _link_objects(objects):
-#
-#    for object in objects:
-#        bpy.context.scene.objects.link(object)
-#    return objects
+def _link_objects(objects):
+
+    for object in objects:
+        bpy.context.collection.objects.link(object)
+
+    return objects
 
 
 def create_material(color, alpha=1):
@@ -60,16 +66,16 @@ def xdraw_labels(labels, **kwargs):
 
 def xdraw_points(points, **kwargs):
 
-    objects = []
+    objects = [0] * len(points)
 
-    for data in points:
+    for c, data in enumerate(points):
         
         bpy.ops.object.empty_add(type='SPHERE', radius=data['radius'], location=data['pos'])
         object = bpy.context.object
         
         object.name = data.get('name', 'point')
         # layer
-        objects.append(object)
+        objects[c] = object
 
     set_deselect(objects=objects)
 
@@ -78,15 +84,38 @@ def xdraw_points(points, **kwargs):
 
 def xdraw_lines(lines, **kwargs):
 
-    raise NotImplementedError
+    objects = []
+
+    for data in lines:
+
+        start = data.get('start')
+        end   = data.get('end')
+        name  = data.get('name', 'line')
+        
+        bpy.ops.curve.primitive_nurbs_curve_add(radius=1, location=(0, 0, 0))
+        object = bpy.context.object
+
+        spline = object.data.splines[0]
+        spline.points[0].co = start + [1]
+        spline.points[1].co = start + [1]
+        spline.points[2].co = end + [1]
+        spline.points[3].co = end + [1]
+        spline.order_u = 1
+
+        object.data.fill_mode = 'FULL'
+        object.data.bevel_depth = data.get('width', 0.05)
+        object.data.bevel_resolution = 0
+        object.data.materials.append(create_material(color=data.get('color', [1, 1, 1])))
+        # layer
+        
+        objects.append(object)
+        
+    set_deselect(objects=objects)
+
+    return objects
 
 
 def xdraw_geodesics(geodesics, **kwargs):
-
-    raise NotImplementedError
-
-
-def xdraw_polylines(polylines, **kwargs):
 
     raise NotImplementedError
 
@@ -96,9 +125,33 @@ def xdraw_breps(faces, **kwargs):
     raise NotImplementedError
 
 
-def xdraw_cylinders(cylinders, cap=False, **kwargs):
+def xdraw_cylinders(cylinders, div=10, **kwargs):
+    
+    objects = []
 
-    raise NotImplementedError
+    for data in cylinders:
+        
+        bpy.ops.mesh.primitive_cylinder_add(radius=1, depth=1, vertices=div, location=[0, 0, 0])
+        object = bpy.context.object
+        
+        radius = data.get('radius', 1)
+        start  = data.get('start', [0, 0, 0])
+        end    = data.get('end', [0, 0, 1])
+        L      = distance_point_point(start, end)
+        pos    = centroid_points([start, end])
+        
+        object.name = data.get('name', 'cylinder')
+        object.rotation_euler[1] = acos((end[2] - start[2]) / L)
+        object.rotation_euler[2] = atan2(end[1] - start[1], end[0] - start[0])
+        object.location = pos
+        object.scale = ((radius, radius, L))
+        # layer
+        object.data.materials.append(create_material(color=data.get('color', [1, 1, 1])))
+        objects.append(object)
+
+    set_deselect(objects=objects)
+
+    return objects
 
 
 def xdraw_pipes(pipes, cap=2, fit=1.0, **kwargs):
@@ -150,9 +203,23 @@ def xdraw_cubes(cubes, **kwargs):
     return objects
 
 
-def xdraw_mesh(vertices, faces, name=None, color=None, **kwargs):
+def xdraw_mesh(vertices, faces, edges=None, name='mesh', color=[1, 1, 1], **kwargs):
 
-    raise NotImplementedError
+    edges = [] if not edges else edges
+
+    mesh = bpy.data.meshes.new(name)
+    mesh.from_pydata(vertices, edges, faces)
+    mesh.update(calc_edges=True)
+
+    object = bpy.data.objects.new(name, mesh)
+    bpy.context.collection.objects.link(object)
+    object.show_wire = True
+    object.data.materials.append(create_material(color=color))
+    # layer
+    
+    set_deselect(objects=[object])
+    
+    return object    
 
 
 def xdraw_faces(faces, **kwargs):
@@ -182,9 +249,25 @@ if __name__ == '__main__':
         {'pos': [2, 0, 1], 'radius': 0.2, 'name': 'cube', 'color': [1, 1, 0]},
         {'pos': [2, 0, 3], 'radius': 0.5, 'color': [0, 1, 1]},
     ]
-
-    objects = xdraw_points(points=points)
-    objects = xdraw_spheres(spheres=spheres)
-    objects = xdraw_cubes(cubes=cubes)
     
-    set_objects_show_names(objects=objects, show=1)
+    cylinders = [
+        {'start': [3, 1, 1], 'end': [3, 0, 2], 'radius': 0.2, 'name': 'cylinder', 'color': [1, 0, 1]},
+        {'start': [3, 1, 3], 'end': [3, 0, 4], 'radius': 0.1, 'color': [0, 0, 1]},
+    ]
+    
+    lines = [
+        {'start': [4, 1, 1], 'end': [4, 0, 2], 'radius': 0.05, 'name': 'line', 'color': [1, 0, 0]},
+        {'start': [4, 1, 3], 'end': [4, 0, 4], 'radius': 0.1, 'color': [1, 0, 1]},
+    ]
+
+    for i in range(1000):
+        xdraw_points(points=points)
+        #xdraw_spheres(spheres=spheres)
+        #xdraw_cubes(cubes=cubes)
+        #xdraw_cylinders(cylinders=cylinders)
+        #xdraw_lines(lines=lines)
+    
+    #vertices = [[-1, 0, 1], [-2, 0, 2], [-2, 1, 1], [-1, 1, 0]]
+    #faces    = [[0, 1, 2], [2, 3, 0]]
+    #bmesh    = xdraw_mesh(name='bmesh', vertices=vertices, faces=faces, color=[1, 0, 1])
+    
