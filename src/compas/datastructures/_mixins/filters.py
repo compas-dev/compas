44e504e47812f79de35ba4c23e79b177dc5a4072
Achillx <xydis@arch.ethz.ch>
2018-12-04 15:12:08 +0100

    - added vertices_where_predicate() - vertices_where() now can search for value/s even if attr is a list

--- src/compas/datastructures/_mixins/filters.py
+++ src/compas/datastructures/_mixins/filters.py
@@ -44,6 +44,12 @@ class VertexFilter(object):
                 if callable(method):
                     val = method(key)
 
+                    if isinstance(val, list):
+                       if value not in val:
+                          is_match = False
+                          break
+                       break
+
                     if isinstance(value, (tuple, list)):
                         minval, maxval = value
 
@@ -60,6 +66,12 @@ class VertexFilter(object):
                         is_match = False
                         break
 
+                    if isinstance(attr[name], list):
+                       if value not in attr[name]:
+                          is_match = False
+                          break
+                       break
+
                     if isinstance(value, (tuple, list)):
                         minval, maxval = value
 
@@ -73,6 +85,62 @@ class VertexFilter(object):
 
             if is_match:
 
+                if data:
+                    yield key, attr
+                else:
+                    yield key
+
+    def vertices_where_predicate(self, predicate, data=False):
+        """Get vertices for which a certain condition or set of conditions is true using a lambda function.
+
+        Parameters
+        ----------
+        predicate : function
+            The condition you want to evaluate
+        data : bool, optional
+            Yield the vertices and their data attributes.
+            Default is ``False``.
+
+        Yields
+        ------
+        key: hashable
+            The next vertex that matches the condition.
+        2-tuple
+            The next vertex and its attributes, if ``data=True``.
+
+        Examples
+        --------
+        .. code-block:: python
+
+            from compas.datastructures import Mesh
+
+            mesh = Mesh()
+
+            v0 = mesh.add_vertex( x = 0.0, y = 0.0, z = 0.0, extra_attr1 = 0, extra_attr2 = [])
+            v1 = mesh.add_vertex( x = 100.0, y = 0.0, z = 0.0, extra_attr1 = 2, extra_attr2 = [3,5,9])
+            v2 = mesh.add_vertex( x = 100.0, y = 100.0, z = 0.0, extra_attr1 = 1, extra_attr2 = ['value1', 'value2'])
+            v3 = mesh.add_vertex( x = 0.0, y = 100.0, z = 0.0, extra_attr1 = 2, extra_attr2 = [3,7,12])
+
+            mesh.add_face([v0,v1,v3])
+            mesh.add_face([v1,v2,v3])
+
+            for v_key in mesh.vertices_where_predicate(lambda _, attr: attr['x'] == 100.0):
+                print v_key
+
+            for v_key, v_data in mesh.vertices_where_predicate(lambda _, attr: 50.0 <= attr['y'] <= 150.0, True):
+                print v_key, v_data
+
+            for v_key in mesh.vertices_where_predicate(lambda _, attr: 'extra_attr2' in attr):
+                print v_key
+
+            for v_key in mesh.vertices_where_predicate(lambda _, attr: 3 in attr['extra_attr2']):
+                print v_key
+
+            for v_key in mesh.vertices_where_predicate(lambda _, attr: 'value2' in attr['extra_attr2']):
+                print v_key
+        """
+        for key, attr in self.vertices(True):
+            if predicate(key, attr):
                 if data:
                     yield key, attr
                 else:
