442fcf79f1a67ee3cc0e891dabd4fc8a5f99b1cc
robin-oval <34314884+robin-oval@users.noreply.github.com>
2018-12-05 15:16:18 +0100

    second level import in join

--- src/compas/datastructures/mesh/operations/join.py
+++ src/compas/datastructures/mesh/operations/join.py
@@ -4,7 +4,7 @@ from __future__ import division
 
 from compas.utilities import geometric_key
 
-from compas.geometry.algorithms.purging import mesh_cull_duplicate_vertices
+from compas.geometry import mesh_cull_duplicate_vertices
 
 from copy import deepcopy
 
