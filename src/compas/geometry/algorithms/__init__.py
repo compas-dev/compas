ed0aaa80b5091a79594db5d98a6ec7cbb457d739
Tom Van Mele <vanmelet@ethz.ch>
2018-12-06 11:55:54 +0100

    fixed circular imports for robin’s pull request

--- src/compas/geometry/algorithms/__init__.py
+++ src/compas/geometry/algorithms/__init__.py
@@ -1,8 +1,9 @@
-from __future__ import absolute_import
+from __future__ import absolute_import, division, print_function
 
 from .bbox import *
 from .bbox_numpy import *
 from .bestfit import *
+from .bestfit_numpy import *
 from .boolean import *
 from .geodesics import *
 from .hull import *
@@ -16,33 +17,4 @@ from .smoothing import *
 from .smoothing_cpp import *
 from .offset import *
 
-from . import bbox
-from . import bbox_numpy
-from . import bestfit
-from . import boolean
-from . import geodesics
-from . import hull
-from . import hull_numpy
-from . import interpolation
-from . import isolines
-from . import parallelisation
-from . import planarisation
-from . import purging
-from . import smoothing
-from . import smoothing_cpp
-from . import offset
-
-__all__ = []
-
-__all__ += bbox.__all__ + bbox_numpy.__all__
-__all__ += bestfit.__all__
-__all__ += boolean.__all__
-__all__ += geodesics.__all__
-__all__ += hull.__all__ + hull_numpy.__all__
-__all__ += interpolation.__all__
-__all__ += isolines.__all__
-__all__ += parallelisation.__all__
-__all__ += planarisation.__all__
-__all__ += purging.__all__
-__all__ += smoothing.__all__ + smoothing_cpp.__all__
-__all__ += offset.__all__
+__all__ = [name for name in dir() if not name.startswith('_')]
