18b317c230432e91114476e1730288d1fa4667b2
Tom Van Mele <vanmelet@ethz.ch>
2018-12-10 13:38:52 +0100

    tests for centroid points

--- tests/geometry/test_basics.py
+++ tests/geometry/test_basics.py
@@ -5,6 +5,13 @@ from math import pi
 from compas.geometry import angle_vectors
 from compas.geometry import angles_vectors
 
+from compas.geometry import centroid_points
+
+
+# ==============================================================================
+# angles
+# ==============================================================================
+
 
 @pytest.mark.parametrize(
     ("u", "v", "angle"), 
@@ -49,3 +56,33 @@ def test_angle_vectors(u, v, angle):
 def test_angles_vectors(u, v, angles):
     a, b = angles
     assert angles_vectors(u, v) == (pytest.approx(a), pytest.approx(b))
+
+
+# ==============================================================================
+# average
+# ==============================================================================
+
+
+@pytest.mark.parametrize(
+    ("points", "centroid"), 
+    [
+        pytest.param([0.0, 0.0, 0.0], None, marks=pytest.mark.xfail(raises=TypeError)),
+        pytest.param([[0.0, 0.0, 0.0], [0.0, 0.0]], None, marks=pytest.mark.xfail(raises=ValueError)),
+        pytest.param([[0.0, 0.0]], None, marks=pytest.mark.xfail(raises=ValueError)),
+
+        ([[0.0, 0.0, 0.0]], [0.0, 0.0, 0.0]),
+        ([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]], [0.0, 0.0, 0.0]),
+
+        ([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]], [0.0, 0.0, 0.0]),
+        ([[0.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.5, 1.0, 0.0]], [0.1666, 0.6666, 0.0]),
+
+        ([[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [1.0, 1.0, 0.0], [0.0, 1.0, 0.0]], [0.5, 0.5, 0.0]),
+        ([[0.0, 0.0, 0.0], [0.1, 0.0, 0.0], [1.0, 0.0, 0.0], [1.0, 1.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.1, 0.0]], [0.35, 0.35, 0.0]),
+    ]
+)
+def test_centroid_points(points, centroid):
+    if centroid is None:
+        x, y, z = 0.0, 0.0, 0.0
+    else:
+        x, y, z = centroid
+    assert centroid_points(points) == [pytest.approx(x, 0.001), pytest.approx(y, 0.001), pytest.approx(z, 0.001)]
