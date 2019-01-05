5f53ea946946b22f6a9dc1438ab5bc41b879d96d
freeeki <freeeki@gmail.com>
2019-01-05 11:08:28 +0100

    rereference docs

--- docs/conf.py
+++ docs/conf.py
@@ -180,7 +180,7 @@ intersphinx_mapping = {
 # -- Options for HTML output ----------------------------------------------
 
 html_theme = 'compas'
-html_theme_path = ['../../sphinx_compas_theme', '../temp/sphinx_compas_theme']
+html_theme_path = ['../../sphinx_compas_theme/sphinx_compas_theme', '../temp/sphinx_compas_theme/sphinx_compas_theme']
 html_theme_options = {
     'navbar_active' : 'main',
 }
