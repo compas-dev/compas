51f5edcbd7965ef520752e2c1047283664ae176b
Gonzalo Casas <casas@arch.ethz.ch>
2019-01-20 08:00:10 +0100

    Add planarity back and a test case

--- tests/datastructures/test_network.py
+++ tests/datastructures/test_network.py
@@ -1,4 +1,7 @@
+import compas
+
 from compas.datastructures import Network
+from compas.datastructures import network_is_planar
 
 
 def test_add_vertex():
@@ -7,3 +10,8 @@ def test_add_vertex():
     assert network.add_vertex(x=0, y=0, z=0) == 1
     assert network.add_vertex(key=2) == 2
     assert network.add_vertex(key=0, x=1) == 0
+
+def test_planarity():
+    network = Network.from_obj(compas.get('lines.obj'))
+    network.add_edge(6, 15)
+    assert network_is_planar(network) is not True
