3e583b6274bb6f1954e2f7bf6075bcf03a10a19e
Robin Oval <oval@arch.ethz.ch>
2019-02-12 13:26:56 +0100

    binomial coefficient function

--- src/compas/utilities/functions.py
+++ src/compas/utilities/functions.py
@@ -4,7 +4,8 @@ from __future__ import division
 
 
 __all__ = [
-    'fibonacci'
+    'fibonacci',
+    'binomial_coefficient'
 ]
 
 
@@ -19,6 +20,35 @@ def fibonacci(n, memo={}):
         memo[n] = fibonacci(n - 2, memo) + fibonacci(n - 1, memo)
     return memo[n]
 
+def binomial_coefficient(n, k):
+    """Binomial coefficient (n k), i.e. the number of possible combinations to select k elements among n ones.
+
+    Parameters
+    ----------
+    n : int
+        Number of elements.
+    k : int
+        Number of selected elements.
+
+    Returns
+    -------
+    x : int
+        The number of possible combinations.
+
+    """
+
+    k = min(k, n - k)
+    x = 1
+    y = 1
+    i = n - k + 1
+
+    while i <= n:
+        x = (x * i) // y
+        y += 1
+        i += 1
+    
+    return x
+
 # ==============================================================================
 # Main
 # ==============================================================================
@@ -26,3 +56,5 @@ def fibonacci(n, memo={}):
 if __name__ == "__main__":
 
     print(fibonacci(100))
+
+    print(binomial_coefficient(5, 3))
