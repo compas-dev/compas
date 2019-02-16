ce2841608806db55f54f89995414f02f9a71541c
Tom Van Mele <vanmelet@ethz.ch>
2019-02-16 07:18:18 +0100

    cleanup

--- src/compas/datastructures/mesh/offset.py
+++ src/compas/datastructures/mesh/offset.py
@@ -9,11 +9,12 @@ from compas.datastructures.mesh.orientation import mesh_flip_cycles
 from compas.datastructures.mesh.join import meshes_join
 
 __all__ = [
-	'mesh_offset',
-	'mesh_thicken'
-	]
+    'mesh_offset',
+    'mesh_thicken'
+]
 
-def mesh_offset(mesh, offset = 1., cls = None):
+
+def mesh_offset(mesh, offset=1.0, cls=None):
     """Offset a mesh.
 
     Parameters
@@ -23,26 +24,31 @@ def mesh_offset(mesh, offset = 1., cls = None):
     offset : real
         The offset distance.
 
-
     Returns
     -------
     Mesh
         The offset mesh.
 
     """
-
     if cls is None:
-    	cls = type(mesh)
+        cls = type(mesh)
 
     # new coordinates of vertex keys
-    vertex_map = {vkey: (i, [0,0,0]) if len(mesh.vertex_neighbors(vkey)) == 0 else (i, add_vectors(mesh.vertex_coordinates(vkey), scale_vector(mesh.vertex_normal(vkey), offset))) for i, vkey in enumerate(mesh.vertices())}
-    
-    vertices = [xyz for i, xyz in vertex_map.values()]   
-    faces = [[vertex_map[vkey][0] for vkey in mesh.face_vertices(fkey)] for fkey in mesh.faces()] 
-    
+    vertex_map = {}
+    for i, vkey in enumerate(mesh.vertices()):
+        if len(mesh.vertex_neighbors(vkey)) == 0:
+            vertex_map[vkey] = i, [0, 0, 0]
+        else:
+            vertex_map[vkey] = i, add_vectors(mesh.vertex_coordinates(vkey),
+                                              scale_vector(mesh.vertex_normal(vkey), offset))
+
+    vertices = [xyz for i, xyz in vertex_map.values()]
+    faces = [[vertex_map[vkey][0] for vkey in mesh.face_vertices(fkey)] for fkey in mesh.faces()]
+
     return cls.from_vertices_and_faces(vertices, faces)
 
-def mesh_thicken(mesh, thickness = 1., cls = None):
+
+def mesh_thicken(mesh, thickness=1.0, cls=None):
     """Thicken a mesh.
 
     Parameters
@@ -57,9 +63,8 @@ def mesh_thicken(mesh, thickness = 1., cls = None):
     thickened_mesh : Mesh
         The thickened mesh.
     """
-
     if cls is None:
-    	cls = type(mesh)
+        cls = type(mesh)
 
     # offset in both directions
     mesh_top, mesh_bottom = map(lambda eps: mesh_offset(mesh, eps * thickness / 2., cls), [+1, -1])
@@ -78,10 +83,10 @@ def mesh_thicken(mesh, thickness = 1., cls = None):
 
     return thickened_mesh
 
+
 # ==============================================================================
 # Main
 # ==============================================================================
 
 if __name__ == '__main__':
-
-    import compas
+    pass
