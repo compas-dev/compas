14bdbb73a8e2c6018accc6e713febc16597dc5fb
Robin Oval <oval@arch.ethz.ch>
2019-02-19 18:12:16 +0100

    optional z parameter

--- src/compas/geometry/analytical.py
+++ src/compas/geometry/analytical.py
@@ -10,7 +10,7 @@ __all__ = [
 	'helix_evaluate'
 ]
 
-def circle_evaluate(t, r, z):
+def circle_evaluate(t, r, z=0):
 	"""Evalutes a circle at a parameter.
 
 	Parameters
@@ -19,8 +19,8 @@ def circle_evaluate(t, r, z):
 		Parameter
 	r : float
 		Constant
-	z : float
-		Height
+	z : float, optional
+		Height. Default is 0.
 
 	Notes
 	-----
@@ -34,7 +34,7 @@ def circle_evaluate(t, r, z):
 	"""
 	return (r * cos(t), r * sin(t), z)
 
-def ellipse_evaluate(t, r, z):
+def ellipse_evaluate(t, r, z=0):
 	"""Evalutes an ellipse at a parameter.
 
 	Parameters
@@ -45,8 +45,8 @@ def ellipse_evaluate(t, r, z):
 		Constant
 	b : float
 		Constant
-	z : float
-		Height
+	z : float, optional
+		Height. Default is 0.
 
 	Notes
 	-----
@@ -60,7 +60,7 @@ def ellipse_evaluate(t, r, z):
 	"""
 	return (a * cos(t), b * sin(t), z)
 
-def archimedean_spiral_evaluate(t, a, b):
+def archimedean_spiral_evaluate(t, a, b, z=0):
 	"""Evalutes a spiral at a parameter. The analytical polar equation is r = a + b * theta.
 
 	Parameters
@@ -71,11 +71,13 @@ def archimedean_spiral_evaluate(t, a, b):
 		Constant
 	b : float
 		Constant
+	z : float, optional
+		Height. Default is 0.
 
 	Returns
 	-------
 	tuple
-		The (x, y) coordinates.
+		The (x, y, z) coordinates.
 
 	Notes
 	-----
@@ -88,9 +90,9 @@ def archimedean_spiral_evaluate(t, a, b):
 	.. [1] GeoGebra. *Archimedean Spiral built by parametric equations*.
 		   Available at: https://www.geogebra.org/m/dZuH5hWa.
 	"""
-	return (b * t * cos(t + a), b * t * sin(t + a))
+	return (b * t * cos(t + a), b * t * sin(t + a), z)
 
-def logarithmic_spiral_evaluate(t, a, b):
+def logarithmic_spiral_evaluate(t, a, b, z=0):
 	"""Evalutes a logarithmic spiral at a parameter. The analytical polar equation is r = a * exp(b * theta).
 
 	Parameters
@@ -101,18 +103,20 @@ def logarithmic_spiral_evaluate(t, a, b):
 		Constant
 	b : float
 		Constant
+	z : float, optional
+		Height. Default is 0.
 
 	Returns
 	-------
 	tuple
-		The (x, y) coordinates.
+		The (x, y, z) coordinates.
 
 	References
 	----------
 	.. [1] GeoGebra. *An equiangular spiral - parametric equation*.
 		   Available at: https://www.geogebra.org/m/zsHgCvq7.
 	"""
-	return (a * exp(b * t) * cos(t), a * exp(b * t) * sin(t))
+	return (a * exp(b * t) * cos(t), a * exp(b * t) * sin(t), z)
 
 def helix_evaluate(t, a, b):
 	"""Evalutes an helix at a parameter.
