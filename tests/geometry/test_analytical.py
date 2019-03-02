67c2f54b96e51a8cd30ae0a751f3e707d5ac3454
Robin Oval <oval@arch.ethz.ch>
2019-03-02 18:50:57 +0100

    testing of circle and ellipse

---
+++ tests/geometry/test_analytical.py
@@ -0,0 +1,35 @@
+import pytest
+from compas.geometry import circle_evaluate
+from compas.geometry import ellipse_evaluate
+from compas.geometry import archimedean_spiral_evaluate
+from compas.geometry import logarithmic_spiral_evaluate
+from compas.geometry import helix_evaluate
+from math import pi
+
+
+def test_circle():
+    x, y, z = circle_evaluate(0, 1, 0)
+    assert x == 1 and y == 0 and z == 0
+    x, y, z = circle_evaluate(0, 1, 1)
+    assert x == 1 and y == 0 and z == 1
+    x, y, z = circle_evaluate(pi / 2, 1, 0)
+    assert x == 0 and y == 1 and z == 0
+    x, y, z = circle_evaluate(0, 0, 0)
+    assert x == 0 and y == 0 and z == 0
+    x, y, z = circle_evaluate(0, -1, 0)
+    assert x == -1 and y == 0 and z == 0
+
+
+def test_ellipse():
+    x, y, z = ellipse_evaluate(0, 1, 1, 0)
+    assert x == 1 and y == 0 and z == 0
+    x, y, z = ellipse_evaluate(0, 2, 1, 1)
+    assert x == 2 and y == 0 and z == 1
+    x, y, z = ellipse_evaluate(pi / 2, 1, 1, 0)
+    assert x == 0 and y == 1 and z == 0
+    x, y, z = ellipse_evaluate(pi / 2, 1, 2, 0)
+    assert x == 0 and y == 2 and z == 0
+    x, y, z = ellipse_evaluate(0, 0, 0, 0)
+    assert x == 0 and y == 0 and z == 0
+    x, y, z = ellipse_evaluate(0, -1, 1, 0)
+    assert x == -1 and y == 0 and z == 0
