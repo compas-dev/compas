91947139e613cb1a9368dff2b7ec692079fdbc55
Tom Van Mele <vanmelet@ethz.ch>
2019-03-04 01:20:30 +0100

    Bump version to 0.4.18

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ optional_requirements = {
 
 setup(
     name='COMPAS',
-    version='0.4.17',
+    version='0.4.18',
     description='The COMPAS framework',
     long_description=long_description,
     long_description_content_type='text/markdown',
