7e84da4cedea672999e4f63786b1f3b59baad42d
Robin Oval <oval@arch.ethz.ch>
2019-03-08 11:37:19 +0100

    name fix

--- tests/datastructures/test_mesh_operations.py
+++ tests/datastructures/test_mesh_operations.py
@@ -26,24 +26,24 @@ def mesh_0():
 
 def test__insert_vertex_on_edge(mesh_0):
     mesh_insert_vertex_on_edge(mesh_0, 0, 1)
-    assert mesh.face_degree(0) == 4
-    assert mesh.face_degree(0) == 4
-    assert mesh.face_vertex_descendant(0, 0) == 5
-    assert mesh.face_vertex_descendant(1, 1) == 5
+    assert mesh_0.face_degree(0) == 4
+    assert mesh_0.face_degree(0) == 4
+    assert mesh_0.face_vertex_descendant(0, 0) == 5
+    assert mesh_0.face_vertex_descendant(1, 1) == 5
 
     mesh_insert_vertex_on_edge(mesh_0, 0, 2, 4)
-    assert mesh.face_degree(0) == 5
-    assert mesh.face_vertex_descendant(0, 2) == 4
+    assert mesh_0.face_degree(0) == 5
+    assert mesh_0.face_vertex_descendant(0, 2) == 4
 
 
 def test_mesh_substitute_vertex_in_faces(mesh_0):
-    mesh_substitute_vertex_in_faces(mesh, 0, 4)
-    assert 4 in mesh.face_vertices(0)
-    assert 0 not in mesh.face_vertices(0)
-    assert 4 in mesh.face_vertices(1)
-    assert 0 not in mesh.face_vertices(1)
-    mesh_substitute_vertex_in_faces(4, 0, 1)
-    assert 4 in mesh.face_vertices(0)
-    assert 0 not in mesh.face_vertices(0)
-    assert 0 in mesh.face_vertices(1)
-    assert 4 not in mesh.face_vertices(1)
+    mesh_substitute_vertex_in_faces(mesh_0, 0, 4)
+    assert 4 in mesh_0.face_vertices(0)
+    assert 0 not in mesh_0.face_vertices(0)
+    assert 4 in mesh_0.face_vertices(1)
+    assert 0 not in mesh_0.face_vertices(1)
+    mesh_substitute_vertex_in_faces(mesh_0, 4, 0, 1)
+    assert 4 in mesh_0.face_vertices(0)
+    assert 0 not in mesh_0.face_vertices(0)
+    assert 0 in mesh_0.face_vertices(1)
+    assert 4 not in mesh_0.face_vertices(1)
