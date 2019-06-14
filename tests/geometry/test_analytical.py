162fa092451a1e2b2c51806701f189ea7603236d
Robin Oval <oval@arch.ethz.ch>
2019-06-14 17:30:02 +0200

    threshold value for tests

--- tests/geometry/test_analytical.py
+++ tests/geometry/test_analytical.py
@@ -8,12 +8,13 @@ from math import pi
 
 
 def test_circle():
+    threshold = 1e-6
     x, y, z = circle_evaluate(0, 1, 0)
     assert x == 1 and y == 0 and z == 0
     x, y, z = circle_evaluate(0, 1, 1)
     assert x == 1 and y == 0 and z == 1
     x, y, z = circle_evaluate(pi / 2, 1, 0)
-    assert x == 0 and y == 1 and z == 0
+    assert x < threshold and x > -threshold and y == 1 and z == 0
     x, y, z = circle_evaluate(0, 0, 0)
     assert x == 0 and y == 0 and z == 0
     x, y, z = circle_evaluate(0, -1, 0)
@@ -21,14 +22,15 @@ def test_circle():
 
 
 def test_ellipse():
+    threshold = 1e-6
     x, y, z = ellipse_evaluate(0, 1, 1, 0)
     assert x == 1 and y == 0 and z == 0
     x, y, z = ellipse_evaluate(0, 2, 1, 1)
     assert x == 2 and y == 0 and z == 1
     x, y, z = ellipse_evaluate(pi / 2, 1, 1, 0)
-    assert x == 0 and y == 1 and z == 0
+    assert x < threshold and x > -threshold and y == 1 and z == 0
     x, y, z = ellipse_evaluate(pi / 2, 1, 2, 0)
-    assert x == 0 and y == 2 and z == 0
+    assert x < threshold and x > -threshold and y == 2 and z == 0
     x, y, z = ellipse_evaluate(0, 0, 0, 0)
     assert x == 0 and y == 0 and z == 0
     x, y, z = ellipse_evaluate(0, -1, 1, 0)
