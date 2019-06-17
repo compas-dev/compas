a3623ab64f57260508ade10d029e20b4fde3b37d
Achillx <xydis@arch.ethz.ch>
2019-06-17 11:31:54 +0200

    - renamed function - corrected documentation - added a function to get all attributes of multiple faces

--- src/compas/datastructures/_mixins/attributes.py
+++ src/compas/datastructures/_mixins/attributes.py
@@ -768,7 +768,7 @@ class FaceAttributesManagement(object):
         temp = list(zip(names, values))
         return [[self.get_face_attribute(fkey, name, value) for name, value in temp] for fkey in fkeys]
 
-    def get_all_face_attributes(self, key, data=False):
+    def get_face_all_attributes(self, key, data=True):
         """Get all attributes of one face.
 
         Parameters
@@ -776,15 +776,15 @@ class FaceAttributesManagement(object):
         key : hashable
             The identifier of the face.
         data : bool, optional
-            Yield the attributes and their values.
-            Default is ``False``.
+            Returns the attributes and their values.
+            Default is ``True``.
 
         Returns
         ------
-        attributes: list
-            A list of all attribute keys.
         dict
             The dictionary containing all attributes and their values, if ``data=True``.
+        list
+            A list of all attribute keys, if ``data=False`.
 
         See Also
         --------
@@ -792,12 +792,42 @@ class FaceAttributesManagement(object):
         * :meth:`get_face_attributes`
         * :meth:`get_faces_attribute`
         * :meth:`get_faces_attributes`
+        * :meth:'get_faces_all_attributes'
 
         """
         if data:
             return self.facedata[key]
         else:
-            return self.facedata[key].keys()
+            return list(self.facedata[key].keys())
+
+    def get_faces_all_attributes(self, keys, data=True):
+        """Get all attributes of multiple faces.
+
+        Parameters
+        ----------
+        keys : list of hashable
+            A list of face identifiers.
+        data : bool, optional
+            Returns the attributes and their values.
+            Default is ``True``.
+
+        Returns
+        ------
+        list of dict
+            A list of dictionaries containing all attributes and their values, if ``data=True``.
+        list of list
+            A list of all attribute keys for each specified face key, if ``data=False`.
+
+        See Also
+        --------
+        * :meth:`get_face_attribute`
+        * :meth:`get_face_attributes`
+        * :meth:`get_faces_attribute`
+        * :meth:`get_faces_attributes`
+        * :meth:'get_face_all_attributes'
+
+        """
+        return [self.get_face_all_attributes(key, data) for key in keys]
 
 
 # ==============================================================================
