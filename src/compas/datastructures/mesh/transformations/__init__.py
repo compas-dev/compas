27aa384cf6173238de436316bcb5436e5334c2a7
Tom Van Mele <vanmelet@ethz.ch>
2019-10-01 23:01:04 +0200

    mesh numpy based transformations break rhino import

--- src/compas/datastructures/mesh/transformations/__init__.py
+++ src/compas/datastructures/mesh/transformations/__init__.py
@@ -2,9 +2,12 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import compas
 
 from .transformations import *
-from .transformations_numpy import *
+
+if not compas.IPY:
+    from .transformations_numpy import *
 
 
 __all__ = [name for name in dir() if not name.startswith('_')]
