56824a7593c74b91701ff78e9383d54c6e4c17c4
Romana Rust <rust@arch.ethz.ch>
2019-10-02 10:20:31 +0200

    update mesh subd gh example

--- docs/examples/gh/mesh-subd-modeling.py
+++ docs/examples/gh/mesh-subd-modeling.py
@@ -1,7 +1,9 @@
 """Mesh subdivision in Grasshopper using the catmull clark algorithm.
     Inputs:
-        fixed_keys: list of int
-            The vertex keys to be set to `is_fixed` 
+        is_fixed: list of boolean
+            A list of booleans relating to the vertex keys (index = key).
+        k: int
+            The number of levels of subdivision.
     Output:
         P: Rhino points (vertices) of the control mesh
         L: Rhino lines (edges) of the control mesh 
@@ -29,9 +31,12 @@ artist = MeshArtist(mesh)
 P = artist.draw_vertices()
 L = artist.draw_edges()
 
-# make a subd mesh (using catmullclark) and keep some of the vertices fixed
-mesh.set_vertices_attribute('is_fixed', True, fixed_keys)
-subd = mesh_subdivide(mesh, scheme='catmullclark', k=5, fixed=fixed_keys)
+# keep some of the vertices fixed and make a subd mesh (using catmullclark)
+for key, value in zip(range(len(is_fixed)), is_fixed):
+    mesh.set_vertex_attribute(key, 'is_fixed', value)
+            
+fixed = mesh.vertices_where({'is_fixed': True})
+subd = mesh_subdivide(mesh, scheme='catmullclark', k=k, fixed=fixed)
 
 # pass the new mesh to the artist
 artist.mesh = subd
