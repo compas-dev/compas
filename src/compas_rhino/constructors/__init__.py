0f59468b3369fc265241236f9305b98cddd43e41
Licini <leochen1990@gmail.com>
2019-10-15 15:58:45 +0200

    relink docs for rhino constructors

--- src/compas_rhino/constructors/__init__.py
+++ src/compas_rhino/constructors/__init__.py
@@ -1,4 +1,37 @@
-# TODO: drop in docs here
+"""
+********************************************************************************
+compas_rhino.constructors
+********************************************************************************
+
+.. currentmodule:: compas_rhino.constructors
+
+
+This package contains constructors for working with COMPAS data structures in Rhino.
+
+mesh
+====
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    mesh_from_guid
+    mesh_from_surface
+    mesh_from_surface_uv
+    mesh_from_surface_heightfield
+
+
+volmesh
+=======
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    volmesh_from_polysurfaces
+    volmesh_from_wireframe
+
+"""
 
 from __future__ import absolute_import
 
