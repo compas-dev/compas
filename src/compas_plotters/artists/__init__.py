2491b3263c8697bfcc93970f723459755d110242
Tom Van Mele <vanmelet@ethz.ch>
2019-10-19 08:05:10 +0200

    artists

---
+++ src/compas_plotters/artists/__init__.py
@@ -0,0 +1,29 @@
+"""
+********************************************************************************
+compas_plotters.artists
+********************************************************************************
+
+.. currentmodule:: compas_plotters.artists
+
+
+Classes
+=======
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from .artist import Artist
+
+from .vectorartist import VectorArtist
+from .pointartist import PointArtist
+from .lineartist import LineArtist
+
+__all__ = [name for name in dir() if not name.startswith('_')]
---
+++ src/compas_plotters/artists/pointartist.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+
+from compas_plotters.artists import Artist
+
+__all__ = ['PointArtist']
+
+
+class PointArtist(Artist):
+    """"""
+
+    def __init__():
+        super(PointArtist, self).__init__()
+
+    def draw(self):
+        pass
+
+
+# ==============================================================================
+# Main
+# ==============================================================================
+
+if __name__ == '__main__':
+    pass
