1a053579c523426c6a78658530b0742120966631
tetov <a@tetov.se>
2019-10-23 16:50:33 +0200

    Refactor and pathlib implemented, issues with ply and urllib in python2

--- src/compas/files/_BaseReader.py
+++ src/compas/files/_BaseReader.py
@@ -19,36 +19,73 @@ class BaseReader(object):
     location : str or pathlib object
         Path or URL to the file
     """
-
     def __init__(self, location):
-        self.location = location
+        self.is_URL = False
+        self.location = self.check_location(location)
 
-    def read(self, mode='ascii'):
+    def read(self, mode='text'):
         """Returns iterable reading file line by line when given a location,
            either as pathlib object, string containing path or string containing url
+           TODO: Update docstring
+        Arguments
+        ---------
+        mode : string
+            Treat file as ascii or binary
 
-        Returns
+        Yields
         -------
-        iterable
-            Iterable returning file line by line
+        string
+            yields next line
         """
 
-        if mode == 'ascii':
+        if mode == 'text':
 
-            if isinstance(self.location, Path):
-                with self.location.open(mode='r') as fh:
-                    for line in fh:
-                        yield line
-            elif self.location.startswith('http'):
+            if self.is_URL:
                 resp = urlopen(self.location)
-                yield from iter(resp.read().decode('utf-8').split('\n'))
+                print(resp)
+                for line in resp.read().decode('utf-8').split('\n'):
+                    yield line
             else:
-                with open(self.location, 'r') as fh:
-                    for line in fh:
-                        yield line
+                try:
+                    with self.location.open(mode='r') as fh:
+                        for line in fh:
+                            yield line
+                except UnicodeDecodeError:
+                    with self.location.open(mode='r', errors='replace', newline='\r') as fh:
+                        for line in fh:
+                            yield line
 
         elif mode == 'binary':
             raise NotImplementedError
 
         else:
-            raise ValueError("mode most be ascii or binary")
+            raise ValueError('mode {} not recognized'.format(mode))
+
+    def check_location(self, location):
+        """ TODO: Docstring
+        """
+        if str(location).startswith('http'):
+            self.is_URL = True
+            return location
+        elif not isinstance(location, Path):
+            pathobj = Path(location)
+        else:
+            pathobj = location
+
+        if pathobj.exists():
+            return pathobj
+        else:
+            raise FileNotFoundError
+
+
+    def check_file_signature(self):
+        '''
+        TODO: Docstring
+           https://en.wikipedia.org/wiki/List_of_file_signatures
+           https://en.wikipedia.org/wiki/File_format#Magic_number
+        '''
+        if not self.file_signature:
+            raise TypeError('File type has no associated file signature')
+
+        with self.location.open(mode="rb") as fd:
+            return fd.read(len(self.file_signature)) == self.file_signature
