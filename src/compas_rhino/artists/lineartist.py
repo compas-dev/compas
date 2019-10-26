eec5e774f30c61db45b0d6bd35b73da392d728d8
brgcode <brg@arch.ethz.ch>
2019-10-26 11:00:41 +0200

    clean up

--- src/compas_rhino/artists/lineartist.py
+++ src/compas_rhino/artists/lineartist.py
@@ -9,7 +9,7 @@ import compas_rhino
 from compas_rhino.artists import _PrimitiveArtist
 
 
-__all__ = ['PointArtist']
+__all__ = ['LineArtist']
 
 
 def list_like(target, value, fillvalue=None):
@@ -18,15 +18,15 @@ def list_like(target, value, fillvalue=None):
     return [u for _ , u in zip_longest(target, value, fillvalue=fillvalue)]
 
 
-class PointArtist(_PrimitiveArtist):
-    """Artist for drawing ``Point`` objects.
+class LineArtist(_PrimitiveArtist):
+    """Artist for drawing ``Line`` objects.
 
     Parameters
     ----------
-    point : :class:`compas.geometry.Point`
-        A COMPAS point.
+    line : :class:`compas.geometry.Line`
+        A COMPAS line.
     layer : str (optional)
-        The name of the layer that will contain the point.
+        The name of the layer that will contain the line.
         Default value is ``None``, in which case the current layer will be used.
 
     Examples
@@ -37,13 +37,13 @@ class PointArtist(_PrimitiveArtist):
 
     __module__ = "compas_rhino.artists"
 
-    def __init__(self, point, layer=None):
-        super(PointArtist, self).__init__(point, layer=layer)
+    def __init__(self, line, layer=None):
+        super(LineArtist, self).__init__(line, layer=layer)
         self.settings.update({
-            'color.point': (0, 0, 0)})
+            'color.line': (0, 0, 0)})
 
     def draw(self):
-        """Draw the point.
+        """Draw the line.
         
         Returns
         -------
@@ -51,24 +51,26 @@ class PointArtist(_PrimitiveArtist):
             The GUID of the created Rhino object.
 
         """
-        points = [{'pos': list(self.primitive), 'color': self.settings['color.point']}]
-        guids = compas_rhino.draw_points(points, layer=self.settings['layer'], clear=False)
+        start = list(self.primitive.start)
+        end = list(self.primitive.end)
+        lines = [{'start': start, 'end': end, 'color': self.settings['color.Line']}]
+        guids = compas_rhino.draw_lines(lines, layer=self.settings['layer'], clear=False)
         return guids
 
     @staticmethod
     def draw_collection(collection, color=None, layer=None, clear=False, group_collection=False, group_name=None):
-        """Draw a collection of points.
+        """Draw a collection of lines.
         
         Parameters
         ----------
-        collection: list of compas.geometry.Point
-            A collection of ``Point`` objects.
+        collection: list of compas.geometry.Line
+            A collection of ``Line`` objects.
         color: tuple or list of tuple (optional)
-            Color specification of the points.
-            If one RGB color is provided, it will be applied to all points.
-            If a list of RGB colors is provided, these colors are applied to the corresponding points.
+            Color specification of the lines.
+            If one RGB color is provided, it will be applied to all lines.
+            If a list of RGB colors is provided, these colors are applied to the corresponding lines.
             A list of colors should have the same length as the collection, with one color per item.
-            Default value is ``None`` in which case the default point color of the artist is used.
+            Default value is ``None`` in which case the default line color of the artist is used.
         layer: str (optional)
             The layer in which the objects of the collection should be created.
             Default is ``None``, in which case the default layer setting of the artist is used.
@@ -91,18 +93,19 @@ class PointArtist(_PrimitiveArtist):
 
         """
         layer = layer or self.settings['layer']
-        colors = list_like(collection, color, fill_value=self.settings['color.point'])
-        points = []
-        for point, color in zip(collection, colors):
-            points.append({
-                'pos': list(point),
+        colors = list_like(collection, color, fill_value=self.settings['color.Line'])
+        lines = []
+        for line, color in zip(collection, colors):
+            lines.append({
+                'start': list(line.start),
+                'end': list(line.end),
                 'color': color})
         if clear:
             if layer:
                 compas_rhino.clear_layer(layer)
             else:
                 compas_rhino.clear_current_layer()
-        guids = compas_rhino.draw_points(points, layer=layer)
+        guids = compas_rhino.draw_lines(lines, layer=layer)
         if not group_collection:
             return guids
         group = compas_rhino.rs.AddGroup(group_name)
