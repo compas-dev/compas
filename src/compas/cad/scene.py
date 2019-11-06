a352e52f65de6c75b30654157bcfcb57bb4f7cb9
Licini <leochen1990@gmail.com>
2019-11-06 16:19:12 +0100

    make scene separate object
    not inherent from Object3D

--- src/compas/cad/scene.py
+++ src/compas/cad/scene.py
@@ -1,10 +1,7 @@
-from compas.cad import Object3D
-
-
 __all__ = ["Scene"]
 
 
-class Scene(Object3D):
+class Scene(object):
 
     """
     Scence class serves as a container to store object3ds for visualization
@@ -13,18 +10,68 @@ class Scene(Object3D):
     __module__ = "compas.cad"
 
     def __init__(self):
-        super(Scene, self).__init__()
+        self._children = []
 
     @property
-    def parent(self):
-        return None
+    def children(self):
+        """get the children nodes of this scene"""
+        return self._children
+
+    def get_parents(self, _):
+        pass
+
+    def get_children(self, children_list=None):
+        """get flat list of all children nodes of this scene"""
+        if children_list is None:
+            children_list = []
+        for child in self.children:
+            children_list.append(child)
+            child.get_children(children_list)
+        return children_list
+
+    def add(self, child):
+        """add a child node under this object3d"""
+        if child == self:
+            raise ValueError("cannot add object itself")
+        if child._parent is not None:
+            child._parent.remove(child)
+        child._parent = self
+        self._children.append(child)
+
+    def remove(self, child):
+        """remove a child node under this object3d"""
+        child._parent = None
+        self._children.remove(child)
+
+    def create_artist(self, frontend=None):
+        """assign an artist to visualize this scene in a give frontend"""
+        raise NotImplementedError()
+
+    def draw(self, **kwargs):
+        """draw this all of object3Ds under this scene through assigned artist"""
+        # self.artist.draw(**kwargs)
+        raise NotImplementedError()
+
+    def redraw(self, **kwargs):
+        """update and redraw this scene through assigned artist"""
+        # self.artist.redraw(**kwargs)
+        raise NotImplementedError()
 
 
 if __name__ == "__main__":
 
-    from compas.geometry import Point
     from compas.cad import Object3D
 
     scene = Scene()
-    scene.add(Object3D(geometry=Point(0, 0, 0)))
+
+    o1 = Object3D()
+    o2 = Object3D()
+    o1.add(o2)
+    scene.add(o1)
+
     print(scene.get_children())
+    print(o2.get_parents())
+
+    scene.remove(o1)
+    print(scene.get_children())
+    print(o2.get_parents())
