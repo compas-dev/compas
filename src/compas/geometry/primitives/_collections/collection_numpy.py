571227836cf68d792f8c357823318ea09813ecce
Tom Van Mele <vanmelet@ethz.ch>
2019-11-12 09:47:08 +0100

    point collection and collection with numpy backend

---
+++ src/compas/geometry/primitives/_collections/collection_numpy.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+
+__all__ = ['CollectionNumpy']
+
+
+class CollectionNumpy(object):
+
+    __slots__ = ['_items']
+
+    def transform(self, X):
+        raise NotImplementedError
+
+    def transformed(self, X):
+        raise NotImplementedError
+
+
+# ==============================================================================
+# Main
+# ==============================================================================
+
+if __name__ == '__main__':
+    pass
