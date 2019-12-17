22258c4a9c25dfdf3bc49a3e2a6e9103ea630a45
tetov <a@tetov.se>
2019-12-17 21:42:13 +0100

    @brgcode\'s suggestions and iter_chunks

--- src/compas/files/base_reader.py
+++ src/compas/files/base_reader.py
@@ -10,6 +10,8 @@ try:
 except ImportError:
     from urllib import urlretrieve
 
+import binaryornot.check
+
 
 class BaseReader(object):
     """Base class containing file reading functions for file extension specific readers
@@ -20,8 +22,14 @@ class BaseReader(object):
         Path or URL to the file
 
     """
-    def __init__(self, location):
-        self._location = location
+
+    FILE_SIGNATURE = {
+         'content': None,
+         'offset': None,
+    }
+
+    def __init__(self, adress):
+        self._adress = adress
         self._is_binary = None
 
     @property
@@ -30,37 +38,43 @@ class BaseReader(object):
 
         Parameters
         ----------
-        location : string or Pathlib object
-            location specified either as an URL, string containing path or
+        self._adress : string or Pathlib object
+            Adress specified either as an URL, string containing path or
             Pathlib object
 
-        Raises
+        Returns
         ------
-        IOError
-            If file is not found
+        Path object
+            Path object pointing to file location
         """
-        if self.is_location_url():
-            pathobj = self.download(self._location)
+        if self.is_adress_url():
+            pathobj = self._download(self._adress)
         else:
-            if not isinstance(self._location, Path):
-                pathobj = Path(self._location)
+            if not isinstance(self._adress, Path):
+                pathobj = Path(self._adress)
             else:
-                pathobj = self._location
+                pathobj = self._adress
 
         if pathobj.exists():
             return pathobj
 
-        raise IOError('File not found.')
-
     @property
     def is_binary(self):
-        pass
+        """ Tries to determine if a file is binary nor not using the
+            binaryornot library
+
+        Returns
+        -------
+        bool
+            True if binary else false.
+        """
+        return binaryornot.check.is_binary(str(self.location))
 
     @property
     def is_valid(self):
         return NotImplementedError
 
-    def is_location_url(self):
+    def is_adress_url(self):
         """Checks if given location is a string containing an URL
 
         Returns
@@ -68,10 +82,9 @@ class BaseReader(object):
         bool
             True if recognized as an URL
         """
-        self._is_url = str(self._location).startswith('http')
-        return self._is_url
+        return str(self._adress).startswith('http')
 
-    def download(self, url):
+    def _download(self, url):
         """Downloads file and returns path to tempfle
 
         Called by property self.location
@@ -86,13 +99,12 @@ class BaseReader(object):
         location : Pathlib object
             Path to tempfile
         """
-
         location, _ = urlretrieve(url)
 
         return Path(location)
 
     def open_ascii(self):
-        """Open ascii or binary file and return file object
+        """Open ascii file and return file object
 
         Returns
         -------
@@ -100,7 +112,6 @@ class BaseReader(object):
         """
         try:
             file_object = self.location.open(mode='r')
-        # unnecessary now?
         except UnicodeDecodeError:
             file_object = self.location.open(mode='r', errors='replace', newline='\r')
         return file_object
@@ -112,11 +123,10 @@ class BaseReader(object):
         -------
         file object
         """
-
         return self.location.open(mode='rb')
 
     def iter_lines(self):
-        """Yields lines from local ascii files
+        """Yields lines from ascii file
 
         Yields
         -------
@@ -129,8 +139,8 @@ class BaseReader(object):
             for line in fo:
                 yield line.rstrip()
 
-    def iter_chunks(self, chunk_size=1024):
-        """Yields lines from local binary files
+    def iter_chunks(self, chunk_size=4096):
+        """Yields lines from binary files
 
         Parameters
         ----------
@@ -142,12 +152,14 @@ class BaseReader(object):
         bytes
             Next chunk of file
         """
-        raise NotImplementedError
+        with self.open_binary() as fo:
+            for chunk in iter(lambda: fo.read(chunk_size), b''):
+                yield chunk
 
     def read(self):
         raise NotImplementedError
 
-    def check_file_signature(self):
+    def is_file_signature_correct(self):
         """Checks wether file signature (also known as magic number) is present
         in input file.
 
@@ -158,31 +170,33 @@ class BaseReader(object):
         More information about file signatures can be found on Wikipedia[1]_
         as well as examples of file signatures[2]_
 
-        Raises
+        Returns
         ------
-        Exception
-            If file signature is specified for file format but not present
-            in file.
+        bool
+            True file signature for file type is found in file or if file type
+            lacks file signature.
 
         .. [1] https://en.wikipedia.org/wiki/List_of_file_signatures
         .. [2] https://en.wikipedia.org/wiki/File_format#Magic_number
         """
-        # TODO: Integrate with is_binary checks in file readers
-        # TODO. Look into binaryornot library to help with above
 
-        try:
-            file_signature = self.FILE_SIGNATURE['content']
-            signature_offset = self.FILE_SIGNATURE['offset']
-
-        # if file type has no associated file signature
-        except AttributeError:
+        if self.FILE_SIGNATURE['content'] is None:
             return
+
+        file_signature = self.FILE_SIGNATURE['content']
+
+        if self.FILE_SIGNATURE['offset'] is None:
+            signature_offset = 0
+        else:
+            signature_offset = self.FILE_SIGNATURE['offset']
 
         with self.location.open(mode="rb") as fd:
             fd.seek(signature_offset)
             found_signature = fd.read(len(file_signature))
 
         if isinstance(found_signature, str) and found_signature != file_signature.decode():
-            raise Exception('File not valid, import failed.')
+            return False
         elif found_signature != file_signature:
-            raise Exception('File not valid, import failed.')
+            return False
+
+        return True
--- tests/compas/files/test_base_reader.py
+++ tests/compas/files/test_base_reader.py
@@ -33,12 +33,37 @@ def first_five_lines_of_faces_obj():
     return ['# Rhino', '', 'v 0 0 0', 'v 2 0 0', 'v 4 0 0']
 
 
+@pytest.fixture
+def binary_stl_with_ascii_header():
+    return os.path.join(BASE_FOLDER, 'fixtures', 'binary-1.stl')
+
+
+@pytest.fixture
+def binary_stl():
+    return os.path.join(BASE_FOLDER, 'fixtures', 'binary-2.stl')
+
+
+@pytest.fixture
+def ascii_stl():
+    return os.path.join(BASE_FOLDER, 'fixtures', 'ascii.stl')
+
+
+@pytest.fixture
+def pathobj_boxes_obj():
+    return Path.cwd().joinpath('data/boxes.obj')
+
+
+@pytest.fixture
+def binary_ply():
+    return os.path.join(BASE_FOLDER, 'fixtures', 'triangle_binary.ply')
+
+
 def test_iterate_over_ascii_file(ascii_ply):
     ply = BaseReader(ascii_ply)
     assert len(list(ply.iter_lines())) == 23598
 
 
-def test_iterate_over_downloaded_ascii_file(url_to_faces_obj, first_five_lines_of_faces_obj):
+def test_iterate_over_downloaded_file(url_to_faces_obj, first_five_lines_of_faces_obj):
     url_faces = BaseReader(url_to_faces_obj)
     five_lines_from_url = []
 
@@ -50,13 +75,54 @@ def test_iterate_over_downloaded_ascii_file(url_to_faces_obj, first_five_lines_o
     assert five_lines_from_url == first_five_lines_of_faces_obj
 
 
-def test_file_signature_check():
-    pass
+def test_iterate_over_binary_file(binary_stl):
+    binary_file = BaseReader(binary_stl)
+
+    two_chunks = []
+
+    for i, chunk in enumerate(binary_file.iter_chunks(chunk_size=80)):
+        two_chunks.append(chunk)
+        if i >= 1:
+            break
+
+    assert two_chunks[0] == b'dilos link_6                                                                    '
+    assert two_chunks[1][:5] == b'\x9a%\x00\x00\x8b'
 
 
-def test_iterate_over_binary_file():
-    pass
+def test_location_from_string_adress(ascii_ply):
+    file = BaseReader(ascii_ply)
+    assert isinstance(file.location, Path)
 
 
-def test_iterate_over_downloaded_binary_file():
-    pass
+def test_location_from_url(url_to_faces_obj):
+    file = BaseReader(url_to_faces_obj)
+    assert isinstance(file.location, Path)
+
+
+def test_location_from_pathobj(pathobj_boxes_obj):
+    file = BaseReader(pathobj_boxes_obj)
+    assert file.location == pathobj_boxes_obj
+
+
+def test_file_signature_check(binary_ply):
+    ply = BaseReader(binary_ply)
+
+    # Default offset is zero
+    ply.FILE_SIGNATURE['content'] = b'ply'
+
+    assert ply.is_file_signature_correct() is True
+
+
+def test_is_binary_positive(binary_stl):
+    stl = BaseReader(binary_stl)
+    assert stl.is_binary is True
+
+
+def test_is_binary_negative(ascii_stl):
+    stl = BaseReader(ascii_stl)
+    assert stl.is_binary is False
+
+
+def test_is_binary_binary_w_ascii_header(binary_stl_with_ascii_header):
+    stl = BaseReader(binary_stl_with_ascii_header)
+    assert stl.is_binary is True
