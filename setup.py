efd8d8f25ca24b9f0dfb407122fce3c969a08f37
brgcode <brg@arch.ethz.ch>
2020-01-06 21:32:13 +0100

    setting version back

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ optional_requirements = {
 
 setup(
     name='COMPAS',
-    version='0.13.3',
+    version='0.13.1',
     description='The COMPAS framework',
     long_description=long_description,
     long_description_content_type='text/markdown',
