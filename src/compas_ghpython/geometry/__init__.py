09d397e1f0076c5ffa2afd81f75a0ea42b89b801
tetov <anton@tetov.se>
2020-01-16 16:34:22 +0100

    autosummary for modules, numpy docstring formatting

--- src/compas_ghpython/geometry/__init__.py
+++ src/compas_ghpython/geometry/__init__.py
@@ -5,6 +5,14 @@ compas_ghpython.geometry
 
 .. currentmodule:: compas_ghpython.geometry
 
+.. autosummary::
+    :toctree: generated/
+
+    xform_from_transformation
+    xform_from_transformation_matrix
+    xtransform
+    xtransformed
+
 """
 from __future__ import absolute_import
 
--- src/compas_ghpython/helpers/__init__.py
+++ src/compas_ghpython/helpers/__init__.py
@@ -5,6 +5,13 @@ compas_ghpython.helpers
 
 .. currentmodule:: compas_ghpython.helpers
 
+.. autosummary::
+    :toctree: generated/
+    mesh_draw
+    mesh_draw_vertices
+    mesh_draw_edges
+    mesh_draw_faces
+
 """
 from __future__ import absolute_import
 
