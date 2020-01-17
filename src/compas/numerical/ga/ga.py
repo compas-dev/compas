b0ead8c48ebadab4dd942c558b9c7416c90c39b3
brgcode <brg@arch.ethz.ch>
2020-01-17 11:36:00 +0100

    cleaning up numerical

--- src/compas/numerical/ga/ga.py
+++ src/compas/numerical/ga/ga.py
@@ -12,25 +12,15 @@ __all__ = ['ga']
 
 
 TPL = """
-================================================================================
 GA summary
-================================================================================
-
+==========
 - fitness function name: {}
-
 - fitnes function type : {}
-
 - number of generations : {}
-
 - number of individuals : {}
-
 - number of variables : {}
-
 - optimal individual : {}
-
 - optimal fitness value : {}
-
-================================================================================
 """
 
 
@@ -125,59 +115,7 @@ def ga(fit_function,
 
     Examples
     --------
-    .. code-block:: python
-
-        import os
-        import compas
-        from compas.numerical.solvers.ga import ga
-
-        def foo(X):
-            fit = sum(X)
-            return fit
-
-        fit_function = foo
-        fit_type = 'min'
-        num_var = 10
-        boundaries = [(0, 1)] * num_var
-        num_bin_dig  = [8] * num_var
-        output_path = os.path.join(compas.TEMP, 'ga_out/')
-
-        if not os.path.exists(output_path):
-            os.makedirs(output_path)
-
-        ga = ga(fit_function,
-                fit_type,
-                num_var,
-                boundaries,
-                num_gen=100,
-                num_pop=100,
-                num_elite=40,
-                num_bin_dig=num_bin_dig,
-                output_path=output_path,
-                min_fit=0.01)
-
-    .. code-block:: none
-
-        ================================================================================
-        GA summary
-        ================================================================================
-
-        - fitness function name: foo
-
-        - fitnes function type : min
-
-        - number of generations : 100
-
-        - number of individuals : 100
-
-        - number of variables : 10
-
-        - optimal individual : 31
-
-        - optimal fitness value : 0.0705882352941
-
-        ================================================================================
-
+    >>>
     """
     ga_ = GA()
 
@@ -878,48 +816,4 @@ class GA(object):
 # ==============================================================================
 
 if __name__ == '__main__':
-
-    import os
-    import compas
-    from compas_plotters.gaplotter import visualize_evolution
-    from math import cos
-    from math import pi
-
-    def rastrigin(X):
-        a = 10
-        fit = a * 2 + sum([(x ** 2 - a * cos(2 * pi * x)) for x in X])
-        return fit
-
-    def foo(X):
-        fit = sum(X)
-        # print ('fit', fit, X)
-        return fit
-
-    fit_function = rastrigin
-    # fit_function = foo
-    fit_type = 'min'
-    num_var = 2
-    boundaries = [(-5.12, 5.12)] * num_var
-    # boundaries = [(1, 5)] * num_var
-
-    num_bin_dig = [40] * num_var
-    output_path = os.path.join(compas.TEMP, 'ga_out/')
-    min_fit = 0.000001  # num_var * boundaries[0][0]
-
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
-
-    ga_ = ga(fit_function,
-             fit_type,
-             num_var,
-             boundaries,
-             num_gen=100,
-             num_pop=100,
-             num_elite=20,
-             num_bin_dig=num_bin_dig,
-             output_path=output_path,
-             min_fit=min_fit,
-             mutation_probability=0.03,
-             n_cross=2)
-
-    visualize_evolution(ga_.output_path)
+    pass
