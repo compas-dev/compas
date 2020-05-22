e55675406ca6ccf1978b923a47afc9b4473de17d
Victor Leung <yck011522@gmail.com>
2020-05-22 14:11:24 +0200

    add sphere scaling test file

---
+++ tests/compas/geometry/test_sphere.py
@@ -0,0 +1,12 @@
+from compas.geometry import Point
+from compas.geometry import Scale
+from compas.geometry import Sphere
+
+
+def test_sphere_scaling():
+    s = Sphere(Point(1, 1, 1), 10)
+    s.transform(Scale([2, 3, 4]))
+    assert s.point.x == 2
+    assert s.point.y == 3
+    assert s.point.z == 4
+    assert s.radius == 40
