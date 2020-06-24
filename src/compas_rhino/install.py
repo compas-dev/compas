a4e2b7a5ea6123534c09c91d274e090a5433bf8e
Tom Van Mele <vanmelet@ethz.ch>
2020-06-24 09:48:02 +0200

    add 7.0 to argparse options

--- src/compas_rhino/install.py
+++ src/compas_rhino/install.py
@@ -151,7 +151,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-v', '--version', choices=['5.0', '6.0'], default='6.0', help="The version of Rhino to install the packages in.")
+    parser.add_argument('-v', '--version', choices=['5.0', '6.0', '7.0'], default='6.0', help="The version of Rhino to install the packages in.")
     parser.add_argument('-p', '--packages', nargs='+', help="The packages to install.")
 
     args = parser.parse_args()
--- src/compas_rhino/uninstall.py
+++ src/compas_rhino/uninstall.py
@@ -38,7 +38,7 @@ def uninstall(version=None, packages=None):
         $ python -m compas_rhino.uninstall -v 6.0
 
     """
-    if version not in ('5.0', '6.0'):
+    if version not in ('5.0', '6.0', '7.0'):
         version = '6.0'
 
     print('Uninstalling COMPAS packages from Rhino {0} IronPython lib:'.format(version))
@@ -109,7 +109,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-v', '--version', choices=['5.0', '6.0'], default='6.0', help="The version of Rhino to install the packages in.")
+    parser.add_argument('-v', '--version', choices=['5.0', '6.0', '7.0'], default='6.0', help="The version of Rhino to install the packages in.")
     parser.add_argument('-p', '--packages', nargs='+', help="The packages to uninstall.")
 
     args = parser.parse_args()
