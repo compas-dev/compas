25ad0b074d935f7ae716fc780190a51654c2a85e
Tom Van Mele <vanmelet@ethz.ch>
2020-08-21 15:27:44 +0200

    rebased artists

---
+++ src/compas_blender/artists/_artist.py
@@ -0,0 +1,90 @@
+from __future__ import annotations
+
+import bpy
+import abc
+import compas
+import compas_blender
+
+from typing import Any, Type, List
+
+
+__all__ = ['BaseArtist']
+
+
+_ITEM_ARTIST = {}
+
+
+class BaseArtist(abc.ABC):
+    """Base class for all Blender artists.
+
+    Attributes
+    ----------
+    objects : list
+        A list of Blender objects (unique object names) created by the artist.
+
+    """
+
+    def __init__(self):
+        self.objects = []
+
+    @staticmethod
+    def register(item_type: Type[compas.base.Base], artist_type: Type[BaseArtist]):
+        """Register a type of COMPAS object with a Blender artist.
+
+        Parameters
+        ----------
+        item_type : :class:`compas.base.Base`
+        artist_type : :class:`compas_blender.artists.BaseArtist`
+
+        """
+        _ITEM_ARTIST[item_type] = artist_type
+
+    @staticmethod
+    def build(item: compas.base.Base, **kwargs: Any) -> BaseArtist:
+        """Build an artist corresponding to the item type.
+
+        Parameters
+        ----------
+        kwargs : dict, optional
+            The keyword arguments (kwargs) collected in a dict.
+            For relevant options, see the parameter lists of the matching artist type.
+
+        Returns
+        -------
+        :class:`compas_blender.artists.BaseArtist`
+            An artist of the type matching the provided item according to an item-artist map.
+            The map is created by registering item-artist type pairs using ``~BaseArtist.register``.
+        """
+        artist_type = _ITEM_ARTIST[type(item)]
+        artist = artist_type(item, **kwargs)
+        return artist
+
+    @abc.abstractmethod
+    def draw(self):
+        """Draw the item."""
+        pass
+
+    @staticmethod
+    def draw_collection(collection: List[compas.base.Base]):
+        """Draw a collection of items."""
+        raise NotImplementedError
+
+    def redraw(self):
+        """Trigger a redraw."""
+        bpy.ops.wm.redraw_timer(type='DRAW_WIN_SWAP', iterations=1)
+
+    def clear(self):
+        """Delete all objects created by the artist."""
+        if not self.objects:
+            return
+        compas_blender.delete_objects(self.objects)
+        self.objects = []
+
+
+# ==============================================================================
+# Main
+# ==============================================================================
+
+if __name__ == "__main__":
+
+    pass
--- src/compas_blender/artists/networkartist.py
+++ src/compas_blender/artists/networkartist.py
@@ -1,6 +1,9 @@
-from compas_blender.artists import Artist
-from compas_blender.artists.mixins import VertexArtist
-from compas_blender.artists.mixins import EdgeArtist
+from __future__ import annotations
+
+import compas_blender
+
+from compas_blender.artists._artist import BaseArtist
+from compas.utilities import color_to_colordict as colordict
 
 
 __all__ = [
@@ -8,30 +11,186 @@ __all__ = [
 ]
 
 
-class NetworkArtist(EdgeArtist, VertexArtist, Artist):
+class NetworkArtist(BaseArtist):
+    """Artist for COMPAS network objects.
 
-    def __init__(self, network, layer=None):
-        super().__init__(layer=layer)
+    Parameters
+    ----------
+    network : :class:`compas.datastructures.Network`
+        A COMPAS network.
+    settings : dict, optional
+        A dict with custom visualisation settings.
+
+    Attributes
+    ----------
+    network : :class:`compas.datastructures.Network`
+        The COMPAS network associated with the artist.
+    settings : dict
+        Default settings for color, scale, tolerance, ...
+
+    """
+
+    def __init__(self, network, settings=None):
+        super().__init__()
+        self._nodecollection = None
+        self._edgecollection = None
+        self._pathcollection = None
+        self._object_node = {}
+        self._object_edge = {}
+        self._object_path = {}
         self.network = network
-        self.defaults.update({
-            'color.vertex': [255, 255, 255],
-            'color.edge':   [0, 0, 0],
-        })
+        self.settings = {
+            'color.nodes': (255, 255, 255),
+            'color.edges': (0, 0, 0),
+            'show.nodes': True,
+            'show.edges': True,
+            'show.nodelabels': False,
+            'show.edgelabels': False
+        }
+        if settings:
+            self.settings.update(settings)
 
     @property
-    def network(self):
-        return self.datastructure
+    def nodecollection(self):
+        if not self._nodecollection:
+            self._nodecollection = compas_blender.create_collections_from_path('Network::Nodes')[1]
+        return self._nodecollection
 
-    @network.setter
-    def network(self, network):
-        self.datastructure = network
+    @property
+    def edgecollection(self):
+        if not self._edgecollection:
+            self._edgecollection = compas_blender.create_collections_from_path('Network::Edges')[1]
+        return self._edgecollection
 
-    def draw(self):
-        raise NotImplementedError
+    @property
+    def pathcollection(self):
+        if not self._pathcollection:
+            self._pathcollection = compas_blender.create_collections_from_path('Network::Paths')[1]
+        return self._pathcollection
+
+    @property
+    def object_node(self):
+        if not self._object_node:
+            self._object_node = {}
+        return self._object_node
+
+    @object_node.setter
+    def object_node(self, values):
+        self._object_node = dict(values)
+
+    @property
+    def object_edge(self):
+        if not self._object_edge:
+            self._object_edge = {}
+        return self._object_edge
+
+    @object_edge.setter
+    def object_edge(self, values):
+        self._object_edge = dict(values)
+
+    @property
+    def object_path(self):
+        if not self._object_path:
+            self._object_path = {}
+        return self._object_path
+
+    @object_path.setter
+    def object_path(self, values):
+        self._object_path = dict(values)
 
     def clear(self):
-        self.clear_vertices()
-        self.clear_edges()
+        objects = list(self.object_node.keys())
+        objects += list(self.object_edge.keys())
+        objects += list(self.object_path.keys())
+        compas_blender.delete_objects(objects)
+        self._object_node = {}
+        self._object_edge = {}
+        self._object_path = {}
+
+    def draw(self, settings=None):
+        """Draw the network.
+
+        Parameters
+        ----------
+        settings : dict, optional
+            Dictionary of visualisation settings that will be merged with the settings of the artist.
+
+        Returns
+        -------
+        list of :class:`bpy.types.Object`
+            The created Blender objects.
+
+        """
+        self.clear()
+        if not settings:
+            settings = {}
+        self.settings.update(settings)
+        if self.settings['show.nodes']:
+            self.draw_nodes()
+        if self.settings['show.edges']:
+            self.draw_edges()
+        return self.objects
+
+    def draw_nodes(self, nodes=None, color=None):
+        """Draw a selection of nodes.
+
+        Parameters
+        ----------
+        nodes : list of int, optional
+            A list of node identifiers.
+            Default is ``None``, in which case all nodes are drawn.
+        color : rgb-tuple or dict of rgb-tuples
+            The color specififcation for the nodes.
+
+        Returns
+        -------
+        list of :class:`bpy.types.Object`
+
+        """
+        nodes = nodes or list(self.network.nodes())
+        node_color = colordict(color, nodes, default=self.settings['color.nodes'], colorformat='rgb', normalize=False)
+        points = []
+        for node in nodes:
+            points.append({
+                'pos': self.network.node_coordinates(node),
+                'name': "{}.node.{}".format(self.network.name, node),
+                'color': node_color[node],
+                'radius': 0.05})
+
+        objects = compas_blender.draw_points(points, self.nodecollection)
+        self.object_node = zip(objects, nodes)
+        return objects
+
+    def draw_edges(self, edges=None, color=None):
+        """Draw a selection of edges.
+
+        Parameters
+        ----------
+        edges : list
+            A list of edge keys (as uv pairs) identifying which edges to draw.
+            The default is ``None``, in which case all edges are drawn.
+        color : rgb-tuple or dict of rgb-tuples
+            The color specififcation for the edges.
+
+        Returns
+        -------
+        list of :class:`bpy.types.Object`
+
+        """
+        edges = edges or list(self.network.edges())
+        edge_color = colordict(color, edges, default=self.settings['color.edges'], colorformat='rgb', normalize=False)
+        lines = []
+        for edge in edges:
+            lines.append({
+                'start': self.network.node_coordinates(edge[0]),
+                'end': self.network.node_coordinates(edge[1]),
+                'color': edge_color[edge],
+                'name': "{}.edge.{}-{}".format(self.network.name, *edge),
+                'width': 0.02})
+
+        objects = compas_blender.draw_lines(lines, self.edgecollection)
+        self.object_edge = zip(objects, edges)
+        return objects
 
 
 # ==============================================================================
@@ -39,5 +198,4 @@ class NetworkArtist(EdgeArtist, VertexArtist, Artist):
 # ==============================================================================
 
 if __name__ == "__main__":
-
     pass
