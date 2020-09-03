f901b56f87a691efd195ff7a86277c0d64cc4a63
duchaoyu <duch@student.ethz.ch>
2020-09-03 15:03:10 +0200

    - lint

--- src/compas_rhino/objects/volmeshobject.py
+++ src/compas_rhino/objects/volmeshobject.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import compas_rhino
 from compas_rhino.objects._object import BaseObject
 
 
