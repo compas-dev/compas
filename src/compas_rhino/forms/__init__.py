6174f3b9132d5b6559955cbbc33b2e4c74d9f0c6
Tom Van Mele <vanmelet@ethz.ch>
2020-09-07 23:44:05 +0200

    check for mono

--- src/compas_rhino/forms/__init__.py
+++ src/compas_rhino/forms/__init__.py
@@ -33,21 +33,22 @@ Base Classes
 """
 from __future__ import absolute_import
 
+import compas
+
 from .base import BaseForm  # noqa: F401 E402
 
 from .browser import BrowserForm  # noqa: F401 E402
 from .chart import ChartForm  # noqa: F401 E402
 from .slider import SliderForm  # noqa: F401 E402
 
-try:
+if compas.MONO:
     from .etoforms import ImageForm  # noqa: F401 E402
     from .etoforms import TextForm  # noqa: F401 E402
-except ImportError:
-    from .image import ImageForm  # noqa: F401 E402
-    from .text import TextForm  # noqa: F401 E402
-else:
     from .etoforms import PropertyListForm  # noqa: F401 E402
     from .etoforms import SettingsForm  # noqa: F401 E402
+else:
+    from .image import ImageForm  # noqa: F401 E402
+    from .text import TextForm  # noqa: F401 E402
 
 
 __all__ = [name for name in dir() if not name.startswith('_')]
