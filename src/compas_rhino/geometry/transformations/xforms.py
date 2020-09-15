0b0527d98e0c49e7fd40835e089a7cad281d7097
Gonzalo Casas <casas@arch.ethz.ch>
2020-09-15 19:51:30 +0200

    lint

--- src/compas_rhino/geometry/transformations/xforms.py
+++ src/compas_rhino/geometry/transformations/xforms.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import compas
+
 if compas.RHINO:
     from Rhino.Geometry import Transform
 
