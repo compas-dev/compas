a29b7f54b56f60ea4540590755efe011436a1258
Tom Van Mele <vanmelet@ethz.ch>
2020-09-17 11:06:28 +0200

    add intersections pluggable

--- src/compas/geometry/intersections/__init__.py
+++ src/compas/geometry/intersections/__init__.py
@@ -2,6 +2,28 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from compas.plugins import pluggable
 from .intersections import *  # noqa: F401 F403
+
+
+@pluggable(category="intersections")
+def intersection_mesh_mesh(A, B):
+    """Compute the intersection of tow meshes.
+
+    Parameters
+    ----------
+    A : tuple of vertices and faces
+        Mesh A.
+    B : tuple of vertices and faces
+        Mesh B.
+
+    Returns
+    -------
+    list of arrays of points
+        The intersection polylines as arrays of points.
+
+    """
+    pass
+
 
 __all__ = [name for name in dir() if not name.startswith('_')]
