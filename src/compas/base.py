56897783b9e2a2bb31a1decdff3f42d8c6c34dbe
Tom Van Mele <vanmelet@ethz.ch>
2020-09-27 00:53:30 +0200

    dtype should be a property

--- src/compas/base.py
+++ src/compas/base.py
@@ -66,6 +66,7 @@ class Base(ABC):
     def name(self, name):
         self._name = name
 
+    @property
     def dtype(self):
         """str :
         The type of the object in the form of a "2-level" import and a class name.
