c475a9465805f4cdf5417769d78f612af5bdf8c6
Tom Van Mele <vanmelet@ethz.ch>
2020-09-27 00:53:12 +0200

    module names

--- src/compas/datastructures/network/__init__.py
+++ src/compas/datastructures/network/__init__.py
@@ -9,7 +9,7 @@ from .combinatorics import *  # noqa: F401 F403
 from .complementarity import *  # noqa: F401 F403
 from .duality import *  # noqa: F401 F403
 from .explode import *  # noqa: F401 F403
-from .planarity_ import *  # noqa: F401 F403
+from .planarity import *  # noqa: F401 F403
 from .transformations import *  # noqa: F401 F403
 
 
