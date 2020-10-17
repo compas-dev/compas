3126ead39900f3cefa77e113c1ef12d78de29921
Tom Van Mele <vanmelet@ethz.ch>
2020-10-17 08:22:22 +0200

    converted to_vertices_and_faces to abstract method

--- src/compas/geometry/shapes/_shape.py
+++ src/compas/geometry/shapes/_shape.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
+import abc
 from compas.geometry import Primitive
 
 
@@ -14,8 +15,9 @@ class Shape(Primitive):
     def __init__(self):
         super(Shape, self).__init__()
 
+    @abc.abstractmethod
     def to_vertices_and_faces(self, **kwargs):
-        raise NotImplementedError
+        pass
 
 
 # ==============================================================================
