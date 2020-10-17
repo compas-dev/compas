a86544ee2dd620bf17a0affdead01e71dcda59ee
Tom Van Mele <vanmelet@ethz.ch>
2020-10-17 08:23:41 +0200

    stuffs

--- src/compas/geometry/primitives/_primitive.py
+++ src/compas/geometry/primitives/_primitive.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-
 import json
 from copy import deepcopy
 from compas.base import Base
