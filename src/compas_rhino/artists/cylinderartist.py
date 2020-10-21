44e54e0b1c4169d24b143ed4a2274b81109fa646
Tom Van Mele <vanmelet@ethz.ch>
2020-10-21 09:56:56 +0200

    add option to not show vertices

--- src/compas_rhino/artists/cylinderartist.py
+++ src/compas_rhino/artists/cylinderartist.py
@@ -20,7 +20,7 @@ class CylinderArtist(ShapeArtist):
 
     """
 
-    def draw(self, u=10):
+    def draw(self, u=10, show_vertices=True):
         """Draw the cylinder associated with the artist.
 
         Parameters
@@ -36,9 +36,11 @@ class CylinderArtist(ShapeArtist):
         """
         vertices, faces = self.shape.to_vertices_and_faces(u=u)
         vertices = [list(vertex) for vertex in vertices]
-        points = [{'pos': point, 'color': self.color} for point in vertices]
+        guids = []
+        if show_vertices:
+            points = [{'pos': point, 'color': self.color} for point in vertices]
+            guids += compas_rhino.draw_points(points, layer=self.layer, clear=False, redraw=False)
         polygons = [{'points': [vertices[index] for index in face], 'color': self.color} for face in faces]
-        guids = compas_rhino.draw_points(points, layer=self.layer, clear=False, redraw=False)
         guids += compas_rhino.draw_faces(polygons, layer=self.layer, clear=False, redraw=False)
         self._guids = guids
         return guids
