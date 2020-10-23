bb6e0f42a7e09c0f9e00cc8b886df2b26704a4f3
Tom Van Mele <vanmelet@ethz.ch>
2020-10-23 17:30:33 +0200

    lint

--- src/compas_rhino/artists/boxartist.py
+++ src/compas_rhino/artists/boxartist.py
@@ -2,13 +2,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
-try:
-    basestring
-except NameError:
-    basestring = str
-
 import compas_rhino
-from compas.utilities import iterable_like
 from ._shapeartist import ShapeArtist
 
 
