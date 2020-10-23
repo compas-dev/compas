a9eab15327698da4eba84023722b36b3c920cf85
Tom Van Mele <vanmelet@ethz.ch>
2020-10-23 13:40:19 +0200

    update capsule artist draw function

--- src/compas_rhino/artists/capsuleartist.py
+++ src/compas_rhino/artists/capsuleartist.py
@@ -18,9 +18,32 @@ class CapsuleArtist(ShapeArtist):
     -----
     See :class:`compas_rhino.artists.ShapeArtist` for all other parameters.
 
+    Examples
+    --------
+    .. code-block:: python
+
+        import random
+        from compas.geometry import Pointcloud
+        from compas.geometry import Capsule
+        from compas.geometry import Translation
+        from compas.utilities import i_to_rgb
+
+        import compas_rhino
+        from compas_rhino.artists import CapsuleArtist
+
+        pcl = Pointcloud.from_bounds(10, 10, 10, 100)
+        tpl = Capsule([[0, 0, 0], [0.8, 0, 0]], 0.15)
+
+        compas_rhino.clear_layer("Test::CapsuleArtist")
+
+        for point in pcl.points[:len(pcl) // 2]:
+            capsule = tpl.transformed(Translation.from_vector(point))
+            artist = CapsuleArtist(capsule, color=i_to_rgb(random.random()), layer="Test::CapsuleArtist")
+            artist.draw()
+
     """
 
-    def draw(self, u=10, v=10):
+    def draw(self, u=10, v=10, show_vertices=False, show_edges=False, show_faces=True, join_faces=True):
         """Draw the capsule associated with the artist.
 
         Parameters
@@ -31,6 +54,14 @@ class CapsuleArtist(ShapeArtist):
         v : int, optional
             Number of faces in the "v" direction.
             Default is ``10``.
+        show_vertices : bool, optional
+            Default is ``False``.
+        show_edges : bool, optional
+            Default is ``False``.
+        show_faces : bool, optional
+            Default is ``True``.
+        join_faces : bool, optional
+            Default is ``True``.
 
         Returns
         -------
@@ -39,10 +70,19 @@ class CapsuleArtist(ShapeArtist):
         """
         vertices, faces = self.shape.to_vertices_and_faces(u=u, v=v)
         vertices = [list(vertex) for vertex in vertices]
-        points = [{'pos': point, 'color': self.color} for point in vertices]
-        polygons = [{'points': [vertices[index] for index in face], 'color': self.color} for face in faces]
-        guids = compas_rhino.draw_points(points, layer=self.layer, clear=False, redraw=False)
-        guids += compas_rhino.draw_faces(polygons, layer=self.layer, clear=False, redraw=False)
+        guids = []
+        if show_vertices:
+            points = [{'pos': point, 'color': self.color} for point in vertices]
+            guids += compas_rhino.draw_points(points, layer=self.layer, clear=False, redraw=False)
+        if show_edges:
+            raise NotImplementedError
+        if show_faces:
+            if join_faces:
+                guid = compas_rhino.draw_mesh(vertices, faces, layer=self.layer, name=self.name, color=self.color, disjoint=True)
+                guids.append(guid)
+            else:
+                polygons = [{'points': [vertices[index] for index in face], 'color': self.color} for face in faces]
+                guids += compas_rhino.draw_faces(polygons, layer=self.layer, clear=False, redraw=False)
         self._guids = guids
         return guids
 
