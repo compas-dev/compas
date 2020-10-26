4cd9e7fdb2f0024f36c5c1c4ce19d2ecd6b17fe1
Tom Van Mele <vanmelet@ethz.ch>
2020-10-26 09:05:22 +0100

    start of scene implementation

---
+++ src/compas/scene/object.py
@@ -0,0 +1,154 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import abc
+from uuid import uuid4
+from .artist import BaseArtist
+
+ABC = abc.ABCMeta('ABC', (object,), {'__slots__': ()})
+
+
+__all__ = ['BaseObject']
+
+
+_ITEM_OBJECT = {}
+
+
+class BaseObject(ABC):
+    """Abstract base class for COMPAS Rhino objects.
+
+    Parameters
+    ----------
+    item : {:class:`compas.geometry.Geometry`, :class:`compas.datastructures.Datastructure`}
+        A COMPAS geometry object or data structure.
+    scene : :class:`compas.scenes.Scene`, optional
+        A scene object.
+    name : str, optional
+        The name of the object.
+    visible : bool, optional
+        Toggle for the visibility of the object.
+    settings : dict, optional
+        A dictionary of settings.
+
+    Attributes
+    ----------
+    item : {:class:`compas.geometry.Geometry`, :class:`compas.datastructures.Datastructure`}
+        A COMPAS geometry object or data structure.
+    scene : :class:`compas.scenes.Scene`
+        A scene object.
+    artist : :class:`compas_rhino.artists.Artist`
+        The artist matching the type of ``item``.
+    name : str
+        The name of the object.
+        This is an alias for the name of ``item``.
+    layer : str
+        The layer for drawing.
+        This is an alias for the layer of ``artist``.
+    visible : bool
+        Toggle for the visibility of the object in the scene.
+    settings : dict
+        A dictionary of settings related to visualisation and interaction.
+
+    """
+
+    def __init__(self, item, scene=None, name=None, visible=True, settings=None):
+        super(BaseObject, self).__init__()
+        self._item = None
+        self._id = None
+        self._scene = None
+        self._artist = None
+        self.scene = scene
+        self.item = item
+        self.name = name
+        self.visible = visible
+        self.settings = settings or {}
+
+    # ==========================================================================
+    # Properties
+    # ==========================================================================
+
+    @property
+    def scene(self):
+        return self._scene
+
+    @scene.setter
+    def scene(self, scene):
+        self._scene = scene
+
+    @property
+    def item(self):
+        return self._item
+
+    @item.setter
+    def item(self, item):
+        self._item = item
+        self._artist = BaseArtist.build(item)
+
+    @property
+    def artist(self):
+        return self._artist
+
+    @property
+    def id(self):
+        if not self._id:
+            self._id = uuid4()
+        return self._id
+
+    @property
+    def name(self):
+        return self.item.name
+
+    @name.setter
+    def name(self, name):
+        self.item.name = name
+
+    # ==========================================================================
+    # Methods
+    # ==========================================================================
+
+    @staticmethod
+    def register(item_type, object_type):
+        _ITEM_OBJECT[item_type] = object_type
+
+    @staticmethod
+    def registered_object_types():
+        return [_ITEM_OBJECT[item_type] for item_type in _ITEM_OBJECT]
+
+    @staticmethod
+    def build(item, **kwargs):
+        object_type = _ITEM_OBJECT[type(item)]
+        return object_type(item, **kwargs)
+
+    @abc.abstractmethod
+    def clear(self):
+        """Clear all previously created Rhino objects."""
+        pass
+
+    @abc.abstractmethod
+    def draw(self):
+        """Draw the object representing the item."""
+        pass
+
+    @abc.abstractmethod
+    def select(self):
+        """Select the object representing the item."""
+        pass
+
+    @abc.abstractmethod
+    def modify(self):
+        """Modify the item represented by the object."""
+        pass
+
+    @abc.abstractmethod
+    def move(self):
+        """Move the item represented by the object."""
+        pass
+
+
+# ============================================================================
+# Main
+# ============================================================================
+
+if __name__ == "__main__":
+    pass
