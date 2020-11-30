66cd5834581aa63f29c00165910cd8d268e1a2cf
Tom Van Mele <vanmelet@ethz.ch>
2020-11-30 16:13:13 +0100

    lint

--- src/compas/datastructures/mesh/core/halfedge.py
+++ src/compas/datastructures/mesh/core/halfedge.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from collections import OrderedDict
+# from collections import OrderedDict
 from ast import literal_eval
 from random import sample
 from random import choice
