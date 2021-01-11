d17074d701531acb069b9e70987fb9a02ad6f21a
Xingxin HE <xingxin.he@mail.polimi.it>
2021-01-11 23:54:57 +0100

    Revise trimesh_samplepoints_numpy

--- src/compas/datastructures/mesh/__init__.py
+++ src/compas/datastructures/mesh/__init__.py
@@ -35,6 +35,7 @@ if not IPY:
     from .pull_numpy import *  # noqa: F401 F403
     from .smoothing_numpy import *  # noqa: F401 F403
     from .transformations_numpy import *  # noqa: F401 F403
+    from .trimesh_samplepoints_numpy import *  # noqa: F401 F403
 
 
 __all__ = [name for name in dir() if not name.startswith('_')]
--- src/compas/datastructures/mesh/trimesh_samplepoints.py
+++ src/compas/datastructures/mesh/trimesh_samplepoints.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 
 from numpy import array
+from numpy import ndarray
 from numpy.random import choice
 from numpy.random import rand
 from numpy import sqrt
@@ -12,14 +13,16 @@ from numpy import cross
 from numpy.linalg import norm
 from numpy import clip
 from numpy import finfo
+from typing import Tuple, Union
+from compas.datastructures import Mesh
 
 
 __all__ = [
-    'trimesh_samplepoints',
+    'trimesh_samplepoints_numpy',
 ]
 
 
-def trimesh_samplepoints(mesh, num_points: int = 1000, return_normals: bool = False):
+def trimesh_samplepoints_numpy(mesh: Mesh, num_points: int = 1000, return_normals: bool = False) -> Union[ndarray, Tuple[ndarray, ndarray]]:
     """Compute sample points on a triangle mesh surface
 
     Parameters
@@ -34,22 +37,26 @@ def trimesh_samplepoints(mesh, num_points: int = 1000, return_normals: bool = Fa
     Returns
     -------
     samples_points(numpy.ndarray)
-        A numpy array representing sampled points with dim = [num_points, 3]
+        A numpy ndarray representing sampled points with dim = [num_points, 3]
     (if True) samples_points_normals(numpy.ndarray)
-        A numpy array representing the normal vector of sampled points  with dim = [num_points, 3]
+        A numpy ndarray representing the normal vector of sampled points  with dim = [num_points, 3]
 
     Examples
     --------
+    Make a triangle mesh.
     >>> hypar = Mesh.from_obj(compas.get('hypar.obj'))
-    >>> hypar.is_trimesh() # First check if the input mesh is triangle mesh
+    >>> hypar.is_trimesh()
     False
     >>> hypar.quads_to_triangles()
-    >>> samples_pts, pts_normals = trimesh_samplepoints(hypar, 1000, True)
+
+    Compute sample points.
+    >>> samples_pts, pts_normals = trimesh_samplepoints_numpy(hypar, 1000, True)
     >>> # the x,y,z of sample points would be the following
     >>> x, y, z = samples_pts[:,0], samples_pts[:,1], samples_pts[:,2]
     >>> # the sample points added normal vector would be the following
     >>> X, Y, Z = x + pts_normals[:,0] , y + pts_normals[:,1] , z + pts_normals[:,2]
-    >>> # You can import matplotlib for visualization
+
+    Visualize with Matplotlib.
     >>> import matplotlib.pyplot as plt
     >>> from mpl_toolkits.mplot3d import Axes3D
     >>> fig = plt.figure(figsize=(5, 5))
@@ -82,21 +89,26 @@ def trimesh_samplepoints(mesh, num_points: int = 1000, return_normals: bool = Fa
     if not mesh.is_valid():
         raise ValueError("Mesh is invalid.")
 
-    # (1)  acquire the area of all mesh faces
-    area_list = array([mesh.face_area(face) for face in mesh.faces()])
+    # (1)  Prepare data for computing
+    key_index = mesh.key_index()
+    vertices = mesh.vertices_attributes('xyz')
+    faces = [[key_index[key] for key in mesh.face_vertices(fkey)] for fkey in mesh.faces()]
+    V = array(vertices, dtype=float64)
+    F = array(faces, dtype=int)
+
+    e01 = V[F[:, 1]] - V[F[:, 0]]
+    e12 = V[F[:, 2]] - V[F[:, 1]]
+
+    normal = cross(e01, e12)
+    area_list = 0.5 * norm(normal, axis=1)
     area_list_norm = area_list / area_list.sum()
 
-    # (2) sample num_points on a mesh face regarding its weight of area
-    faces = list(range(mesh.number_of_faces()))
-    samples_faces = choice(faces, num_points, p=area_list_norm)
+    # (2) Sample num_points of times on a mesh face regarding its weight of area
+    faces_idx = list(range(mesh.number_of_faces()))
+    samples_faces_idx = choice(faces_idx, num_points, p=area_list_norm)
 
-    # (3) create a ndarray of vertices regarding the faces
-    vertices, faces = mesh.to_vertices_and_faces()
-    vertices = array(vertices, dtype=float64)
-    faces = array(faces)
-    faces_vertices = vertices[faces]
-
-    # (4) Barycentric Coordinate for Surface Sampling
+    # (3) Barycentric Coordinate for Surface Sampling
+    faces_vertices = V[F]
     v0, v1, v2 = faces_vertices[:, 0], faces_vertices[:, 1], faces_vertices[:, 2]
     r1_r2 = rand(2, num_points)
     r1, r2 = r1_r2[0], r1_r2[1]  # r1, r2 uniformly distributed from 0 to 1
@@ -104,25 +116,25 @@ def trimesh_samplepoints(mesh, num_points: int = 1000, return_normals: bool = Fa
     w0 = 1.0 - r1_sqrt
     w1 = r1_sqrt * (1.0 - r2)
     w2 = r1_sqrt * r2
-    a = v0[samples_faces]
-    b = v1[samples_faces]
-    c = v2[samples_faces]
+    a = v0[samples_faces_idx]
+    b = v1[samples_faces_idx]
+    c = v2[samples_faces_idx]
 
-    # (5) return vertices of points in the format of ndarray
+    # (4) return sample points in the format of ndarray
     samples_points = w0[:, None] * a + w1[:, None] * b + w2[:, None] * c
 
-    # (6) (if True) Return the normal vector of the sampled points
+    # (5) (if True) Return the normal vector of the sampled points
     if return_normals:
 
         samples_points_normals = cross((v1 - v0), (v2 - v1), axis=1)
         samples_points_normals_norm = norm(samples_points_normals, ord=2, axis=1, keepdims=True)
         samples_points_normals = samples_points_normals / samples_points_normals_norm
         samples_points_normals = clip(samples_points_normals, a_min=finfo(float64).eps, a_max=None)
-        samples_points_normals = samples_points_normals[samples_faces]
+        samples_points_normals = samples_points_normals[samples_faces_idx]
 
         return samples_points, samples_points_normals
-    else:
-        return samples_points
+
+    return samples_points
 
 
 if __name__ == '__main__':
