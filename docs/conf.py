98f8be686d917f3c24289cfd8946d37fafbed56e
brgcode <vanmelet@ethz.ch>
2021-02-12 18:52:10 +0100

    Bump version to 1.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ project = "COMPAS"
 copyright = "Block Research Group - ETH Zurich"
 author = "Tom Van Mele"
 
-release = "1.0.0"
+release = "1.1.0"
 version = ".".join(release.split(".")[0:2])
 
 master_doc = "index"
--- src/compas_plotters/__init__.py
+++ src/compas_plotters/__init__.py
@@ -19,7 +19,7 @@ Classes
 
 """
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 from .core import *  # noqa: F401 F403
 from .artists import *  # noqa: F401 F403
