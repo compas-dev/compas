828911609d48f5d5ae6a986e107f404b4c274a05
beverly <lytle@arch.ethz.ch>
2021-03-15 07:09:35 +0100

    split tests

--- tests/compas/geometry/test_circle.py
+++ tests/compas/geometry/test_circle.py
@@ -1,4 +1,5 @@
 from compas.geometry import Circle
+# These imports are used to check __repr__.
 from compas.geometry import Plane  # noqa: F401
 from compas.geometry import Point  # noqa: F401
 from compas.geometry import Vector  # noqa: F401
@@ -11,8 +12,31 @@ def test_circle():
     c = Circle(plane, 1.0)
     assert c.radius == 1.0
     assert c.plane == plane
+
+
+def test_equality():
+    point = [0, 0, 0]
+    vector = [1, 0, 0]
+    plane = (point, vector)
+    c = Circle(plane, 1.0)
     assert c == (plane, 1.0)
     assert c == Circle(plane, 1.0)
     assert c != 1.0
     assert c != (plane, 2.0)
+
+
+def test___repr__():
+    point = [0, 0, 0]
+    vector = [1, 0, 0]
+    plane = (point, vector)
+    c = Circle(plane, 1.0)
     assert c == eval(repr(c))
+
+
+def test___getitem__():
+    point = [0, 0, 0]
+    vector = [1, 0, 0]
+    plane = (point, vector)
+    c = Circle(plane, 1.0)
+    assert c[0] == plane
+    assert c[1] == 1.0
--- tests/compas/geometry/test_line.py
+++ tests/compas/geometry/test_line.py
@@ -1,4 +1,5 @@
 from compas.geometry import Line
+# This import is use to test __repr__.
 from compas.geometry import Point  # noqa: F401
 
 
@@ -8,8 +9,28 @@ def test_line():
     line = Line(p1, p2)
     assert line.start == p1
     assert line.end == p2
+
+
+def test_equality():
+    p1 = [0, 0, 0]
+    p2 = [1, 0, 0]
+    line = Line(p1, p2)
     assert (p1, p2) == line
     assert line == Line(p1, p2)
     assert line != (p2, p1)
     assert line != 1
+
+
+def test___repr__():
+    p1 = [0, 0, 0]
+    p2 = [1, 0, 0]
+    line = Line(p1, p2)
     assert line == eval(repr(line))
+
+
+def test___getitem__():
+    p1 = [0, 0, 0]
+    p2 = [1, 0, 0]
+    line = Line(p1, p2)
+    assert line[0] == p1
+    assert line[1] == p2
