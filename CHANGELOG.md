f5bc9b62e919c4598b0e84a9dab2136f0f17bc46
brgcode <vanmelet@ethz.ch>
2021-03-15 13:13:30 +0100

    static api for blender artists

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## Unreleased
 
 ### Added
+
 * Added `divide_polyline`, `divide_polyline_by_length`, `Polyline.split_at_corners` and `Polyline.tangent_at_point_on_polyline`.
 * Added the magic method `__str__` to `compas.geoemetry.Transformation`.
 * Added `redraw` flag to the `compas_rhino` methods `delete_object`, `delete_objects` and `purge_objects`.
--- src/compas_blender/artists/__init__.py
+++ src/compas_blender/artists/__init__.py
@@ -29,12 +29,13 @@ Classes
 
 """
 
-from ._artist import *  # noqa: F401 F403
+from ._artist import BaseArtist
 
-from .networkartist import *  # noqa: F401 F403
-from .meshartist import *  # noqa: F401 F403
-from .robotmodelartist import *  # noqa: F401 F403
-# from .volmeshartist import *  # noqa: F401 F403
+from .networkartist import NetworkArtist
+from .meshartist import MeshArtist
+from .robotmodelartist import BaseRobotModelArtist, RobotModelArtist
 
 
-__all__ = [name for name in dir() if not name.startswith('_')]
+__all__ = [
+    'BaseArtist', 'NetworkArtist', 'MeshArtist', 'BaseRobotModelArtist', 'RobotModelArtist'
+]
