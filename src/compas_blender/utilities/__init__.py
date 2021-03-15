24771d88c05648ee30036e8e0250ed9ce1948130
brgcode <vanmelet@ethz.ch>
2021-03-15 13:20:00 +0100

    static api for blender ui and utilities

--- src/compas_blender/utilities/__init__.py
+++ src/compas_blender/utilities/__init__.py
@@ -61,11 +61,63 @@ objects
     get_objects_by_names
 
 """
-from .data import *  # noqa: F401 F403
-from .objects import *  # noqa: F401 F403
-from .collections import *  # noqa: F401 F403
-from .drawing import *  # noqa: F401 F403
-from .misc import *  # noqa: F401 F403
+from .data import delete_unused_data
+from .objects import (
+    delete_object,
+    delete_objects,
+    delete_all_objects,
+    delete_object_by_name,
+    delete_objects_by_names,
+    get_object_by_name,
+    get_objects_by_names
+)
+from .collections import (
+    create_collection,
+    create_collections,
+    create_collections_from_path,
+    clear_collection,
+    clear_collections
+)
+from .drawing import (
+    draw_points,
+    draw_pointcloud,
+    draw_lines,
+    draw_polylines,
+    draw_cylinders,
+    draw_spheres,
+    draw_cubes,
+    draw_pipes,
+    draw_faces,
+    draw_texts,
+    draw_mesh
+)
+from .misc import unload_modules
 
 
-__all__ = [name for name in dir() if not name.startswith('_')]
+__all__ = [
+    'delete_unused_data',
+    'delete_object',
+    'delete_objects',
+    'delete_all_objects',
+    'delete_object_by_name',
+    'delete_objects_by_names',
+    'get_object_by_name',
+    'get_objects_by_names',
+    'create_collection',
+    'create_collections',
+    'create_collections_from_path',
+    'clear_collection',
+    'clear_collections',
+    'draw_points',
+    'draw_pointcloud',
+    'draw_lines',
+    'draw_polylines',
+    'draw_cylinders',
+    'draw_spheres',
+    'draw_cubes',
+    'draw_pipes',
+    'draw_faces',
+    'draw_texts',
+    'draw_mesh',
+    'unload_modules'
+]
