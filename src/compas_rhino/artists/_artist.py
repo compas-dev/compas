da5ffd26718cafd6b55933352fba734e669c135e
brgcode <vanmelet@ethz.ch>
2021-03-18 11:47:35 +0100

    remove local copy

--- src/compas_rhino/artists/_artist.py
+++ src/compas_rhino/artists/_artist.py
@@ -9,9 +9,6 @@ from compas.scene import BaseArtist
 __all__ = ["Artist"]
 
 
-_ITEM_ARTIST = {}
-
-
 class Artist(BaseArtist):
     """Base class for all Rhino artists.
 
@@ -34,10 +31,12 @@ class Artist(BaseArtist):
         raise NotImplementedError
 
     def redraw(self):
+        """Trigger a redraw."""
         compas_rhino.rs.EnableRedraw(True)
         compas_rhino.rs.Redraw()
 
     def clear(self):
+        """Delete all objects created by the artist."""
         if not self._guids:
             return
         compas_rhino.delete_objects(self._guids)
