18ee8a8fe6f58fc678bfe7339e3ccb88fa7c8b69
Gonzalo Casas <casas@arch.ethz.ch>
2021-05-03 11:48:29 +0200

    lint

--- src/compas_ghpython/components/Compas_FromJson/code.py
+++ src/compas_ghpython/components/Compas_FromJson/code.py
@@ -1,8 +1,6 @@
 """
 Deserializes JSON into COMPAS objects.
 """
-import os
-
 from ghpythonlib.componentbase import executingcomponent as component
 
 import compas
@@ -12,5 +10,5 @@ class CompasInfo(component):
     def RunScript(self, json_file_or_string):
         try:
             return compas.json_load(json_file_or_string)
-        except:
+        except:    # noqa: E722
             return compas.json_loads(json_file_or_string)
