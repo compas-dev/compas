362c437d8f8b6b361bd61561e1ff4242529f6a63
brgcode <vanmelet@ethz.ch>
2021-05-12 15:48:30 +0200

    Bump version to 1.6.1

--- src/compas/__init__.py
+++ src/compas/__init__.py
@@ -88,7 +88,7 @@ __copyright__ = 'Copyright 2014-2019 - Block Research Group, ETH Zurich'
 __license__ = 'MIT License'
 __email__ = 'vanmelet@ethz.ch'
 
-__version__ = '1.6.0'
+__version__ = '1.6.1'
 
 
 HERE = os.path.dirname(__file__)
--- src/compas_plotters/__init__.py
+++ src/compas_plotters/__init__.py
@@ -19,7 +19,7 @@ Classes
 
 """
 
-__version__ = '1.6.0'
+__version__ = '1.6.1'
 
 from .core import *  # noqa: F401 F403
 from .artists import *  # noqa: F401 F403
