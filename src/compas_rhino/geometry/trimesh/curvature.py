8029fbfea0b8de75f173e8197911dd5637221e55
Xingxin HE <xingxin.he@mail.polimi.it>
2021-05-14 23:14:28 +0800

    Finished feature trimesh_principle_curvature in compas_rhino
    The principle curvature currently is scalar curvature. The dual cell
    area is computed by barycentric area which is with less computing time.
    
    Future implementation could be vector curvature of principle curvature.

--- src/compas_rhino/geometry/trimesh/curvature.py
+++ src/compas_rhino/geometry/trimesh/curvature.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 import Rhino
 from math import pi
+from math import sqrt
 
 from compas.plugins import plugin
 
@@ -11,6 +12,7 @@ from compas.plugins import plugin
 __all__ = [
     'trimesh_gaussian_curvature',
     'trimesh_mean_curvature',
+    'trimesh_principle_curvature',
 ]
 
 
@@ -173,3 +175,107 @@ def trimesh_mean_curvature(M):
 
     # (4) Output
     return H
+
+
+@pluggable(category="trimesh", requires=['Rhino'])
+def trimesh_principal_curvature(M):
+    """Compute the principal curvature of a triangle mesh.
+    Parameters
+    ----------
+    M : (list, list)
+        A mesh represented by a list of vertices and a list of faces.
+    Returns
+    -------
+    Curvature : (k1, k2)
+        k1_list, the max curvature per vertex.
+        k2_list, the min curvature per vertex.
+
+    Notes
+    -----
+    Description: The discrete principal curvature is computed by mean curvature, Gaussian curvature, and vertex area.
+
+    Notation Convention:
+    :math:`\kappa_1, \kappa_2` - The max principle curvature and the min principle curvature
+    :math:`H_i` - the discrete mean curvature of vertex i
+    :math:`K_i` - the discrete Gaussian curvature of vertex i
+    :math:`A_i` - the vertex area of vertex i
+
+    Formula:
+    .. math::
+
+        \frac{H_i}{A_i}\pm\sqrt{( \,\frac{H_i}{A_i})\,^2-\frac{K_i}{A_i}}
+
+    Examples
+    --------
+    Make a mesh from scratch
+    >>> from compas.geometry import Sphere
+    >>> sphere = Sphere([1, 1, 1], 1)
+    >>> sphere = Mesh.from_shape(sphere, u=30, v=30)
+    >>> sphere.quads_to_triangles()
+    >>> M = sphere.to_vertices_and_faces()
+
+    Compute the discrete principle curvature
+
+    >>> H = trimesh_principle_curvature(M)
+    """
+    # (1) see if input is already Rhino.Geometry.Mesh
+    mesh = Rhino.Geometry.Mesh()
+    if type(M) != Rhino.Geometry.Mesh:
+        for vertices, faces in M:
+            for x, y, z in vertices:
+                mesh.Vertices.Add(x, y, z)
+            for face in faces:
+                mesh.Faces.AddFace(*face)
+    else:
+        mesh = M
+
+    # (2) Prepare ingredient and return list
+    k1 = []
+    k2 = []
+    A = trimesh_barycentric_area(mesh)
+    H = trimesh_mean_curvature(mesh)
+    K = trimesh_gaussian_curvature(mesh)
+
+    # (3) Main - loop over all vertices
+    for i in range(mesh.Vertices.Count):
+        if(A[i] == 0):
+            k1.append(0)
+            k2.append(0)
+            continue
+        H_ = H[i] / A[i]
+        K_ = K[i] / A[i]
+
+        k1.append(H_ + sqrt(max(0, H_ * H_ - K_)))
+        k2.append(H_ - sqrt(max(0, H_ * H_ - K_)))
+
+    # (4) Output
+    return k1, k2
+
+
+def trimesh_barycentric_area(mesh):
+    # (1) prepare return list
+    areas = [0] * mesh.Vertices.Count
+
+    # (2) Main - loop over mesh faces rather than vertices
+    for i in range(mesh.Faces.Count):
+        # Heron's Formula
+        ptA = mesh.Faces.GetFaceVertices(i)[1]
+        ptB = mesh.Faces.GetFaceVertices(i)[2]
+        ptC = mesh.Faces.GetFaceVertices(i)[3]
+        dA = ptB.DistanceTo(ptC)
+        dB = ptA.DistanceTo(ptC)
+        dC = ptA.DistanceTo(ptB)
+        p = (dA+dB+dC)/2
+        area = sqrt(p*(p-dA)*(p-dB)*(p-dC)) / 3
+
+        # topology vertices
+        verts_topo = mesh.Faces.GetTopologicalVertices(i)
+        # vertices
+        verts = []
+        for vert_topo in verts_topo:
+            verts.extend(mesh.TopologyVertices.MeshVertexIndices(vert_topo))
+        for vert in verts:
+            areas[vert] += area
+
+    # (3) output
+    return areas
