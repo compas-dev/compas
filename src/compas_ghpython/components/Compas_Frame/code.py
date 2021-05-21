b3df55dac5bef1b901f3140821d2b9f6bfe69088
beverly <lytle@arch.ethz.ch>
2021-05-21 15:10:20 +0200

    Add frame visualization gh component

---
+++ src/compas_ghpython/components/Compas_Frame/code.py
@@ -0,0 +1,22 @@
+"""
+Draws a frome or plane.
+"""
+from ghpythonlib.componentbase import executingcomponent as component
+
+import compas
+from compas_ghpython.artists import FrameArtist
+from compas_ghpython.components import coerce_frame
+
+
+class CompasFrame(component):
+    def RunScript(self, plane):
+        origin = None
+        axes = []
+
+        if plane:
+            frame = coerce_frame(plane)
+            artist = FrameArtist(frame)
+
+            origin, axes = artist.draw()
+
+        return origin, axes
