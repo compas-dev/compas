6d5e6a0ea878b8d2b3fb3f635d5ac01120ea1e8b
brgcode <vanmelet@ethz.ch>
2021-05-24 20:16:31 +0200

    remove unneeded

--- src/compas/data/__init__.py
+++ src/compas/data/__init__.py
@@ -30,9 +30,7 @@ Functions
     json_dumps
 
 """
-from __future__ import print_function
 from __future__ import absolute_import
-from __future__ import division
 
 from .encoders import DataEncoder
 from .encoders import DataDecoder
