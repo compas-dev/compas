a0c2ab496862774412bdb18c4a55d737dc0947bc
brgcode <vanmelet@ethz.ch>
2021-06-03 20:47:11 +0200

    test pickling of actual object

--- tests/compas/data/test_pickle.py
+++ tests/compas/data/test_pickle.py
@@ -1,30 +1,12 @@
 import pickle
-from compas.data import Data
-
-
-class DataObject(Data):
-
-    def __init__(self):
-        super(DataObject, self).__init__()
-        self.point = [0, 0, 0]
-        self.normal = [0, 0, 1]
-        self.radius = 1.0
-
-    @property
-    def data(self):
-        return {'plane': {'point': self.point, 'normal': self.normal}, 'radius': self.radius}
-
-    @data.setter
-    def data(self, data):
-        self.point = data['plane']['point']
-        self.normal = data['plane']['normal']
-        self.radius = data['radius']
+from compas.geometry import Frame
 
 
 def test_pickling():
-    d1 = DataObject()
-    s = pickle.dumps(d1, protocol=pickle.HIGHEST_PROTOCOL)
-    d2 = pickle.loads(s)
-    assert all(a == b for a, b in zip(d1.point, d2.point))
-    assert all(a == b for a, b in zip(d1.normal, d2.normal))
-    assert d1.radius == d2.radius
+    f1 = Frame.worldXY()
+    s = pickle.dumps(f1, protocol=pickle.HIGHEST_PROTOCOL)
+    f2 = pickle.loads(s)
+    assert all(a == b for a, b in zip(f1.point, f2.point))
+    assert all(a == b for a, b in zip(f1.xaxis, f2.xaxis))
+    assert all(a == b for a, b in zip(f1.yaxis, f2.yaxis))
+    assert all(a == b for a, b in zip(f1.zaxis, f2.zaxis))
