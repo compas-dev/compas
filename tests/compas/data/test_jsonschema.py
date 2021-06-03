cf6574e25e94afa7cdd092b2b665133400d1a7c8
brgcode <vanmelet@ethz.ch>
2021-06-03 16:55:55 +0200

    tests for primitive and shape schemas

--- tests/compas/data/test_jsonschema.py
+++ tests/compas/data/test_jsonschema.py
@@ -73,6 +73,86 @@ if not compas.IPY:
         validator = jsonschema.Draft7Validator(schema, resolver=resolver)
         return validator
 
+    @pytest.fixture
+    def quaternion_validator(resolver):
+        path = os.path.join(compas.HERE, 'data', 'schemas', 'quaternion.json')
+        with open(path) as f:
+            schema = json.load(f)
+        validator = jsonschema.Draft7Validator(schema, resolver=resolver)
+        return validator
+
+    @pytest.fixture
+    def polygon_validator(resolver):
+        path = os.path.join(compas.HERE, 'data', 'schemas', 'polygon.json')
+        with open(path) as f:
+            schema = json.load(f)
+        validator = jsonschema.Draft7Validator(schema, resolver=resolver)
+        return validator
+
+    @pytest.fixture
+    def polyline_validator(resolver):
+        path = os.path.join(compas.HERE, 'data', 'schemas', 'polyline.json')
+        with open(path) as f:
+            schema = json.load(f)
+        validator = jsonschema.Draft7Validator(schema, resolver=resolver)
+        return validator
+
+    @pytest.fixture
+    def box_validator(resolver):
+        path = os.path.join(compas.HERE, 'data', 'schemas', 'box.json')
+        with open(path) as f:
+            schema = json.load(f)
+        validator = jsonschema.Draft7Validator(schema, resolver=resolver)
+        return validator
+
+    @pytest.fixture
+    def capsule_validator(resolver):
+        path = os.path.join(compas.HERE, 'data', 'schemas', 'capsule.json')
+        with open(path) as f:
+            schema = json.load(f)
+        validator = jsonschema.Draft7Validator(schema, resolver=resolver)
+        return validator
+
+    @pytest.fixture
+    def cone_validator(resolver):
+        path = os.path.join(compas.HERE, 'data', 'schemas', 'cone.json')
+        with open(path) as f:
+            schema = json.load(f)
+        validator = jsonschema.Draft7Validator(schema, resolver=resolver)
+        return validator
+
+    @pytest.fixture
+    def cylinder_validator(resolver):
+        path = os.path.join(compas.HERE, 'data', 'schemas', 'cylinder.json')
+        with open(path) as f:
+            schema = json.load(f)
+        validator = jsonschema.Draft7Validator(schema, resolver=resolver)
+        return validator
+
+    @pytest.fixture
+    def polyhedron_validator(resolver):
+        path = os.path.join(compas.HERE, 'data', 'schemas', 'polyhedron.json')
+        with open(path) as f:
+            schema = json.load(f)
+        validator = jsonschema.Draft7Validator(schema, resolver=resolver)
+        return validator
+
+    @pytest.fixture
+    def sphere_validator(resolver):
+        path = os.path.join(compas.HERE, 'data', 'schemas', 'sphere.json')
+        with open(path) as f:
+            schema = json.load(f)
+        validator = jsonschema.Draft7Validator(schema, resolver=resolver)
+        return validator
+
+    @pytest.fixture
+    def torus_validator(resolver):
+        path = os.path.join(compas.HERE, 'data', 'schemas', 'torus.json')
+        with open(path) as f:
+            schema = json.load(f)
+        validator = jsonschema.Draft7Validator(schema, resolver=resolver)
+        return validator
+
 
 if not compas.IPY:
 
@@ -182,3 +262,165 @@ if not compas.IPY:
     def test_schema_frame_invalid(frame_validator, frame):
         with pytest.raises(jsonschema.exceptions.ValidationError):
             frame_validator.validate(frame)
+
+    @pytest.mark.parametrize("quaternion", [
+        {'x': 0, 'y': 0, 'z': 0, 'w': 0},
+        {'w': 0, 'x': 0, 'y': 0, 'z': 0},
+        {'x': 0, 'z': 0, 'y': 0, 'w': 0}
+    ])
+    def test_schema_quaternion_valid(quaternion_validator, quaternion):
+        quaternion_validator.validate(quaternion)
+
+    @pytest.mark.parametrize("quaternion", [
+        {'x': 0, 'y': 0, 'z': 0},
+        {'x': 0, 'y': 0, 'w': 0},
+        {'y': 0, 'z': 0, 'w': 0},
+        {'X': 0, 'Y': 0, 'Z': 0, 'W': 0}
+    ])
+    def test_schema_quaternion_invalid(quaternion_validator, quaternion):
+        with pytest.raises(jsonschema.exceptions.ValidationError):
+            quaternion_validator.validate(quaternion)
+
+    @pytest.mark.parametrize("polygon", [
+        {'points': [[0, 0, 0], [1, 0, 0]]},
+        {'points': [[0, 0, 0], [1, 0, 0], [1, 1, 0]]}
+    ])
+    def test_schema_polygon_valid(polygon_validator, polygon):
+        polygon_validator.validate(polygon)
+
+    @pytest.mark.parametrize("polygon", [
+        {'points': [[0, 0, 0]]},
+        {'points': [0, 0, 0]},
+        {'POINTS': [[0, 0, 0], [1, 0, 0]]}
+    ])
+    def test_schema_polygon_invalid(polygon_validator, polygon):
+        with pytest.raises(jsonschema.exceptions.ValidationError):
+            polygon_validator.validate(polygon)
+
+    @pytest.mark.parametrize("polyline", [
+        {'points': [[0, 0, 0], [1, 0, 0]]},
+        {'points': [[0, 0, 0], [1, 0, 0], [1, 1, 0]]}
+    ])
+    def test_schema_polyline_valid(polyline_validator, polyline):
+        polyline_validator.validate(polyline)
+
+    @pytest.mark.parametrize("polyline", [
+        {'points': [[0, 0, 0]]},
+        {'points': [0, 0, 0]},
+        {'POINTS': [[0, 0, 0], [1, 0, 0]]}
+    ])
+    def test_schema_polyline_invalid(polyline_validator, polyline):
+        with pytest.raises(jsonschema.exceptions.ValidationError):
+            polyline_validator.validate(polyline)
+
+    @pytest.mark.parametrize("box", [
+        {'frame': {'point': [0, 0, 0], 'xaxis': [1, 0, 0], 'yaxis': [0, 1, 0]}, 'xsize': 1, 'ysize': 1, 'zsize': 1}
+    ])
+    def test_schema_box_valid(box_validator, box):
+        box_validator.validate(box)
+
+    @pytest.mark.parametrize("box", [
+        {'frame': {'point': [0, 0, 0], 'xaxis': [1, 0, 0], 'yaxis': [0, 1, 0]}, 'xsize': 0, 'ysize': 1, 'zsize': 1},
+        {'frame': {'point': [0, 0, 0], 'xaxis': [1, 0, 0], 'yaxis': [0, 1, 0]}, 'xsize': 1, 'ysize': 1, 'zsize': 0},
+        {'frame': {'point': [0, 0, 0], 'xaxis': [1, 0, 0], 'yaxis': [0, 1, 0]}, 'xsize': 1, 'ysize': 0, 'zsize': 1},
+        {'FRAME': {'point': [0, 0, 0], 'xaxis': [1, 0, 0], 'yaxis': [0, 1, 0]}, 'XSIZE': 1, 'YSIZE': 1, 'ZSIZE': 1}
+    ])
+    def test_schema_box_invalid(box_validator, box):
+        with pytest.raises(jsonschema.exceptions.ValidationError):
+            box_validator.validate(box)
+
+    @pytest.mark.parametrize("capsule", [
+        {'line': {'start': [0, 0, 0], 'end': [1, 0, 0]}, 'radius': 1.0}
+    ])
+    def test_schema_capsule_valid(capsule_validator, capsule):
+        capsule_validator.validate(capsule)
+
+    @pytest.mark.parametrize("capsule", [
+        {'start': [0, 0, 0], 'end': [1, 0, 0], 'radius': 1.0},
+        {'line': [[0, 0, 0], [1, 0, 0]], 'radius': 1.0},
+        {'line': {'start': [0, 0, 0], 'end': [1, 0, 0]}, 'radius': 0.0},
+        {'LINE': {'start': [0, 0, 0], 'end': [1, 0, 0]}, 'RADIUS': 1.0}
+    ])
+    def test_schema_capsule_invalid(capsule_validator, capsule):
+        with pytest.raises(jsonschema.exceptions.ValidationError):
+            capsule_validator.validate(capsule)
+
+    @pytest.mark.parametrize("cone", [
+        {'circle': {'plane': {'point': [0, 0, 0], 'normal': [0, 0, 1]}, 'radius': 1.0}, 'height': 1.0}
+    ])
+    def test_schema_cone_valid(cone_validator, cone):
+        cone_validator.validate(cone)
+
+    @pytest.mark.parametrize("cone", [
+        {'circle': {'plane': {'point': [0, 0, 0], 'normal': [0, 0, 1]}, 'radius': 1.0}, 'height': 0.0},
+        {'circle': {'plane': {'point': [0, 0, 0], 'normal': [0, 0, 1]}, 'radius': 0.0}, 'height': 1.0},
+        {'circle': {'plane': {'point': [0, 0, 0], 'normal': [0, 0, 1]}, 'radius': 0.0}, 'height': 0.0},
+        {'CIRCLE': {'plane': {'point': [0, 0, 0], 'normal': [0, 0, 1]}, 'radius': 0.0}, 'HEIGHT': 0.0}
+    ])
+    def test_schema_cone_invalid(cone_validator, cone):
+        with pytest.raises(jsonschema.exceptions.ValidationError):
+            cone_validator.validate(cone)
+
+    @pytest.mark.parametrize("cylinder", [
+        {'circle': {'plane': {'point': [0, 0, 0], 'normal': [0, 0, 1]}, 'radius': 1.0}, 'height': 1.0}
+    ])
+    def test_schema_cylinder_valid(cylinder_validator, cylinder):
+        cylinder_validator.validate(cylinder)
+
+    @pytest.mark.parametrize("cylinder", [
+        {'circle': {'plane': {'point': [0, 0, 0], 'normal': [0, 0, 1]}, 'radius': 1.0}, 'height': 0.0},
+        {'circle': {'plane': {'point': [0, 0, 0], 'normal': [0, 0, 1]}, 'radius': 0.0}, 'height': 1.0},
+        {'circle': {'plane': {'point': [0, 0, 0], 'normal': [0, 0, 1]}, 'radius': 0.0}, 'height': 0.0},
+        {'CIRCLE': {'plane': {'point': [0, 0, 0], 'normal': [0, 0, 1]}, 'radius': 0.0}, 'HEIGHT': 0.0}
+    ])
+    def test_schema_cylinder_invalid(cylinder_validator, cylinder):
+        with pytest.raises(jsonschema.exceptions.ValidationError):
+            cylinder_validator.validate(cylinder)
+
+    @pytest.mark.parametrize("polyhedron", [
+        {'vertices': [[0, 0, 0], [1, 0, 0], [1, 1, 0], [1, 0, 1]], 'faces': [[0, 1, 3], [1, 2, 3], [2, 0, 3], [0, 2, 1]]},
+    ])
+    def test_schema_polyhedron_valid(polyhedron_validator, polyhedron):
+        polyhedron_validator.validate(polyhedron)
+
+    @pytest.mark.parametrize("polyhedron", [
+        {'vertices': [[0, 0, 0], [1, 0, 0], [1, 1, 0], [1, 0, 1]], 'faces': [[0, 1, 3], [1, 2, 3], [2, 0, 3]]},
+        {'vertices': [[0, 0, 0], [1, 0, 0], [1, 1, 0]], 'faces': [[0, 1, 3], [1, 2, 3], [2, 0, 3], [0, 2, 1]]},
+        {'vertices': [[0, 0, 0], [1, 0, 0], [1, 1, 0], [1, 0, 1]], 'faces': []},
+        {'vertices': [], 'faces': [[0, 1, 3], [1, 2, 3], [2, 0, 3], [0, 2, 1]]},
+        {'vertices': [], 'faces': []}
+    ])
+    def test_schema_polyhedron_invalid(polyhedron_validator, polyhedron):
+        with pytest.raises(jsonschema.exceptions.ValidationError):
+            polyhedron_validator.validate(polyhedron)
+
+    @pytest.mark.parametrize("sphere", [
+        {'point': [0, 0, 0], 'radius': 1.0}
+    ])
+    def test_schema_sphere_valid(sphere_validator, sphere):
+        sphere_validator.validate(sphere)
+
+    @pytest.mark.parametrize("sphere", [
+        {'point': [0, 0], 'radius': 1},
+        {'point': [0, 0, 0, 0], 'radius': 1},
+        {'point': [0, 0, 0], 'radius': 0},
+        {'POINT': [0, 0, 0], 'RADIUS': 1}
+    ])
+    def test_schema_sphere_invalid(sphere_validator, sphere):
+        with pytest.raises(jsonschema.exceptions.ValidationError):
+            sphere_validator.validate(sphere)
+
+    @pytest.mark.parametrize("torus", [
+        {'plane': {'point': [0, 0, 0], 'normal': [0, 0, 1]}, 'radius_axis': 1.0, 'radius_pipe': 1.0}
+    ])
+    def test_schema_torus_valid(torus_validator, torus):
+        torus_validator.validate(torus)
+
+    @pytest.mark.parametrize("torus", [
+        {'plane': {'point': [0, 0, 0], 'normal': [0, 0, 1]}, 'radius_axis': 1.0, 'radius_pipe': 0.0},
+        {'plane': {'point': [0, 0, 0], 'normal': [0, 0, 1]}, 'radius_axis': 0.0, 'radius_pipe': 1.0},
+        {'plane': {'point': [0, 0, 0], 'normal': [0, 0, 1]}, 'radius_axis': 0.0, 'radius_pipe': 0.0}
+    ])
+    def test_schema_torus_invalid(torus_validator, torus):
+        with pytest.raises(jsonschema.exceptions.ValidationError):
+            torus_validator.validate(torus)
