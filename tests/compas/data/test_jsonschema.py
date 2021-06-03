d9d0ff15d0cf26978fdb9f2a24c9846d5d87d3e5
brgcode <vanmelet@ethz.ch>
2021-06-03 15:09:35 +0200

    more primitive tests

--- tests/compas/data/test_jsonschema.py
+++ tests/compas/data/test_jsonschema.py
@@ -65,6 +65,14 @@ if not compas.IPY:
         validator = jsonschema.Draft7Validator(schema, resolver=resolver)
         return validator
 
+    @pytest.fixture
+    def frame_validator(resolver):
+        path = os.path.join(compas.HERE, 'data', 'schemas', 'frame.json')
+        with open(path) as f:
+            schema = json.load(f)
+        validator = jsonschema.Draft7Validator(schema, resolver=resolver)
+        return validator
+
 
 if not compas.IPY:
 
@@ -106,9 +114,71 @@ if not compas.IPY:
 
     @pytest.mark.parametrize("line", [
         [[0, 0, 0], [0, 0, 0]],
-        {'START': [0, 0, 0], 'END': [0, 0, 0]},
-        {'sp': [0, 0, 0], 'ep': [0, 0, 0]}
+        {'START': [0, 0, 0], 'END': [0, 0, 0]}
     ])
     def test_schema_line_invalid(line_validator, line):
         with pytest.raises(jsonschema.exceptions.ValidationError):
             line_validator.validate(line)
+
+    @pytest.mark.parametrize("plane", [
+        {'point': [0, 0, 0], 'normal': [0, 0, 1]}
+    ])
+    def test_schema_plane_valid(plane_validator, plane):
+        plane_validator.validate(plane)
+
+    @pytest.mark.parametrize("plane", [
+        [[0, 0, 0], [0, 0, 1]],
+        {'POINT': [0, 0, 0], 'NORMAL': [0, 0, 1]}
+    ])
+    def test_schema_plane_invalid(plane_validator, plane):
+        with pytest.raises(jsonschema.exceptions.ValidationError):
+            plane_validator.validate(plane)
+
+    @pytest.mark.parametrize("circle", [
+        {'plane': {'point': [0, 0, 0], 'normal': [0, 0, 1]}, 'radius': 1.0}
+    ])
+    def test_schema_circle_valid(circle_validator, circle):
+        circle_validator.validate(circle)
+
+    @pytest.mark.parametrize("circle", [
+        {'plane': {'point': [0, 0, 0], 'normal': [0, 0, 1]}, 'radius': 0.0},
+        {'plane': [[0, 0, 0], [0, 0, 1]], 'radius': 1.0},
+        {'PLANE': {'point': [0, 0, 0], 'normal': [0, 0, 1]}, 'RADIUS': 1.0}
+    ])
+    def test_schema_circle_invalid(circle_validator, circle):
+        with pytest.raises(jsonschema.exceptions.ValidationError):
+            circle_validator.validate(circle)
+
+    @pytest.mark.parametrize("ellipse", [
+        {'plane': {'point': [0, 0, 0], 'normal': [0, 0, 1]}, 'major': 1.0, 'minor': 0.5}
+    ])
+    def test_schema_ellipse_valid(ellipse_validator, ellipse):
+        ellipse_validator.validate(ellipse)
+
+    @pytest.mark.parametrize("ellipse", [
+        {'plane': {'point': [0, 0, 0], 'normal': [0, 0, 1]}, 'major': 1.0, 'minor': 0.0},
+        {'plane': {'point': [0, 0, 0], 'normal': [0, 0, 1]}, 'major': 0.0, 'minor': 0.5},
+        {'plane': {'point': [0, 0, 0], 'normal': [0, 0, 1]}, 'major': 0.0, 'minor': 0.0},
+        {'plane': [[0, 0, 0], [0, 0, 1]], 'major': 1.0, 'minor': 0.5},
+        {'PLANE': {'point': [0, 0, 0], 'normal': [0, 0, 1]}, 'MAJOR': 1.0, 'MINOR': 0.5}
+    ])
+    def test_schema_ellipse_invalid(ellipse_validator, ellipse):
+        with pytest.raises(jsonschema.exceptions.ValidationError):
+            ellipse_validator.validate(ellipse)
+
+    @pytest.mark.parametrize("frame", [
+        {'point': [0, 0, 0], 'xaxis': [1, 0, 0], 'yaxis': [0, 1, 0]},
+        {'point': [0, 0, 0], 'yaxis': [0, 1, 0], 'xaxis': [1, 0, 0]},
+        {'point': [0, 0, 0], 'xaxis': [1, 0, 0], 'yaxis': [0, 1, 0], 'zaxis': [0, 0, 1]},
+        {'point': [0, 0, 0], 'xaxis': [1, 0, 0], 'zaxis': [0, 0, 1], 'yaxis': [0, 1, 0]}
+    ])
+    def test_schema_frame_valid(frame_validator, frame):
+        frame_validator.validate(frame)
+
+    @pytest.mark.parametrize("frame", [
+        {'point': [0, 0, 0], 'yaxis': [0, 1, 0], 'zaxis': [0, 0, 1]},
+        {'point': [0, 0, 0], 'xaxis': [1, 0, 0], 'zaxis': [0, 0, 1]}
+    ])
+    def test_schema_frame_invalid(frame_validator, frame):
+        with pytest.raises(jsonschema.exceptions.ValidationError):
+            frame_validator.validate(frame)
