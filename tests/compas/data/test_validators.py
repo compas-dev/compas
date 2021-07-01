566b013b5738a53dfb14eea41801001634ef79f9
brgcode <vanmelet@ethz.ch>
2021-07-01 18:21:10 +0200

    ipy jeej

--- tests/compas/data/test_validators.py
+++ tests/compas/data/test_validators.py
@@ -1,3 +1,4 @@
+import compas
 import pytest
 
 from compas.data import is_sequence_of_int
@@ -73,13 +74,14 @@ def test_is_float3(sequence, result):
     assert is_float3(sequence) is result
 
 
-@pytest.mark.parametrize('sequence,result', [
-    (map(float, range(3)), False),
-    (map(float, range(+1, -2, -1)), False),
-])
-def test_is_float3_invalid(sequence, result):
-    with pytest.raises(TypeError):
-        assert is_float3(sequence) is result
+if not compas.IPY:
+    @pytest.mark.parametrize('sequence,result', [
+        (map(float, range(3)), False),
+        (map(float, range(+1, -2, -1)), False),
+    ])
+    def test_is_float3_invalid(sequence, result):
+        with pytest.raises(TypeError):
+            assert is_float3(sequence) is result
 
 
 @pytest.mark.parametrize('sequence,result', [
@@ -97,10 +99,11 @@ def test_is_float4x4(sequence, result):
     assert is_float4x4(sequence) is result
 
 
-@pytest.mark.parametrize('sequence,result', [
-    ([map(float, range(4)) for _ in range(4)], True),
-    ([map(float, range(+2, -2, -1)) for _ in range(4)], True),
-])
-def test_is_float4x4_invalid(sequence, result):
-    with pytest.raises(TypeError):
-        assert is_float4x4(sequence) is result
+if not compas.IPY:
+    @pytest.mark.parametrize('sequence,result', [
+        ([map(float, range(4)) for _ in range(4)], True),
+        ([map(float, range(+2, -2, -1)) for _ in range(4)], True),
+    ])
+    def test_is_float4x4_invalid(sequence, result):
+        with pytest.raises(TypeError):
+            assert is_float4x4(sequence) is result
