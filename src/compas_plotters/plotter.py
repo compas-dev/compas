5eb85b7eb6f15024c1162773bcf2a81dd5e7dfc1
brgcode <vanmelet@ethz.ch>
2021-07-02 10:55:02 +0200

    types and docs

--- src/compas_plotters/plotter.py
+++ src/compas_plotters/plotter.py
@@ -1,5 +1,10 @@
+from __future__ import annotations
+
+from typing import Callable, Optional, Tuple, List, Union
+import matplotlib
 import matplotlib.pyplot as plt
 
+import compas
 from compas_plotters import Artist
 
 
@@ -15,18 +20,14 @@ class Plotter:
         The size of the figure in inches.
         Default is ``(8, 5)``
 
-    Attributes
-    ----------
-
-    Examples
-    --------
-
-    Notes
-    -----
-
     """
 
-    def __init__(self, view=[(-8, 16), (-5, 10)], figsize=(8, 5), dpi=100, bgcolor=(1.0, 1.0, 1.0), show_axes=False):
+    def __init__(self,
+                 view: Tuple[Tuple[float, float], Tuple[float, float]] = ((-8.0, 16.0), (-5.0, 10.0)),
+                 figsize: Tuple[float, float] = (8.0, 5.0),
+                 dpi: float = 100,
+                 bgcolor: Tuple[float, float, float] = (1.0, 1.0, 1.0),
+                 show_axes: bool = False):
         self._show_axes = show_axes
         self._bgcolor = None
         self._viewbox = None
@@ -38,19 +39,19 @@ class Plotter:
         self.bgcolor = bgcolor
 
     @property
-    def viewbox(self):
+    def viewbox(self) -> Tuple[Tuple[float, float], Tuple[float, float]]:
         """([xmin, xmax], [ymin, ymax]): The area of the axes that is zoomed into view."""
         return self._viewbox
 
     @viewbox.setter
-    def viewbox(self, view):
+    def viewbox(self, view: Tuple[Tuple[float, float], Tuple[float, float]]):
         xlim, ylim = view
         xmin, xmax = xlim
         ymin, ymax = ylim
-        self._viewbox = [xmin, xmax], [ymin, ymax]
+        self._viewbox = (xmin, xmax), (ymin, ymax)
 
     @property
-    def axes(self):
+    def axes(self) -> matplotlib.axes.Axes:
         """Returns the axes subplot matplotlib object.
 
         Returns
@@ -83,18 +84,6 @@ class Plotter:
             axes.set_yscale('linear')
             if self._show_axes:
                 axes.set_frame_on(True)
-                # major_xticks = np.arange(0, 501, 20)
-                # major_yticks = np.arange(0, 301, 20)
-                # minor_xticks = np.arange(0, 501, 5)
-                # minor_yticks = np.arange(0, 301, 5)
-                # ax.tick_params(axis = 'both', which = 'major', labelsize = 6)
-                # ax.tick_params(axis = 'both', which = 'minor', labelsize = 0)
-                # ax.set_xticks(major_xticks)
-                # ax.set_xticks(minor_yticks, minor = True)
-                # ax.set_yticks(major_xticks)
-                # ax.set_yticks(minor_yticks, minor = True)
-                # axes.tick_params(labelbottom=False, labelleft=False)
-                # axes.grid(axis='both', linestyle='--', linewidth=0.5, color=(0.7, 0.7, 0.7))
                 axes.grid(False)
                 axes.set_xticks([])
                 axes.set_yticks([])
@@ -115,7 +104,7 @@ class Plotter:
         return self._axes
 
     @property
-    def figure(self):
+    def figure(self) -> matplotlib.figure.Figure:
         """Returns the matplotlib figure instance.
 
         Returns
@@ -141,7 +130,7 @@ class Plotter:
         return self.figure.canvas
 
     @property
-    def bgcolor(self):
+    def bgcolor(self) -> str:
         """Returns the background color.
 
         Returns
@@ -153,7 +142,7 @@ class Plotter:
         return self._bgcolor
 
     @bgcolor.setter
-    def bgcolor(self, value):
+    def bgcolor(self, value: Union[str, Tuple[float, float, float]]):
         """Sets the background color.
 
         Parameters
@@ -168,7 +157,7 @@ class Plotter:
         self.figure.set_facecolor(value)
 
     @property
-    def title(self):
+    def title(self) -> str:
         """Returns the title of the plot.
 
         Returns
@@ -180,7 +169,7 @@ class Plotter:
         return self.figure.canvas.get_window_title()
 
     @title.setter
-    def title(self, value):
+    def title(self, value: str):
         """Sets the title of the plot.
 
         Parameters
@@ -192,23 +181,31 @@ class Plotter:
         self.figure.canvas.set_window_title(value)
 
     @property
-    def artists(self):
+    def artists(self) -> List[Artist]:
         """list of :class:`compas_plotters.artists.Artist`"""
         return self._artists
 
     @artists.setter
-    def artists(self, artists):
+    def artists(self, artists: List[Artist]):
         self._artists = artists
 
     # =========================================================================
     # Methods
     # =========================================================================
 
-    def pause(self, pause):
+    def pause(self, pause: float) -> None:
+        """Pause plotting during the specified interval.
+
+        Parameters
+        ----------
+        pause: float
+            The duration of the pause in seconds.
+        """
         if pause:
             plt.pause(pause)
 
-    def zoom_extents(self):
+    def zoom_extents(self) -> None:
+        """Zoom the view to the bounding box of all objects."""
         width, height = self.figsize
         fig_aspect = width / height
         data = []
@@ -232,7 +229,19 @@ class Plotter:
             self.axes.set_ylim(scale * (ymin - 0.1 * yspan), scale * (ymax + 0.1 * yspan))
         self.axes.autoscale_view()
 
-    def add(self, item, artist=None, **kwargs):
+    def add(self,
+            item: Union[compas.geometry.Circle,
+                        compas.geometry.Ellipse,
+                        compas.geometry.Line,
+                        compas.geometry.Point,
+                        compas.geometry.Polygon,
+                        compas.geometry.Polyline,
+                        compas.geometry.Vector,
+                        compas.datastructures.Mesh],
+            artist: Optional[Artist] = None,
+            **kwargs) -> Artist:
+        """Add a COMPAS geometry object or data structure to the plot.
+        """
         if not artist:
             artist = Artist.build(item, **kwargs)
         artist.plotter = self
@@ -240,26 +249,47 @@ class Plotter:
         self._artists.append(artist)
         return artist
 
-    def add_as(self, item, artist_type, **kwargs):
+    def add_as(self,
+               item: Union[compas.geometry.Circle,
+                           compas.geometry.Ellipse,
+                           compas.geometry.Line,
+                           compas.geometry.Point,
+                           compas.geometry.Polygon,
+                           compas.geometry.Polyline,
+                           compas.geometry.Vector,
+                           compas.datastructures.Mesh],
+               artist_type: Artist,
+               **kwargs) -> Artist:
+        """Add a COMPAS geometry object or data structure using a specific artist type."""
         artist = Artist.build_as(item, artist_type, **kwargs)
         artist.plotter = self
         artist.draw()
         self._artists.append(artist)
         return artist
 
-    def add_from_list(self, items, **kwargs):
+    def add_from_list(self, items, **kwargs) -> List[Artist]:
+        """Add multiple COMPAS geometry objects and/or data structures from a list."""
         artists = []
         for item in items:
             artist = self.add(item, **kwargs)
             artists.append(artist)
         return artists
 
-    def find(self, item):
+    def find(self,
+             item: Union[compas.geometry.Circle,
+                         compas.geometry.Ellipse,
+                         compas.geometry.Line,
+                         compas.geometry.Point,
+                         compas.geometry.Polygon,
+                         compas.geometry.Polyline,
+                         compas.geometry.Vector,
+                         compas.datastructures.Mesh]) -> Artist:
+        """Find a geometry object or data structure in the plot."""
         for artist in self._artists:
             if item is artist.item:
                 return artist
 
-    def register_listener(self, listener):
+    def register_listener(self, listener: Callable) -> None:
         """Register a listener for pick events.
 
         Parameters
@@ -284,13 +314,14 @@ class Plotter:
         """
         self.figure.canvas.mpl_connect('pick_event', listener)
 
-    def draw(self, pause=None):
+    def draw(self, pause: Optional[float] = None) -> None:
+        """Draw all objects included in the plot."""
         self.figure.canvas.draw()
         self.figure.canvas.flush_events()
         if pause:
             plt.pause(pause)
 
-    def redraw(self, pause=None):
+    def redraw(self, pause: Optional[float] = None) -> None:
         """Updates and pauses the plot.
 
         Parameters
@@ -306,14 +337,13 @@ class Plotter:
         if pause:
             plt.pause(pause)
 
-    def show(self):
+    def show(self) -> None:
         """Displays the plot.
-
         """
         self.draw()
         plt.show()
 
-    def save(self, filepath, **kwargs):
+    def save(self, filepath: str, **kwargs) -> None:
         """Saves the plot to a file.
 
         Parameters
