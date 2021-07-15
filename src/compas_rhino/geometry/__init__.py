8b40fa5df0e33fe969b007c5f560fa1d1b2efbf6
brgcode <vanmelet@ethz.ch>
2021-07-15 08:16:49 +0200

    this is supposed to work

--- src/compas_rhino/geometry/__init__.py
+++ src/compas_rhino/geometry/__init__.py
@@ -17,75 +17,21 @@ Wrappers for Rhino objects that can be used to convert Rhino geometry and data t
     guid = compas_rhino.select_mesh()
     mesh = RhinoMesh.from_guid(guid).to_compas()
 
+Classes
+=======
 
-BaseRhinoGeometry
-=================
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
 
-.. autoclass:: BaseRhinoGeometry
-    :members: from_geometry, from_selection, to_compas, from_guid, from_object, transform
-
-----
-
-RhinoPoint
-==========
-
-.. autoclass:: RhinoPoint
-    :members: from_geometry, from_selection, to_compas
-    :no-show-inheritance:
-
-----
-
-RhinoVector
-===========
-
-.. autoclass:: RhinoVector
-    :members: from_geometry, from_selection, to_compas
-    :no-show-inheritance:
-
-----
-
-RhinoLine
-=========
-
-.. autoclass:: RhinoLine
-    :members: from_geometry, from_selection, to_compas
-    :no-show-inheritance:
-
-----
-
-RhinoPlane
-==========
-
-.. autoclass:: RhinoPlane
-    :members: from_geometry, from_selection, to_compas
-    :no-show-inheritance:
-
-----
-
-RhinoMesh
-=========
-
-.. autoclass:: RhinoMesh
-    :members: from_geometry, from_selection, to_compas
-    :no-show-inheritance:
-
-----
-
-RhinoCurve
-==========
-
-.. autoclass:: RhinoCurve
-    :members: from_geometry, from_selection, to_compas
-    :no-show-inheritance:
-
-----
-
-RhinoSurface
-============
-
-.. autoclass:: RhinoSurface
-    :members: from_geometry, from_selection, to_compas
-    :no-show-inheritance:
+    BaseRhinoGeometry
+    RhinoCurve
+    RhinoLine
+    RhinoMesh
+    RhinoPlane
+    RhinoPoint
+    RhinoSurface
+    RhinoVector
 
 """
 from __future__ import absolute_import
