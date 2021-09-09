0f8e0bd3b2360f9320807912a365fd17c00934d6
brgcode <vanmelet@ethz.ch>
2021-09-09 16:52:33 +0200

    update example (more or less)

--- docs/tutorial/assembly_blocks.py
+++ docs/tutorial/assembly_blocks.py
@@ -1,6 +1,5 @@
 from compas.datastructures import Assembly
 from compas.datastructures import Part
-from compas.datastructures import Mesh
 from compas.geometry import Box
 from compas.geometry import Translation
 
@@ -9,11 +8,11 @@ from compas.geometry import Translation
 assembly = Assembly()
 
 a = Part(name='A')
-a.mesh = Mesh.from_shape(Box.from_width_height_depth(1, 1, 1))
+a.geometry = Box.from_width_height_depth(1, 1, 1)
 
 b = Part(name='B')
-b.mesh = Mesh.from_shape(Box.from_width_height_depth(1, 1, 1))
-b.mesh.transform(Translation.from_vector([0, 0, 1]))
+b.geometry = Box.from_width_height_depth(1, 1, 1)
+b.add_transformation(Translation.from_vector([0, 0, 1]))
 
 assembly.add_part(a)
 assembly.add_part(b)
