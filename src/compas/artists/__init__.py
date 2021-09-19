aeeac4109c1870f656d72e2303789880cad2ec81
brgcode <vanmelet@ethz.ch>
2021-09-19 10:49:08 +0200

    base artist

---
+++ src/compas/artists/__init__.py
@@ -0,0 +1,26 @@
+"""
+********************************************************************************
+artists
+********************************************************************************
+
+.. currentmodule:: compas.artists
+
+Classes
+=======
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    Artist
+
+"""
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+from .artist import Artist  # noqa: F401
+
+__all__ = [
+    'Artist',
+]
--- src/compas/artists/artist.py
+++ src/compas/artists/artist.py
@@ -4,47 +4,24 @@ from __future__ import print_function
 
 from compas.plugins import pluggable
 
-_ITEM_ARTIST = {}
-
 
 @pluggable(category='factories')
 def new_artist(cls, *args, **kwargs):
-    pass
-
-
-# @plugin(category='factories', pluggable_name='new_artist')
-# def new_artist_rhino(cls, *args, **kwargs):
-#     data = args[0]
-#     cls = Artist.data_artist[type(data)]
-#     return super(Artist, cls).__new__(cls)
+    raise NotImplementedError
 
 
 class Artist(object):
     """Base class for all artists.
-
-    Parameters
-    ----------
-    item: :class:`compas.data.Data`
-        The data item.
-
-    Attributes
-    ----------
-    item: :class:`compas.data.Data`
-        The data item.
     """
+
+    ITEM_ARTIST = {}
 
     def __new__(cls, *args, **kwargs):
         return new_artist(cls, *args, **kwargs)
 
-    def __init__(self, item, *args, **kwargs):
-        super(Artist, self).__init__()
-        self.item = item
-        self.args = args
-        self.kwargs = kwargs
-
     @staticmethod
     def register(item_type, artist_type):
-        _ITEM_ARTIST[item_type] = artist_type
+        Artist.ITEM_ARTIST[item_type] = artist_type
 
     @staticmethod
     def build(item, **kwargs):
@@ -62,7 +39,7 @@ class Artist(object):
             An artist of the type matching the provided item according to an item-artist map.
             The map is created by registering item-artist type pairs using ``~BaseArtist.register``.
         """
-        artist_type = _ITEM_ARTIST[type(item)]
+        artist_type = Artist.ITEM_ARTIST[type(item)]
         artist = artist_type(item, **kwargs)
         return artist
 
