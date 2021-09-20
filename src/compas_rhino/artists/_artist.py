4bc62cfdc5ec990b3de339023f59c58425ea2257
brgcode <vanmelet@ethz.ch>
2021-09-20 10:31:36 +0200

    simplify base rhino artist

--- src/compas_rhino/artists/_artist.py
+++ src/compas_rhino/artists/_artist.py
@@ -8,23 +8,8 @@ from compas.artists import Artist
 
 class RhinoArtist(Artist):
     """Base class for all Rhino artists.
-
-    Attributes
-    ----------
-    guids : list
-        A list of the GUID of the Rhino objects created by the artist.
-
     """
 
-    def __init__(self):
-        self._guids = []
-
-    def redraw(self):
-        compas_rhino.rs.EnableRedraw(True)
-        compas_rhino.rs.Redraw()
-
-    def clear(self):
-        if not self._guids:
-            return
-        compas_rhino.delete_objects(self._guids)
-        self._guids = []
+    def clear_layer(self):
+        if self.layer:
+            compas_rhino.clear_layer(self.layer)
