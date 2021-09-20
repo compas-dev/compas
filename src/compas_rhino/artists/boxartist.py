11d296a8555c120e6bb38611865a6032e8f00865
brgcode <vanmelet@ethz.ch>
2021-09-20 11:54:01 +0200

    rebase box artist

--- src/compas_rhino/artists/boxartist.py
+++ src/compas_rhino/artists/boxartist.py
@@ -3,44 +3,30 @@ from __future__ import absolute_import
 from __future__ import division
 
 import compas_rhino
-from ._shapeartist import ShapeArtist
+from compas.artists import ShapeArtist
+from ._artist import RhinoArtist
 
 
-class BoxArtist(ShapeArtist):
+class BoxArtist(RhinoArtist, ShapeArtist):
     """Artist for drawing box shapes.
 
     Parameters
     ----------
     shape : :class:`compas.geometry.Box`
         A COMPAS box.
-
-    Notes
-    -----
-    See :class:`compas_rhino.artists.ShapeArtist` for all other parameters.
-
-    Examples
-    --------
-    .. code-block:: python
-
-        import random
-        from compas.geometry import Pointcloud
-        from compas.geometry import Box
-        from compas.utilities import i_to_rgb
-
-        import compas_rhino
-        from compas_rhino.artists import BoxArtist
-
-        pcl = Pointcloud.from_bounds(10, 10, 10, 100)
-        tpl = Box.from_width_height_depth(0.3, 0.3, 0.3)
-
-        compas_rhino.clear_layer("Test::BoxArtist")
-
-        for point in pcl.points:
-            box = tpl.copy()
-            box.frame.point = point
-            artist = BoxArtist(box, color=i_to_rgb(random.random()), layer="Test::BoxArtist")
-            artist.draw()
     """
+
+    def __init__(self, box, layer=None):
+        super(BoxArtist, self).__init__(box)
+        self.layer = layer
+
+    @property
+    def box(self):
+        return self.shape
+
+    @box.setter
+    def box(self, box):
+        self.shape = box
 
     def draw(self, show_vertices=False, show_edges=False, show_faces=True, join_faces=True):
         """Draw the box associated with the artist.
@@ -78,5 +64,4 @@ class BoxArtist(ShapeArtist):
             else:
                 polygons = [{'points': [vertices[index] for index in face], 'color': self.color, 'name': self.name} for face in faces]
                 guids += compas_rhino.draw_faces(polygons, layer=self.layer, clear=False, redraw=False)
-        self._guids = guids
         return guids
