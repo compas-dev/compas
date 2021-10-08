fb7a8d7b3e77edc6112f30fb8604a4829ec14b3c
brgcode <vanmelet@ethz.ch>
2021-10-08 16:16:47 +0200

    remove comments

--- src/compas/datastructures/network/planarity.py
+++ src/compas/datastructures/network/planarity.py
@@ -8,15 +8,10 @@ from math import pi
 
 from itertools import product
 
-# import compas
-
 from compas.geometry import angle_vectors_xy
 from compas.geometry import is_intersection_segment_segment_xy
 from compas.geometry import is_ccw_xy
 from compas.geometry import subtract_vectors_xy
-
-# if not compas.IPY:
-#     import planarity
 
 
 __all__ = [
