6ce01dcfe5d535408bb079a4ceb499daa5416acc
brgcode <vanmelet@ethz.ch>
2021-11-02 10:17:34 +0100

    add curves and surfaces

---
+++ src/compas/geometry/curves/curve.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+from compas.data import Data
+
+
+class Curve(Data):
+    """Base class for curves."""
---
+++ src/compas/geometry/surfaces/surface.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+from compas.data import Data
+
+
+class Surface(Data):
+    """Base class for surfaces."""
