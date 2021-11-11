424f45f51d60a6de416dffbd2dee698785f78eff
brgcode <vanmelet@ethz.ch>
2021-11-11 08:55:31 +0100

    compatibility with related minors

--- src/compas_rhino/geometry/__init__.py
+++ src/compas_rhino/geometry/__init__.py
@@ -8,5 +8,39 @@ geometry
 """
 from __future__ import absolute_import
 
+from compas_rhino.conversions import RhinoGeometry
 
-__all__ = []
+from compas_rhino.conversions import RhinoBox
+from compas_rhino.conversions import RhinoCircle
+from compas_rhino.conversions import RhinoCone
+from compas_rhino.conversions import RhinoCurve
+from compas_rhino.conversions import RhinoCylinder
+from compas_rhino.conversions import RhinoEllipse
+from compas_rhino.conversions import RhinoLine
+from compas_rhino.conversions import RhinoMesh
+from compas_rhino.conversions import RhinoPlane
+from compas_rhino.conversions import RhinoPoint
+from compas_rhino.conversions import RhinoPolyline
+from compas_rhino.conversions import RhinoSphere
+from compas_rhino.conversions import RhinoSurface
+from compas_rhino.conversions import RhinoVector
+
+BaseRhinoGeometry = RhinoGeometry
+
+__all__ = [
+    'RhinoGeometry',
+    'RhinoBox',
+    'RhinoCircle',
+    'RhinoCone',
+    'RhinoCurve',
+    'RhinoCylinder',
+    'RhinoEllipse',
+    'RhinoLine',
+    'RhinoMesh',
+    'RhinoPlane',
+    'RhinoPoint',
+    'RhinoPolyline',
+    'RhinoSphere',
+    'RhinoSurface',
+    'RhinoVector',
+]
