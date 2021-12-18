ca9b957cd72f6e0f46beab5cd9478a6589bbe150
brgcode <van.mele@arch.ethz.ch>
2021-12-18 14:36:00 +0100

    file object docstrings

--- src/compas/files/off.py
+++ src/compas/files/off.py
@@ -5,12 +5,6 @@ from __future__ import print_function
 import compas
 from compas import _iotools
 
-__all__ = [
-    'OFF',
-    'OFFReader',
-    'OFFWriter',
-]
-
 
 class OFF(object):
     """Read and write files in OFF format.
@@ -21,6 +15,10 @@ class OFF(object):
     * http://www.geomview.org/docs/html/OFF.html
     * http://segeval.cs.princeton.edu/public/off_format.html
 
+    See Also
+    --------
+    * :class:`OFFReader`
+    * :class:`OFFWriter`
 
     """
 
@@ -31,6 +29,7 @@ class OFF(object):
         self._writer = None
 
     def read(self):
+        """Read and parse the contents of the file."""
         self._reader = OFFReader(self.filepath)
         self._reader.open()
         self._reader.pre()
@@ -39,54 +38,67 @@ class OFF(object):
         self._is_read = True
 
     def write(self, mesh, **kwargs):
+        """Write a mesh to the file.
+
+        Parameters
+        ----------
+        mesh : :class:`compas.datastructures.Mesh`
+            The mesh.
+        author : :obj:`str`, optional
+            The author name to include in the header.
+        email : :obj:`str`, optional
+            The email of the author to include in the header.
+        date : :obj:`str`, optional
+            The date to include in the header.
+        precision : :obj:`str`, optional
+            COMPAS precision specification for parsing geometric data.
+        """
         self._writer = OFFWriter(self.filepath, mesh, **kwargs)
         self._writer.write()
 
     @property
     def reader(self):
+        """:class:`OFFReader` - A OFF file reader."""
         if not self._is_read:
             self.read()
         return self._reader
 
 
 class OFFReader(object):
-    """Read the contents of an *obj* file.
+    """Class for reading raw geometric data from OFF files.
 
     Parameters
     ----------
     filepath : path string, file-like object or URL string
         A path, a file-like object or a URL pointing to a file.
 
-    Attributes
-    ----------
-    vertices : list
-        Vertex coordinates.
-    faces : list
-        Face objects, referencing the list of vertices.
-
     Notes
     -----
     The OFF reader currently only supports reading of vertices and faces of polygon meshes.
-
-    References
-    ----------
-
     """
 
     def __init__(self, filepath):
         self.filepath = filepath
         self.content = None
         self.vertices = []
+        """List[List[:obj:`float`, :obj:`float`, :obj:`float`]] -
+        List of lists of vertex coordinates.
+        """
         self.faces = []
+        """List[List[:obj:`int`] -
+        List of lists of references to vertex coordinates.
+        """
         self.v = 0
         self.f = 0
         self.e = 0
 
     def open(self):
+        """Open the file and read its contents."""
         with _iotools.open_file(self.filepath, 'r') as f:
             self.content = f.readlines()
 
     def pre(self):
+        """Pre-process the contents."""
         lines = []
         is_continuation = False
         needs_decode = None
@@ -111,6 +123,7 @@ class OFFReader(object):
         self.content = iter(lines)
 
     def post(self):
+        """Post-process the contents."""
         pass
 
     def read(self):
@@ -176,6 +189,25 @@ class OFFReader(object):
 
 
 class OFFWriter(object):
+    """Class for writing geometric data to a OBJ file.
+    The parser converts the raw geometric data of the file
+    into corresponding COMPAS geometry objects and data structures.
+
+    Parameters
+    ----------
+    filepath : path string, file-like object or URL string
+        A path, a file-like object or a URL pointing to a file.
+    mesh : :class:`compas.datastructures.Mesh`
+        Mesh to write to the file.
+    author : :obj:`str`, optional
+        The author name to include in the header.
+    email : :obj:`str`, optional
+        The email of the author to include in the header.
+    date : :obj:`str`, optional
+        The date to include in the header.
+    precision : :obj:`str`, optional
+        COMPAS precision specification for parsing geometric data.
+    """
 
     def __init__(self, filepath, mesh, author=None, email=None, date=None, precision=None):
         self.filepath = filepath
@@ -191,12 +223,14 @@ class OFFWriter(object):
         self.file = None
 
     def write(self):
+        """Write the meshes to the file."""
         with _iotools.open_file(self.filepath, 'w') as self.file:
             self.write_header()
             self.write_vertices()
             self.write_faces()
 
     def write_header(self):
+        """Write the header info."""
         self.file.write("OFF\n")
         if self.author:
             self.file.write("# author: {}\n".format(self.author))
@@ -207,11 +241,13 @@ class OFFWriter(object):
         self.file.write("{} {} {}\n".format(self.v, self.f, self.e))
 
     def write_vertices(self):
+        """Write the vertices."""
         for key in self.mesh.vertices():
             x, y, z = self.mesh.vertex_coordinates(key)
             self.file.write(self.vertex_tpl.format(x, y, z))
 
     def write_faces(self):
+        """Write the faces."""
         key_index = self.mesh.key_index()
         for fkey in self.mesh.faces():
             vertices = self.mesh.face_vertices(fkey)
