99b81176fe78bf0baac346658e5e1b40bd0e01db
brgcode <van.mele@arch.ethz.ch>
2022-01-11 10:20:56 +0100

    base

--- src/compas_plotters/artists/artist.py
+++ src/compas_plotters/artists/artist.py
@@ -4,7 +4,16 @@ from compas.artists import Artist
 
 
 class PlotterArtist(Artist):
-    """Base class for all plotter artists."""
+    """Base class for all plotter artists.
+
+    Attributes
+    ----------
+    plotter : :class:`compas_plotters.plotter.Plotter`, read-only
+        A plotter instance.
+    data : list[[float, float]]
+        The geometrical data points visualized with the plotter.
+
+    """
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -17,16 +26,31 @@ class PlotterArtist(Artist):
             self._plotter = Plotter()
         return self._plotter
 
-    def viewbox(self):
-        xlim = self.plotter.axes.get_xlim()
-        ylim = self.plotter.axes.get_ylim()
-        xmin, xmax = xlim
-        ymin, ymax = ylim
-        return [[xmin, ymin], [xmax, ymin], [xmax, ymax], [xmin, ymax]]
-
     @abstractproperty
     def data(self):
         raise NotImplementedError
 
+    def viewbox(self):
+        """Compute the bounds of the current view.
+
+        Returns
+        -------
+        tuple[[float, float], [float, float], [float, float], [float]]
+            Coordinates of the corners of the 2D view box.
+
+        """
+        xlim = self.plotter.axes.get_xlim()
+        ylim = self.plotter.axes.get_ylim()
+        xmin, xmax = xlim
+        ymin, ymax = ylim
+        return [xmin, ymin], [xmax, ymin], [xmax, ymax], [xmin, ymax]
+
     def update_data(self) -> None:
+        """Update the data limits of the plotting axes using the visualization data.
+
+        Returns
+        -------
+        None
+
+        """
         self.plotter.axes.update_datalim(self.data)
