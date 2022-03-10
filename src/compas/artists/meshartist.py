92be97acb780f394e2137a41084f2681cbf100d9
brgcode <van.mele@arch.ethz.ch>
2022-03-10 13:21:44 +0100

    pickle serialization for objects with colordict

--- src/compas/artists/meshartist.py
+++ src/compas/artists/meshartist.py
@@ -120,13 +120,36 @@ class MeshArtist(Artist):
         self._edgelabelcollection = None
         self._facelabelcollection = None
 
-    @property
-    def settings(self):
-        return {}
+    def __getstate__(self):
+        """Return a serializable state of the artist.
 
-    @settings.setter
-    def settings(self, settings):
-        pass
+        The conversion of the color dicts is necessary because of te lambda function in the default dicts
+        created by the color dict descriptors.
+
+        """
+        dictcopy = self.__dict__.copy()
+        if self.__dict__['_vertex_color']:
+            dictcopy['_vertex_color'] = dict(self.__dict__['_vertex_color'])
+        if self.__dict__['_edge_color']:
+            dictcopy['_edge_color'] = dict(self.__dict__['_edge_color'])
+        if self.__dict__['_face_color']:
+            dictcopy['_face_color'] = dict(self.__dict__['_face_color'])
+        return {'__dict__': dictcopy}
+
+    def __setstate__(self, state):
+        """Assign a deserialized state to the artist and recreate the descriptors.
+
+        The creation of color dicts needs to be triggered manually such that the dicts are converted to default dicts.
+
+        """
+        dictcopy = state['__dict__'].copy()
+        dictcopy['_vertex_color'] = None
+        dictcopy['_edge_color'] = None
+        dictcopy['_face_color'] = None
+        self.__dict__.update(dictcopy)
+        self.vertex_color = state['__dict__']['_vertex_color']
+        self.edge_color = state['__dict__']['_edge_color']
+        self.face_color = state['__dict__']['_face_color']
 
     @property
     def mesh(self):
