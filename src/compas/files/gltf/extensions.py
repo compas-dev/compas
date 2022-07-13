325db3ec9cd11c2645fd7f9b15500d7f91325646
Romana Rust <rust@arch.ethz.ch>
2022-07-13 12:40:01 +0200

    black_formatter + KHR_materials_specular

--- src/compas/files/gltf/extensions.py
+++ src/compas/files/gltf/extensions.py
@@ -9,13 +9,15 @@ class KHR_materials_transmission(BaseGLTFDataClass):
     https://github.com/KhronosGroup/glTF/blob/master/extensions/2.0/Khronos/KHR_materials_transmission
     """
 
-    key = 'KHR_materials_transmission'
+    key = "KHR_materials_transmission"
 
-    def __init__(self,
-                 transmission_factor=None,
-                 transmission_texture=None,
-                 extensions=None,
-                 extras=None):
+    def __init__(
+        self,
+        transmission_factor=None,
+        transmission_texture=None,
+        extensions=None,
+        extras=None,
+    ):
         super(KHR_materials_transmission, self).__init__(extras, extensions)
         self.transmission_factor = transmission_factor
         self.transmission_texture = transmission_texture
@@ -23,23 +25,86 @@ class KHR_materials_transmission(BaseGLTFDataClass):
     def to_data(self, texture_index_by_key, **kwargs):
         dct = {}
         if self.transmission_factor is not None:
-            dct['transmissionFactor'] = self.transmission_factor
+            dct["transmissionFactor"] = self.transmission_factor
         if self.transmission_texture is not None:
-            dct['transmissionTexture'] = self.transmission_texture.to_data(texture_index_by_key)
+            dct["transmissionTexture"] = self.transmission_texture.to_data(
+                texture_index_by_key
+            )
         if self.extras is not None:
-            dct['extras'] = self.extras
+            dct["extras"] = self.extras
         if self.extensions is not None:
-            dct['extensions'] = self.extensions_to_data()
+            dct["extensions"] = self.extensions_to_data()
         return dct
 
     @classmethod
     def from_data(cls, dct):
         if dct is None:
             return None
-        return cls(transmission_factor=dct.get('transmissionFactor'),
-                   transmission_texture=TextureInfoData.from_data(dct.get('transmissionTexture')),
-                   extensions=cls.extensions_from_data(dct.get('extensions')),
-                   extras=dct.get('extras'))
+        return cls(
+            transmission_factor=dct.get("transmissionFactor"),
+            transmission_texture=TextureInfoData.from_data(
+                dct.get("transmissionTexture")
+            ),
+            extensions=cls.extensions_from_data(dct.get("extensions")),
+            extras=dct.get("extras"),
+        )
+
+
+class KHR_materials_specular(BaseGLTFDataClass):
+    """glTF extension that defines the optical transmission of a material.
+
+    https://github.com/KhronosGroup/glTF/tree/main/extensions/2.0/Khronos/KHR_materials_specular
+    """
+
+    key = "KHR_materials_specular"
+
+    def __init__(
+        self,
+        specular_factor=None,
+        specular_texture=None,
+        specular_color_factor=None,
+        specular_color_texture=None,
+        extensions=None,
+        extras=None,
+    ):
+        super(KHR_materials_specular, self).__init__(extras, extensions)
+        self.specular_factor = specular_factor
+        self.specular_texture = specular_texture
+        self.specular_color_factor = specular_color_factor
+        self.specular_color_texture = specular_color_texture
+
+    def to_data(self, texture_index_by_key, **kwargs):
+        dct = {}
+        if self.specular_factor is not None:
+            dct["specularFactor"] = self.specular_factor
+        if self.specular_texture is not None:
+            dct["specularTexture"] = self.specular_texture.to_data(texture_index_by_key)
+        if self.specular_color_factor is not None:
+            dct["specularColorFactor"] = self.specular_color_factor
+        if self.specular_color_texture is not None:
+            dct["specularColorTexture"] = self.specular_color_texture.to_data(
+                texture_index_by_key
+            )
+        if self.extras is not None:
+            dct["extras"] = self.extras
+        if self.extensions is not None:
+            dct["extensions"] = self.extensions_to_data()
+        return dct
+
+    @classmethod
+    def from_data(cls, dct):
+        if dct is None:
+            return None
+        return cls(
+            specular_factor=dct.get("specularFactor"),
+            specular_texture=TextureInfoData.from_data(dct.get("specularTexture")),
+            specular_color_factor=dct.get("specularColorFactor"),
+            specular_color_texture=TextureInfoData.from_data(
+                dct.get("specularColorTexture")
+            ),
+            extensions=cls.extensions_from_data(dct.get("extensions")),
+            extras=dct.get("extras"),
+        )
 
 
 class KHR_materials_clearcoat(BaseGLTFDataClass):
@@ -48,16 +113,18 @@ class KHR_materials_clearcoat(BaseGLTFDataClass):
     https://github.com/KhronosGroup/glTF/blob/master/extensions/2.0/Khronos/KHR_materials_clearcoat
     """
 
-    key = 'KHR_materials_clearcoat'
+    key = "KHR_materials_clearcoat"
 
-    def __init__(self,
-                 clearcoat_factor=None,
-                 clearcoat_texture=None,
-                 clearcoat_roughness_factor=None,
-                 clearcoat_roughness_texture=None,
-                 clearcoat_normal_texture=None,
-                 extensions=None,
-                 extras=None):
+    def __init__(
+        self,
+        clearcoat_factor=None,
+        clearcoat_texture=None,
+        clearcoat_roughness_factor=None,
+        clearcoat_roughness_texture=None,
+        clearcoat_normal_texture=None,
+        extensions=None,
+        extras=None,
+    ):
         super(KHR_materials_clearcoat, self).__init__(extras, extensions)
         self.clearcoat_factor = clearcoat_factor
         self.clearcoat_texture = clearcoat_texture
@@ -68,33 +135,44 @@ class KHR_materials_clearcoat(BaseGLTFDataClass):
     def to_data(self, texture_index_by_key, **kwargs):
         dct = {}
         if self.clearcoat_factor is not None:
-            dct['clearcoatFactor'] = self.clearcoat_factor
+            dct["clearcoatFactor"] = self.clearcoat_factor
         if self.clearcoat_texture is not None:
-            dct['clearcoatTexture'] = self.clearcoat_texture.to_data(texture_index_by_key)
+            dct["clearcoatTexture"] = self.clearcoat_texture.to_data(
+                texture_index_by_key
+            )
         if self.clearcoat_roughness_factor is not None:
-            dct['clearcoatRoughnessFactor'] = self.clearcoat_roughness_factor
+            dct["clearcoatRoughnessFactor"] = self.clearcoat_roughness_factor
         if self.clearcoat_roughness_texture is not None:
-            dct['clearcoatRoughnessTexture'] = self.clearcoat_roughness_texture.to_data(texture_index_by_key)
+            dct["clearcoatRoughnessTexture"] = self.clearcoat_roughness_texture.to_data(
+                texture_index_by_key
+            )
         if self.clearcoat_normal_texture is not None:
-            dct['clearcoatNormalTexture'] = self.clearcoat_normal_texture.to_data(texture_index_by_key)
+            dct["clearcoatNormalTexture"] = self.clearcoat_normal_texture.to_data(
+                texture_index_by_key
+            )
         if self.extras is not None:
-            dct['extras'] = self.extras
+            dct["extras"] = self.extras
         if self.extensions is not None:
-            dct['extensions'] = self.extensions_to_data()
+            dct["extensions"] = self.extensions_to_data()
         return dct
 
     @classmethod
     def from_data(cls, dct):
         if dct is None:
             return None
-        return cls(clearcoat_factor=dct.get('clearcoatFactor'),
-                   clearcoat_texture=TextureInfoData.from_data(dct.get('clearcoatTexture')),
-                   clearcoat_roughness_factor=dct.get('clearcoatRoughnessFactor'),
-                   clearcoat_roughness_texture=TextureInfoData.from_data(dct.get('clearcoatRoughnessTexture')),
-                   clearcoat_normal_texture=NormalTextureInfoData.from_data(dct.get('clearcoatNormalTexture')),
-                   extensions=cls.extensions_from_data(dct.get('extensions')),
-                   extras=dct.get('extras'),
-                   )
+        return cls(
+            clearcoat_factor=dct.get("clearcoatFactor"),
+            clearcoat_texture=TextureInfoData.from_data(dct.get("clearcoatTexture")),
+            clearcoat_roughness_factor=dct.get("clearcoatRoughnessFactor"),
+            clearcoat_roughness_texture=TextureInfoData.from_data(
+                dct.get("clearcoatRoughnessTexture")
+            ),
+            clearcoat_normal_texture=NormalTextureInfoData.from_data(
+                dct.get("clearcoatNormalTexture")
+            ),
+            extensions=cls.extensions_from_data(dct.get("extensions")),
+            extras=dct.get("extras"),
+        )
 
 
 class KHR_Texture_Transform(BaseGLTFDataClass):
@@ -103,15 +181,17 @@ class KHR_Texture_Transform(BaseGLTFDataClass):
     https://github.com/KhronosGroup/glTF/tree/master/extensions/2.0/Khronos/KHR_texture_transform
     """
 
-    key = 'KHR_texture_transform'
+    key = "KHR_texture_transform"
 
-    def __init__(self,
-                 offset=None,
-                 rotation=None,
-                 scale=None,
-                 tex_coord=None,
-                 extensions=None,
-                 extras=None):
+    def __init__(
+        self,
+        offset=None,
+        rotation=None,
+        scale=None,
+        tex_coord=None,
+        extensions=None,
+        extras=None,
+    ):
         super(KHR_Texture_Transform, self).__init__(extras, extensions)
         self.offset = offset  # or [0.0, 0.0]
         self.rotation = rotation  # or 0.
@@ -121,81 +201,90 @@ class KHR_Texture_Transform(BaseGLTFDataClass):
     def to_data(self, **kwargs):
         dct = {}
         if self.offset is not None:
-            dct['offset'] = self.offset
+            dct["offset"] = self.offset
         if self.rotation is not None:
-            dct['rotation'] = self.rotation
+            dct["rotation"] = self.rotation
         if self.scale is not None:
-            dct['scale'] = self.scale
+            dct["scale"] = self.scale
         if self.tex_coord is not None:
-            dct['texCoord'] = self.tex_coord
+            dct["texCoord"] = self.tex_coord
         if self.extras is not None:
-            dct['extras'] = self.extras
+            dct["extras"] = self.extras
         if self.extensions is not None:
-            dct['extensions'] = self.extensions_to_data()
+            dct["extensions"] = self.extensions_to_data()
         return dct
 
     @classmethod
     def from_data(cls, dct):
         if dct is None:
             return None
-        return cls(offset=dct.get('offset'),
-                   rotation=dct.get('rotation'),
-                   scale=dct.get('scale'),
-                   tex_coord=dct.get('texCoord'),
-                   extensions=cls.extensions_from_data(dct.get('extensions')),
-                   extras=dct.get('extras'))
+        return cls(
+            offset=dct.get("offset"),
+            rotation=dct.get("rotation"),
+            scale=dct.get("scale"),
+            tex_coord=dct.get("texCoord"),
+            extensions=cls.extensions_from_data(dct.get("extensions")),
+            extras=dct.get("extras"),
+        )
 
 
 class KHR_materials_pbrSpecularGlossiness(BaseGLTFDataClass):
-    """glTF extension that defines the specular-glossiness material model from Physically-Based Rendering (PBR) methodology.
-    """
+    """glTF extension that defines the specular-glossiness material model from Physically-Based Rendering (PBR) methodology."""
 
-    key = 'KHR_materials_pbrSpecularGlossiness'
+    key = "KHR_materials_pbrSpecularGlossiness"
 
-    def __init__(self,
-                 diffuse_factor=None,
-                 diffuse_texture=None,
-                 specular_factor=None,
-                 glossiness_factor=None,
-                 specular_glossiness_texture=None,
-                 extensions=None,
-                 extras=None):
+    def __init__(
+        self,
+        diffuse_factor=None,
+        diffuse_texture=None,
+        specular_factor=None,
+        glossiness_factor=None,
+        specular_glossiness_texture=None,
+        extensions=None,
+        extras=None,
+    ):
         super(KHR_materials_pbrSpecularGlossiness, self).__init__(extras, extensions)
         self.diffuse_factor = diffuse_factor or [1.0, 1.0, 1.0, 1.0]
         self.diffuse_texture = diffuse_texture
         self.specular_factor = specular_factor or [1.0, 1.0, 1.0]
-        self.glossiness_factor = glossiness_factor or 1.
+        self.glossiness_factor = glossiness_factor or 1.0
         self.specular_glossiness_texture = specular_glossiness_texture
 
     def to_data(self, texture_index_by_key, **kwargs):
         dct = {}
         if self.diffuse_factor is not None:
-            dct['diffuseFactor'] = self.diffuse_factor
+            dct["diffuseFactor"] = self.diffuse_factor
         if self.diffuse_texture is not None:
-            dct['diffuseTexture'] = self.diffuse_texture.to_data(texture_index_by_key)
+            dct["diffuseTexture"] = self.diffuse_texture.to_data(texture_index_by_key)
         if self.specular_factor is not None:
-            dct['specularFactor'] = self.specular_factor
+            dct["specularFactor"] = self.specular_factor
         if self.glossiness_factor is not None:
-            dct['glossinessFactor'] = self.glossiness_factor
+            dct["glossinessFactor"] = self.glossiness_factor
         if self.specular_glossiness_texture is not None:
-            dct['specularGlossinessTexture'] = self.specular_glossiness_texture.to_data(texture_index_by_key)
+            dct["specularGlossinessTexture"] = self.specular_glossiness_texture.to_data(
+                texture_index_by_key
+            )
         if self.extras is not None:
-            dct['extras'] = self.extras
+            dct["extras"] = self.extras
         if self.extensions is not None:
-            dct['extensions'] = self.extensions_to_data()
+            dct["extensions"] = self.extensions_to_data()
         return dct
 
     @classmethod
     def from_data(cls, dct):
         if dct is None:
             return None
-        return cls(diffuse_factor=dct.get('diffuseFactor'),
-                   diffuse_texture=TextureInfoData.from_data(dct.get('diffuseTexture')),
-                   specular_factor=dct.get('specularFactor'),
-                   glossiness_factor=dct.get('glossinessFactor'),
-                   specular_glossiness_texture=TextureInfoData.from_data(dct.get('specularGlossinessTexture')),
-                   extensions=cls.extensions_from_data(dct.get('extensions')),
-                   extras=dct.get('extras'))
+        return cls(
+            diffuse_factor=dct.get("diffuseFactor"),
+            diffuse_texture=TextureInfoData.from_data(dct.get("diffuseTexture")),
+            specular_factor=dct.get("specularFactor"),
+            glossiness_factor=dct.get("glossinessFactor"),
+            specular_glossiness_texture=TextureInfoData.from_data(
+                dct.get("specularGlossinessTexture")
+            ),
+            extensions=cls.extensions_from_data(dct.get("extensions")),
+            extras=dct.get("extras"),
+        )
 
 
 SUPPORTED_EXTENSIONS = {
@@ -203,4 +292,5 @@ SUPPORTED_EXTENSIONS = {
     KHR_materials_clearcoat.key: KHR_materials_clearcoat,
     KHR_Texture_Transform.key: KHR_Texture_Transform,
     KHR_materials_pbrSpecularGlossiness.key: KHR_materials_pbrSpecularGlossiness,
+    KHR_materials_specular.key: KHR_materials_specular,
 }
