a466cb9b408a2ee595359f9dc85cc1788170da69
Romana Rust <rust@arch.ethz.ch>
2022-07-13 18:11:57 +0200

    added check_extensions_texture_recursively and update gltf content example with texture

--- src/compas/files/gltf/gltf_content.py
+++ src/compas/files/gltf/gltf_content.py
@@ -6,9 +6,10 @@ from compas.files.gltf.gltf_mesh import GLTFMesh
 from compas.files.gltf.gltf_node import GLTFNode
 from compas.files.gltf.gltf_scene import GLTFScene
 from compas.files.gltf.helpers import get_weighted_mesh_vertices
+from compas.files.gltf.data_classes import TextureInfoData
+from compas.files.gltf.data_classes import BaseGLTFDataClass
 from compas.geometry import transform_points
 from compas.geometry import multiply_matrices
-from compas.utilities import download_file_from_remote
 
 
 class GLTFContent(object):
@@ -77,7 +78,7 @@ class GLTFContent(object):
         def visit(key):
             node = self.nodes[key]
             if key in visited_nodes:
-                raise Exception('Nodes do not form a rooted forest.')
+                raise Exception("Nodes do not form a rooted forest.")
             visited_nodes.add(key)
             for child_key in node.children:
                 visit(child_key)
@@ -159,18 +160,20 @@ class GLTFContent(object):
         self._remove_unvisited(material_visit_log, self.materials)
 
         # walk through existing materials and update textures visit log
+        def check_extensions_texture_recursively(item):
+            # get the extensions that are in the attributes
+            for a in dir(item):
+                if not a.startswith("__") and not callable(getattr(item, a)):
+                    if isinstance(getattr(item, a), TextureInfoData):
+                        texture_visit_log[getattr(item, a).index] = True
+                    elif isinstance(getattr(item, a), BaseGLTFDataClass):
+                        check_extensions_texture_recursively(getattr(item, a))
+            if item.extensions is not None:
+                for _, e in item.extensions.items():
+                    check_extensions_texture_recursively(e)
+
         for material in self.materials.values():
-            if material.normal_texture is not None:
-                texture_visit_log[material.normal_texture.index] = True
-            if material.occlusion_texture is not None:
-                texture_visit_log[material.occlusion_texture.index] = True
-            if material.emissive_texture is not None:
-                texture_visit_log[material.emissive_texture.index] = True
-            if material.pbr_metallic_roughness is not None:
-                if material.pbr_metallic_roughness.base_color_texture is not None:
-                    texture_visit_log[material.pbr_metallic_roughness.base_color_texture.index] = True
-                if material.pbr_metallic_roughness.metallic_roughness_texture is not None:
-                    texture_visit_log[material.pbr_metallic_roughness.metallic_roughness_texture.index] = True
+            check_extensions_texture_recursively(material)
 
         # remove unvisited textures
         self._remove_unvisited(texture_visit_log, self.textures)
@@ -228,8 +231,7 @@ class GLTFContent(object):
                 for child_key in cur.children:
                     child = self.nodes[child_key]
                     child.transform = multiply_matrices(
-                        cur.transform,
-                        child.matrix or child.get_matrix_from_trs()
+                        cur.transform, child.matrix or child.get_matrix_from_trs()
                     )
                     child.position = transform_points([origin], child.transform)[0]
                     queue.append(child_key)
@@ -301,8 +303,6 @@ class GLTFContent(object):
         while key in self.materials:
             key += 1
         self.materials[key] = material
-        # TODO if extensions must be enabled
-        # extensionsUsed = {};
         return key
 
     def add_texture(self, texture):
@@ -319,11 +319,10 @@ class GLTFContent(object):
         while key in self.textures:
             key += 1
         self.textures[key] = texture
-        # TODO if extensions must be enabled
         return key
 
     def add_image(self, image):
-        """Adds a texture to the content.
+        """Adds a image to the content.
 
         Parameters
         ----------
@@ -336,7 +335,6 @@ class GLTFContent(object):
         while key in self.images:
             key += 1
         self.images[key] = image
-        # TODO if extensions must be enabled
         return key
 
     def get_material_index_by_name(self, name):
@@ -384,7 +382,7 @@ class GLTFContent(object):
         :class:`~compas.files.GLTFNode`
         """
         if scene not in self.scenes.values():
-            raise Exception('Cannot find scene.')
+            raise Exception("Cannot find scene.")
         node = GLTFNode(self, node_name, node_extras)
         scene.children.append(node.key)
         return node
@@ -474,7 +472,7 @@ class GLTFContent(object):
         -------
         tuple
         """
-        positions_dict = {'root': [0, 0, 0]}
+        positions_dict = {"root": [0, 0, 0]}
         edges_list = []
 
         def visit(node, key):
@@ -483,7 +481,7 @@ class GLTFContent(object):
                 edges_list.append((key, child_key))
                 visit(self.nodes[child_key], child_key)
 
-        visit(scene, 'root')
+        visit(scene, "root")
 
         return positions_dict, edges_list
 
@@ -492,78 +490,137 @@ class GLTFContent(object):
 # Main
 # ==============================================================================
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     import os
     import compas
+    import urllib
 
+    # https://github.com/Stykka/glTF-Bin
+
+    from compas.geometry import Box, Frame, Point
     from compas.datastructures import Mesh
+    from compas.utilities import download_file_from_remote
     from compas.files.gltf.gltf import GLTF
-    from compas.files.gltf.data_classes import MaterialData, PBRMetallicRoughnessData, TextureInfoData, ImageData, TextureData
-    from compas.files.gltf.extensions import KHR_materials_pbrSpecularGlossiness, KHR_Texture_Transform
+    from compas.files.gltf.data_classes import (
+        MaterialData,
+        PBRMetallicRoughnessData,
+        TextureInfoData,
+        ImageData,
+        TextureData,
+        MineType,
+        BaseGLTFDataClass,
+    )
+    from compas.files.gltf.extensions import (
+        KHR_materials_pbrSpecularGlossiness,
+        KHR_Texture_Transform,
+    )
 
-    source = 'https://raw.githubusercontent.com/ros-industrial/abb/kinetic-devel/abb_irb6600_support/meshes/irb6640/visual/link_1.stl'
-    stl_filepath = os.path.join(compas.APPDATA, 'data', 'meshes', 'ros', 'link_1.stl')
+    dirname = os.path.join(compas.APPDATA, "data", "gltfs")
+    gltf_filepath = os.path.join(dirname, "compas.gltf")
 
-    download_file_from_remote(source, stl_filepath, overwrite=False)
+    image_uri = "compas_icon_white.png"
+    image_file = os.path.join(dirname, image_uri)
+    try:
+        download_file_from_remote(
+            "https://compas.dev/images/compas_icon_white.png", image_file
+        )
+    except urllib.error.HTTPError:
+        pass
 
-    gltf_filepath = os.path.join(compas.APPDATA, 'data', 'gltfs', 'double_link_1.gltf')
-
-    mesh = Mesh.from_stl(stl_filepath)
     cnt = GLTFContent()
     scene = cnt.add_scene()
-    node_1 = scene.add_child(node_name='Node1')
-    mesh_data = node_1.add_mesh(mesh)
-    node_2 = node_1.add_child(child_name='Node2')
-    node_2.translation = [0, 0, 5]
-    node_2.add_mesh(mesh_data.key)
-
-    imagefile = r"C:\Users\rustr\Desktop\IDL\IDL\Cara_Shedland_01_edited.jpg"
 
     # image's uri should be the relative path to the image from the filepath given at the time of export,
     # so if the image will sit in the same directory as the resultant gltf, the uri is just the name of the file.
     # it's the exporter's job to manage how things are stored in the buffer, and it would only store image data
     # in the buffer if it is exporting as glb. otherwise the uri will just stay the relative path to the image
     # and the gltf only makes sense when bundled with these external files.
-    image_data = ImageData(name="Cara_Shedland_01_edited.jpg", mime_type="image/jpeg", uri="Cara_Shedland_01_edited.jpg")
-
-    imageIdx = cnt.add_image(image_data)
+    image_data = ImageData(
+        name=image_uri,
+        mime_type=MineType.PNG,
+        uri=image_file,
+    )
+    image_idx = cnt.add_image(image_data)
     # TextureData.source takes the key of the ImageData that it should use as 'source'
-    texture = TextureData(source=imageIdx)
-    textureIdx = cnt.add_texture(texture)
+    texture = TextureData(source=image_idx)
+    texture_idx = cnt.add_texture(texture)
 
-    # create Material
+    texture = TextureData(source=image_idx)
+    texture_idx2 = cnt.add_texture(texture)
+
     material = MaterialData()
-    material.name = 'Cara_03 Shedland'
+    material.name = "Texture"
     material.pbr_metallic_roughness = PBRMetallicRoughnessData()
-    material.pbr_metallic_roughness.base_color_factor = [0.980392158, 0.980392158, 0.980392158, 1.0]
-    material.pbr_metallic_roughness.metallic_factor = 0.
-    material.pbr_metallic_roughness.roughness_factor = 0.5
-    material.double_sided = True
+    material.pbr_metallic_roughness.metallic_factor = 0.0
+    material.pbr_metallic_roughness.base_color_texture = TextureInfoData(
+        index=texture_idx
+    )
+    material_key = cnt.add_material(material)
+
+    # add extension
     pbr_specular_glossiness = KHR_materials_pbrSpecularGlossiness()
-    pbr_specular_glossiness.diffuse_factor = [0.980392158, 0.980392158, 0.980392158, 1.0]
+    pbr_specular_glossiness.diffuse_factor = [
+        0.980392158,
+        0.980392158,
+        0.980392158,
+        1.0,
+    ]
     pbr_specular_glossiness.specular_factor = [0.0, 0.0, 0.0]
-    pbr_specular_glossiness.glossiness_factor = 0.
+    pbr_specular_glossiness.glossiness_factor = 0.0
     texture_transform = KHR_Texture_Transform()
-    texture_transform.rotation = 0.
-    texture_transform.scale = [70.0, 70.0]
+    texture_transform.rotation = 0.0
+    texture_transform.scale = [2.0, 2.0]
     # same here, TextureInfoData uses the key of the TextureData
-    pbr_specular_glossiness.diffuse_texture = TextureInfoData(textureIdx)
+    pbr_specular_glossiness.diffuse_texture = TextureInfoData(texture_idx2)
     pbr_specular_glossiness.diffuse_texture.add_extension(texture_transform)
     material.add_extension(pbr_specular_glossiness)
 
-    cnt.add_material(material)
+    # add box
+    box = Box(Frame.worldXY(), 1, 1, 1)
+    mesh = Mesh.from_shape(box)
+    mesh.quads_to_triangles()
+
+    node = scene.add_child()
+    mesh_data = node.add_mesh(mesh)
+    normals = [mesh.vertex_normal(k) for k in mesh.vertices()]
+
+    texcoord_0 = [(0, 0) for _ in mesh.vertices()]
+
+    """
+    for fkey in mesh.faces():
+        vkeys = mesh.face_vertices(fkey)
+        plane = mesh.face_plane(fkey)
+        frame = Frame.from_plane(plane)
+        coords = mesh.face_coordinates(fkey)
+        for vkey, xyz in zip(vkeys, coords):
+            u, v, _ = frame.to_local_coordinates(Point(*xyz))
+            texcoord_0[vkey] = (u, v)  # not ideal, gets overwritten
+    """
+
+    # here is the tricky part... for this material to be valid and applied to this mesh,
+    # each of the primitives must have within the attribute `attributes` a key of the form `TEXCOORD_{some integer}`.
+    # the value of this thing should be a list of pairs of floats representing the UV texture coordinates for each vertex.
+    # if `{some integer}` is 0 then there's nothing else to do.  but if a primitive has multiple `TEXTCOORD_{some integer}`s,
+    # then the various `TextureInfoData.tex_coord` associated to this material have to be updated with the appropriate `{some integer}`.
+
+    # would work better if each vertex could have 4 different texture coordinates
+    texcoord_0 = [
+        (0.0, 1.0),
+        (0.0, 0.0),
+        (1.0, 0.0),
+        (1.0, 1.0),
+        (0.0, 0.0),
+        (1.0, 0.0),
+        (1.0, 1.0),
+        (0.0, 1.0),
+    ]
+
+    pd = node.mesh_data.primitive_data_list[0]
+    pd.material = material_key
+    pd.attributes["TEXCOORD_0"] = texcoord_0
+    pd.attributes["NORMAL"] = normals
 
     gltf = GLTF(gltf_filepath)
     gltf.content = cnt
-
-    key = cnt.get_material_index_by_name('Cara_03 Shedland')
-    for pd in node_2.mesh_data.primitive_data_list:
-        # here is the tricky part... for this material to be valid and applied to this mesh,
-        # each of the primitives must have within the attribute `attributes` a key of the form `TEXCOORD_{some integer}`.
-        # the value of this thing should be a list of pairs of floats representing the UV texture coordinates for each vertex.
-        # if `{some integer}` is 0 then there's nothing else to do.  but if a primitive has multiple `TEXTCOORD_{some integer}`s,
-        # then the various `TextureInfoData.tex_coord` associated to this material have to be updated with the appropriate `{some integer}`.
-        pd.material = key  # dict or int? int.
-
     gltf.export(embed_data=False)
