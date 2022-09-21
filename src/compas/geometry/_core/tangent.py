6eff1b3b4b7a3427447eb37272c9860532e6fb68
Tom Van Mele <tom.v.mele@gmail.com>
2022-09-21 08:17:46 +0200

    Update tangent.py

--- src/compas/geometry/_core/tangent.py
+++ src/compas/geometry/_core/tangent.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
-import math
+from math import sqrt
 
 
 __all__ = ["tangent_points_to_circle_xy"]
