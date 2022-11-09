b6acfa791cf961a995da7c77e01f8be6d2d761ef
brgcode <tom.v.mele@gmail.com>
2022-11-09 23:28:22 +0100

    Bump version to 1.17.3

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ optional_requirements = {}
 
 setup(
     name="COMPAS",
-    version="1.17.2",
+    version="1.17.3",
     description="The COMPAS framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
--- src/compas_rhino/__init__.py
+++ src/compas_rhino/__init__.py
@@ -29,7 +29,7 @@ if compas.is_rhino():
     from .utilities import *  # noqa: F401 F403
 
 
-__version__ = "1.17.2"
+__version__ = "1.17.3"
 
 
 PURGE_ON_DELETE = True
