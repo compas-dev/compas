bcc7de06f581546f24bef3da86f2cb538d0b7b44
Chen Kasirer <chen902@gmail.com>
2023-02-01 10:19:54 +0100

    added numeric_owner as kwarg

--- src/compas/__init__.py
+++ src/compas/__init__.py
@@ -319,7 +319,7 @@ def get_bunny(localstorage=None):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(file, localstorage)
