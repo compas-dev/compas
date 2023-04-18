77788b766defd5a75c8d0b4f25b657faeb0916e5
Chen Kasirer <ckasirer@ethz.ch>
2023-04-18 11:15:50 +0200

    simplified Part with Feature in part.py

--- src/compas/datastructures/assembly/part.py
+++ src/compas/datastructures/assembly/part.py
@@ -2,23 +2,92 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
-from collections import deque
-
-# from functools import reduce
-
 from compas.geometry import Frame
-from compas.geometry import Polyhedron as Shape
-from compas.geometry import Transformation
+from compas.geometry import Brep
+from compas.geometry import Polyhedron
+from compas.datastructures import Datastructure
+from compas.data import Data
 
-# from compas.geometry import multiply_matrices
-from compas.geometry import boolean_union_mesh_mesh
-from compas.geometry import boolean_difference_mesh_mesh
-from compas.geometry import boolean_intersection_mesh_mesh
 
-from ..datastructure import Datastructure
-from ..mesh import Mesh
+class Feature(Data):
+    """Base class for a feature which may be applied to a :class:`~compas.datastructures.Part`."""
 
-from .exceptions import FeatureError
+    def apply(self, part):
+        """Apply this Feature to the given part.
+
+        Parameters
+        ----------
+        part : :class:`~compas.datastructures.Part`
+            The part onto which this feature should be applied.
+
+        """
+        raise NotImplementedError
+
+
+class GeometricFeature(Feature):
+    """Base class for geometric feature which may be applied to a :class:`~compas.datastructures.Part`.
+
+    Applies a binary operation on Part's current geometry and the feature's.
+
+    An implementation of this class may offer support for various geometry types by adding an entry to OPERATIONS
+    mapping the geometry type to its corresponding operation.
+
+    """
+
+    OPERATIONS = {
+        Brep: None,
+        Polyhedron: None,
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(GeometricFeature, self).__init__(*args, **kwargs)
+        self._geometry = None
+
+    @property
+    def data(self):
+        return {"geometry": self._geometry}
+
+    @data.setter
+    def data(self, value):
+        self._geometry = value["geometry"]
+
+
+class ParametricFeature(Feature):
+    """Base class for Features that may be applied to the parametric definition
+    of a :class:`~compas.datastructures.Part`.
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(ParametricFeature, self).__init__(*args, **kwargs)
+
+    def restore(self, part):
+        """Reverses the effect this ParametricFeature has incured onto the given part.
+
+        Parameters
+        ----------
+        part : :class:`~compas.datastructures.Part`
+            The part onto which this feature has been previously applied and should now be reverted.
+
+        """
+        raise NotImplementedError
+
+    def accumulate(self, feature):
+        """Returns a new ParametricFeature which has the accumulative effect of this and the given feature.
+
+        A TypeError is raised if the given feature is not compatible with this.
+
+        Parameters
+        ----------
+        feature : :class:`~compas.datastructures.ParametricFeature`
+            Another compatible ParametricFeature whose effect should be accumulated with this one's.
+
+        Returns
+        -------
+        :class:`~compas.datastructures.ParametricFeatures`
+
+        """
+        raise NotImplementedError
 
 
 class Part(Datastructure):
@@ -31,10 +100,6 @@ class Part(Datastructure):
         The name will be stored in :attr:`Part.attributes`.
     frame : :class:`~compas.geometry.Frame`, optional
         The local coordinate system of the part.
-    shape : :class:`~compas.geometry.Shape`, optional
-        The base shape of the part geometry.
-    features : sequence[tuple[:class:`~compas.geometry.Shape`, str]], optional
-        The features to be added to the base shape of the part geometry.
 
     Attributes
     ----------
@@ -44,44 +109,18 @@ class Part(Datastructure):
         The identifier of the part in the connectivity graph of the parent assembly.
     frame : :class:`~compas.geometry.Frame`
         The local coordinate system of the part.
-    shape : :class:`~compas.geometry.Shape`
-        The base shape of the part geometry.
     features : list[tuple[:class:`~compas.geometry.Shape`, str]]
         The features added to the base shape of the part geometry.
-    transformations : Deque[:class:`~compas.geometry.Transformation`]
-        The stack of transformations applied to the part geometry.
-        The most recent transformation is on the left of the stack.
-        All transformations are with respect to the local coordinate system.
-    geometry : :class:`~compas.geometry.Polyhedron`, read-only
-        The geometry of the part after combining the base shape and features through the specified operations.
-
-    Class Attributes
-    ----------------
-    operations : dict[str, callable]
-        Available operations for combining features with a base shape.
 
     """
 
-    operations = {
-        "union": boolean_union_mesh_mesh,
-        "difference": boolean_difference_mesh_mesh,
-        "intersection": boolean_intersection_mesh_mesh,
-    }
-
-    def __init__(self, name=None, frame=None, shape=None, features=None, **kwargs):
+    def __init__(self, name=None, frame=None, **kwargs):
         super(Part, self).__init__()
-        self._frame = None
         self.attributes = {"name": name or "Part"}
         self.attributes.update(kwargs)
         self.key = None
-        self.frame = frame
-        self.shape = shape or Shape([], [])
-        self.features = features or []
-        self.transformations = deque()
-
-    # ==========================================================================
-    # data
-    # ==========================================================================
+        self.frame = frame or Frame.worldXY()
+        self.features = []
 
     @property
     def DATASCHEMA(self):
@@ -92,9 +131,6 @@ class Part(Datastructure):
                 "attributes": dict,
                 "key": int,
                 "frame": Frame,
-                "shape": Shape,
-                "features": list,
-                "transformations": list,
             }
         )
 
@@ -107,10 +143,7 @@ class Part(Datastructure):
         data = {
             "attributes": self.attributes,
             "key": self.key,
-            "frame": self.frame.data,
-            "shape": self.shape.data,
-            "features": [(shape.data, operation) for shape, operation in self.features],
-            "transformations": [T.data for T in self.transformations],
+            "frame": self.frame,
         }
         return data
 
@@ -118,123 +151,41 @@ class Part(Datastructure):
     def data(self, data):
         self.attributes.update(data["attributes"] or {})
         self.key = data["key"]
-        self.frame.data = data["frame"]
-        self.shape.data = data["shape"]
-        self.features = [(Shape.from_data(shape), operation) for shape, operation in data["features"]]
-        self.transformations = deque([Transformation.from_data(T) for T in data["transformations"]])
+        self.frame = data["frame"]
 
-    # ==========================================================================
-    # properties
-    # ==========================================================================
+    def get_geometry(self, with_features=False):
+        """
+        Returns a transformed copy of the part's geometry.
 
-    @property
-    def name(self):
-        return self.attributes.get("name") or self.__class__.__name__
-
-    @name.setter
-    def name(self, value):
-        self.attributes["name"] = value
-
-    @property
-    def frame(self):
-        if not self._frame:
-            self._frame = Frame.worldXY()
-        return self._frame
-
-    @frame.setter
-    def frame(self, frame):
-        self._frame = frame
-
-    @property
-    def geometry(self):
-        # TODO: this is a temp solution
-        # TODO: add memoization or some other kind of caching
-        if self.features:
-            A = self.shape.to_vertices_and_faces(triangulated=True)
-            for shape, operation in self.features:
-                B = shape.to_vertices_and_faces(triangulated=True)
-                A = Part.operations[operation](A, B)
-            geometry = Shape(*A)
-        else:
-            geometry = Shape(*self.shape.to_vertices_and_faces())
-
-        T = Transformation.from_frame_to_frame(Frame.worldXY(), self.frame)
-        geometry.transform(T)
-        return geometry
-
-    # ==========================================================================
-    # customization
-    # ==========================================================================
-
-    def __str__(self):
-        tpl = "<Part with shape {} and features {}>"
-        return tpl.format(self.shape, self.features)
-
-    # ==========================================================================
-    # constructors
-    # ==========================================================================
-
-    # ==========================================================================
-    # methods
-    # ==========================================================================
-
-    def transform(self, T):
-        """Transform the part with respect to the local cooordinate system.
+        The returned type can be drawn with an Artist.
 
         Parameters
         ----------
-        T : :class:`~compas.geometry.Transformation`
+        with_features : bool
+            True if geometry should include all the available features.
+
+        Returns
+        -------
+        :class:`~compas.geometry.Geometry`
+
+        """
+        raise NotImplementedError
+
+    def clear_features(self, features_to_clear=None):
+        raise NotImplementedError
+
+    def add_feature(self, feature, apply=False):
+        """Add a Feature to this Part.
+
+        Parameters
+        ----------
+        feature : :class:`~compas.assembly.Feature`
+            The feature to add
+        apply : :bool:
+            If True, feature is also applied. Otherwise, feature is only added and user must call `apply_features`.
 
         Returns
         -------
         None
-
         """
-        self.transformations.appendleft(T)
-        self.shape.transform(T)
-        for shape, operation in self.features:
-            shape.transform(T)
-
-    def add_feature(self, shape, operation):
-        """Add a feature to the shape of the part and the operation through which it should be integrated.
-
-        Parameters
-        ----------
-        shape : :class:`~compas.geometry.Shape`
-            The shape of the feature.
-        operation : Literal['union', 'difference', 'intersection']
-            The boolean operation through which the feature should be integrated in the base shape.
-
-        Returns
-        -------
-        None
-
-        """
-        if operation not in Part.operations:
-            raise FeatureError
-        self.features.append((shape, operation))
-
-    # def apply_transformations(self):
-    #     """Apply all transformations to the part shape."""
-    #     X = Transformation.from_frame(self.frame)
-    #     transformations = self.transformations[:]
-    #     transformations.append(X)
-    #     T = reduce(multiply_matrices, transformations)
-    #     self.shape.transform(T)
-
-    def to_mesh(self, cls=None):
-        """Convert the part geometry to a mesh.
-
-        Parameters
-        ----------
-        cls : :class:`~compas.datastructures.Mesh`, optional
-            The type of mesh to be used for the conversion.
-
-        Returns
-        -------
-        :class:`~compas.datastructures.Mesh`
-            The resulting mesh.
-
-        """
-        cls = cls or Mesh
-        return cls.from_shape(self.geometry)
+        raise NotImplementedError
