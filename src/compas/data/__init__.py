a4e76691985195705edc580db36e7810cd5a5e9e
brgcode <tom.v.mele@gmail.com>
2023-06-05 15:10:07 +0200

    add schema wrapper

--- src/compas/data/__init__.py
+++ src/compas/data/__init__.py
@@ -78,7 +78,7 @@ from .encoders import DataEncoder
 from .encoders import DataDecoder
 from .data import Data
 
-from .json import json_load, json_loads, json_dump, json_dumps
+from .json import json_load, json_loads, json_dump, json_dumps, wrap_schema_value
 
 __all__ = [
     "Data",
