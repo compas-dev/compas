117eb038290c66a6feee63370119c1633f210383
Chen Kasirer <ckasirer@ethz.ch>
2023-07-19 13:35:59 +0200

    added some unittests

--- tests/compas/artists/test_artists.py
+++ tests/compas/artists/test_artists.py
@@ -2,6 +2,21 @@ import pytest  # noqa: F401
 
 import compas
 from compas.artists import Artist
+from compas.artists.artist import NoArtistContextError
+
+
+if not compas.IPY:
+    @pytest.fixture(autouse=True)
+    def reset_artists():
+        # before each test
+        yield
+        # after each test, reset artists
+        Artist.ITEM_ARTIST.clear()
+        Artist._Artist__ARTISTS_REGISTERED = False
+
+
+def register_fake_context():
+    Artist.register(FakeItem, FakeArtist, context="fake")
 
 
 class FakeArtist(Artist):
@@ -58,5 +73,80 @@ if not compas.IPY:
 
         assert isinstance(artist, FakeArtist)
 
-    def register_fake_context():
-        Artist.register(FakeItem, FakeArtist, context="fake")
+
+    def test_artist_auto_context_discovery_viewer(mocker):
+        mocker.patch("compas.artists.artist.is_viewer_open", return_value=True)
+        Artist.ITEM_ARTIST["Viewer"] = {FakeItem: FakeArtist}
+
+        item = FakeSubItem()
+        artist = Artist(item)
+
+        assert isinstance(artist, FakeArtist)
+
+    def test_artist_auto_context_discovery_viewer(mocker):
+        mocker.patch("compas.artists.artist.is_viewer_open", return_value=True)
+        Artist.ITEM_ARTIST["Viewer"] = {FakeItem: FakeArtist}
+
+        item = FakeSubItem()
+        artist = Artist(item)
+
+        assert isinstance(artist, FakeArtist)
+
+
+    def test_artist_auto_context_discovery_plotter(mocker):
+        mocker.patch("compas.artists.artist.is_plotter_open", return_value=True)
+        Artist.ITEM_ARTIST["Plotter"] = {FakeItem: FakeArtist}
+
+        item = FakeSubItem()
+        artist = Artist(item)
+
+        assert isinstance(artist, FakeArtist)
+
+
+    def test_artist_auto_context_discovery_viewer_priority(mocker):
+        mocker.patch("compas.artists.artist.is_viewer_open", return_value=True)
+
+        class FakeViewerArtist(FakeArtist):
+            pass
+
+        class FakePlotterArtist(FakeArtist):
+            pass
+
+
+        Artist.ITEM_ARTIST["Viewer"] = {FakeItem: FakeViewerArtist}
+        Artist.ITEM_ARTIST["Plotter"] = {FakeItem: FakePlotterArtist}
+
+        item = FakeSubItem()
+        artist = Artist(item)
+
+        assert isinstance(artist, FakeViewerArtist)
+
+
+    def test_artist_auto_context_discovery_manual_override(mocker):
+        mocker.patch("compas.artists.artist.is_viewer_open", return_value=True)
+
+        class FakeViewerArtist(FakeArtist):
+            pass
+
+        class FakePlotterArtist(FakeArtist):
+            pass
+
+
+        Artist.ITEM_ARTIST["Viewer"] = {FakeItem: FakeViewerArtist}
+        Artist.ITEM_ARTIST["Plotter"] = {FakeItem: FakePlotterArtist}
+
+        item = FakeSubItem()
+        artist = Artist(item, context="Plotter")
+
+        assert isinstance(artist, FakePlotterArtist)
+
+
+    def test_artist_auto_context_discovery_no_context(mocker):
+        mocker.patch("compas.artists.artist.is_viewer_open", return_value=False)
+        mocker.patch("compas.artists.artist.is_plotter_open", return_value=False)
+        mocker.patch("compas.artists.artist.compas.is_grasshopper", return_value=False)
+        mocker.patch("compas.artists.artist.compas.is_rhino", return_value=False)
+
+        with pytest.raises(NoArtistContextError):
+            item = FakeSubItem()
+            _ = Artist(item)
