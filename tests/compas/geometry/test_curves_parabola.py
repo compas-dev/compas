9c634e2035f89779ec71d7429f5e31e2506db74c
tomvanmele <tom.v.mele@gmail.com>
2023-07-23 10:03:59 +0200

    updated tests

--- tests/compas/geometry/test_curves_parabola.py
+++ tests/compas/geometry/test_curves_parabola.py
@@ -1,3 +1,49 @@
+import pytest
+
+from compas.geometry import allclose
+from compas.geometry import Frame
+from compas.geometry import Parabola
+
+
+def test_create_parabola():
+    parabola = Parabola(focal=1)
+
+    assert parabola.focal == 1
+    assert parabola.frame == Frame.worldXY()
+
+    assert allclose(parabola.point_at(0.0), parabola.point_at(0.0, world=False), tol=1e-12)
+    assert allclose(parabola.point_at(0.5), parabola.point_at(0.5, world=False), tol=1e-12)
+    assert allclose(parabola.point_at(1.0), parabola.point_at(1.0, world=False), tol=1e-12)
+
+
+def test_create_parabola_frame():
+    frame = Frame.worldZX()
+    parabola = Parabola(focal=1, frame=frame)
+
+    assert parabola.focal == 1
+    assert parabola.frame == frame
+
+    assert allclose(parabola.point_at(0.0), parabola.point_at(0.0, world=False), tol=1e-12)
+    assert not allclose(parabola.point_at(0.5), parabola.point_at(0.5, world=False), tol=1e-12)
+    assert not allclose(parabola.point_at(1.0), parabola.point_at(1.0, world=False), tol=1e-12)
+
+    assert allclose(
+        parabola.point_at(0.0),
+        parabola.point_at(0.0, world=False).transformed(parabola.transformation),
+        tol=1e-12,
+    )
+    assert allclose(
+        parabola.point_at(0.5),
+        parabola.point_at(0.5, world=False).transformed(parabola.transformation),
+        tol=1e-12,
+    )
+    assert allclose(
+        parabola.point_at(1.0),
+        parabola.point_at(1.0, world=False).transformed(parabola.transformation),
+        tol=1e-12,
+    )
+
+
 # =============================================================================
 # Constructors
 # =============================================================================
@@ -5,6 +51,17 @@
 # =============================================================================
 # Properties and Geometry
 # =============================================================================
+
+
+def test_parabola_properties():
+    parabola = Parabola(focal=1.0)
+
+    assert parabola.focal == 1.0
+
+    parabola._focal = None
+    with pytest.raises(ValueError):
+        parabola.focal
+
 
 # =============================================================================
 # Accessors
