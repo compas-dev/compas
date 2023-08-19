956e0e6f302835f7e1a0f399e23729b4807759f9
tomvanmele <tom.v.mele@gmail.com>
2023-08-19 08:21:25 +0200

    native capsule

--- src/compas_rhino/artists/capsuleartist.py
+++ src/compas_rhino/artists/capsuleartist.py
@@ -2,31 +2,37 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
-import compas_rhino
-from compas.artists import ShapeArtist
+from System.Drawing.Color import FromArgb  # type: ignore
+from Rhino.Geometry import Brep as RhinoBrep  # type: ignore
+from Rhino.Geometry import PipeCapMode  # type: ignore
+from Rhino.DocObjects.ObjectColorSource import ColorFromObject  # type: ignore
+from Rhino.DocObjects import ObjectAttributes  # type: ignore
+
+import scriptcontext as sc  # type: ignore
+
+from compas.artists import GeometryArtist
 from compas.colors import Color
+from compas_rhino.conversions import line_to_rhino_curve
 from .artist import RhinoArtist
 
 
-class CapsuleArtist(RhinoArtist, ShapeArtist):
+class CapsuleArtist(RhinoArtist, GeometryArtist):
     """Artist for drawing capsule shapes.
 
     Parameters
     ----------
     capsule : :class:`~compas.geometry.Capsule`
         A COMPAS capsule.
-    layer : str, optional
-        The layer that should contain the drawing.
     **kwargs : dict, optional
         Additional keyword arguments.
         For more info, see :class:`RhinoArtist` and :class:`ShapeArtist`.
 
     """
 
-    def __init__(self, capsule, layer=None, **kwargs):
-        super(CapsuleArtist, self).__init__(shape=capsule, layer=layer, **kwargs)
+    def __init__(self, capsule, **kwargs):
+        super(CapsuleArtist, self).__init__(geometry=capsule, **kwargs)
 
-    def draw(self, color=None, u=None, v=None):
+    def draw(self, color=None, u=16, v=16):
         """Draw the capsule associated with the artist.
 
         Parameters
@@ -36,10 +42,10 @@ class CapsuleArtist(RhinoArtist, ShapeArtist):
             Default is :attr:`compas.artists.ShapeArtist.color`.
         u : int, optional
             Number of faces in the "u" direction.
-            Default is :attr:`~CapsuleArtist.u`.
+            Default is 16.
         v : int, optional
             Number of faces in the "v" direction.
-            Default is :attr:`CapsuleArtist.v`.
+            Default is 16.
 
         Returns
         -------
@@ -47,17 +53,34 @@ class CapsuleArtist(RhinoArtist, ShapeArtist):
             The GUIDs of the objects created in Rhino.
 
         """
+        # color = Color.coerce(color) or self.color
+        # vertices, faces = self.geometry.to_vertices_and_faces(u=u, v=v)
+        # vertices = [list(vertex) for vertex in vertices]
+        # guid = compas_rhino.draw_mesh(
+        #     vertices,
+        #     faces,
+        #     layer=self.layer,
+        #     name=self.geometry.name,
+        #     color=color.rgb255,  # type: ignore
+        #     disjoint=True,
+        # )
+        # return [guid]
+
         color = Color.coerce(color) or self.color
-        u = u or self.u
-        v = v or self.v
-        vertices, faces = self.shape.to_vertices_and_faces(u=u, v=v)
-        vertices = [list(vertex) for vertex in vertices]
-        guid = compas_rhino.draw_mesh(
-            vertices,
-            faces,
-            layer=self.layer,
-            name=self.shape.name,
-            color=color.rgb255,
-            disjoint=True,
-        )
-        return [guid]
+        color = FromArgb(*color.rgb255)  # type: ignore
+
+        abs_tol = sc.doc.ModelAbsoluteTolerance
+        ang_tol = sc.doc.ModelAngleToleranceRadians
+
+        radius = self.geometry.radius
+        line = self.geometry.axis
+        curve = line_to_rhino_curve(line)
+
+        attr = ObjectAttributes()
+        attr.ObjectColor = color
+        attr.ColorSource = ColorFromObject
+
+        breps = RhinoBrep.CreatePipe(curve, radius, False, PipeCapMode.Round, False, abs_tol, ang_tol)
+        guids = [sc.doc.Objects.AddBrep(brep, attr) for brep in breps]
+
+        return guids
