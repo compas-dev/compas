393bee0f23302c3de07c1d836f5bf3bcd2f66d41
tomvanmele <tom.v.mele@gmail.com>
2023-08-19 08:48:23 +0200

    native sphere

--- src/compas_rhino/artists/sphereartist.py
+++ src/compas_rhino/artists/sphereartist.py
@@ -2,31 +2,52 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
-import compas_rhino
-from compas.artists import ShapeArtist
+from System.Drawing.Color import FromArgb  # type: ignore
+from Rhino.Geometry import Sphere as RhinoSphere  # type: ignore
+from Rhino.DocObjects.ObjectColorSource import ColorFromObject  # type: ignore
+from Rhino.DocObjects import ObjectAttributes  # type: ignore
+
+import scriptcontext as sc  # type: ignore
+
+from compas.artists import GeometryArtist
 from compas.colors import Color
+from compas_rhino.conversions import frame_to_rhino
 from .artist import RhinoArtist
 
 
-class SphereArtist(RhinoArtist, ShapeArtist):
+def sphere_to_rhino(sphere):
+    """Convert a COMPAS sphere to a Rhino sphere.
+
+    Parameters
+    ----------
+    sphere : :class:`~compas.geometry.Sphere`
+        A COMPAS sphere.
+
+    Returns
+    -------
+    Rhino.Geometry.Sphere
+
+    """
+    return RhinoSphere(frame_to_rhino(sphere.frame), sphere.radius)
+
+
+class SphereArtist(RhinoArtist, GeometryArtist):
     """Artist for drawing sphere shapes.
 
     Parameters
     ----------
     sphere : :class:`~compas.geometry.Sphere`
         A COMPAS sphere.
-    layer : str, optional
-        The layer that should contain the drawing.
     **kwargs : dict, optional
         Additional keyword arguments.
         For more info, see :class:`RhinoArtist` and :class:`ShapeArtist`.
 
     """
 
-    def __init__(self, sphere, layer=None, **kwargs):
-        super(SphereArtist, self).__init__(shape=sphere, layer=layer, **kwargs)
+    def __init__(self, sphere, **kwargs):
+        super(SphereArtist, self).__init__(geometry=sphere, **kwargs)
 
-    def draw(self, color=None, u=None, v=None):
+    def draw(self, color=None):
         """Draw the sphere associated with the artist.
 
         Parameters
@@ -34,12 +55,6 @@ class SphereArtist(RhinoArtist, ShapeArtist):
         color : tuple[int, int, int] | tuple[float, float, float] | :class:`~compas.colors.Color`, optional
             The RGB color of the sphere.
             Default is :attr:`compas.artists.ShapeArtist.color`.
-        u : int, optional
-            Number of faces in the "u" direction.
-            Default is :attr:`SphereArtist.u`.
-        v : int, optional
-            Number of faces in the "v" direction.
-            Default is :attr:`SphereArtist.v`.
 
         Returns
         -------
@@ -47,17 +62,27 @@ class SphereArtist(RhinoArtist, ShapeArtist):
             The GUIDs of the objects created in Rhino.
 
         """
+        # color = Color.coerce(color) or self.color
+        # u = u or self.u
+        # v = v or self.v
+        # vertices, faces = self.shape.to_vertices_and_faces(u=u, v=v)
+        # vertices = [list(vertex) for vertex in vertices]
+        # guid = compas_rhino.draw_mesh(
+        #     vertices,
+        #     faces,
+        #     layer=self.layer,
+        #     name=self.shape.name,
+        #     color=color.rgb255,
+        #     disjoint=True,
+        # )
+        # return [guid]
+
         color = Color.coerce(color) or self.color
-        u = u or self.u
-        v = v or self.v
-        vertices, faces = self.shape.to_vertices_and_faces(u=u, v=v)
-        vertices = [list(vertex) for vertex in vertices]
-        guid = compas_rhino.draw_mesh(
-            vertices,
-            faces,
-            layer=self.layer,
-            name=self.shape.name,
-            color=color.rgb255,
-            disjoint=True,
-        )
+        color = FromArgb(*color.rgb255)  # type: ignore
+
+        attr = ObjectAttributes()
+        attr.ObjectColor = color
+        attr.ColorSource = ColorFromObject
+
+        guid = sc.doc.Objects.AddSphere(sphere_to_rhino(self.geometry), attr)
         return [guid]
