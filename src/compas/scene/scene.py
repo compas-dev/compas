2f3c3f678be13b3cda7b1d4a78cdab2e19043045
Li Chen <leochen1990@gmail.com>
2023-11-10 11:58:45 +0100

    Tree as attribute

--- src/compas/scene/scene.py
+++ src/compas/scene/scene.py
@@ -1,12 +1,22 @@
+from compas.data import Data
 from compas.datastructures import Tree
 from compas.datastructures import TreeNode
 from compas.artists import Artist
 
-class Scene(Tree):
-    def __init__(self, name=None, attributes=None):
-        super(Scene, self).__init__(name, attributes)
+class Scene(Data):
+    def __init__(self, name=None):
+        super(Scene, self).__init__(name)
+        self._tree = Tree("Scene")
         root = TreeNode(name='root')
-        super(Scene, self).add(root)
+        self.tree.add(root)
+
+    @property
+    def tree(self):
+        return self._tree
+    
+    @property
+    def sceneobjects(self):
+        return [node.attributes['sceneobject'] for node in self.tree.nodes if 'sceneobject' in node.attributes]
 
     def add(self, item, parent=None, **kwargs):
         sceneobject = Artist(item, **kwargs)
@@ -14,21 +24,24 @@ class Scene(Tree):
         node = TreeNode(name, attributes={'sceneobject': sceneobject})
 
         if parent is None:
-            super(Scene, self).add(node, parent=self.root)
+            self.tree.add(node, parent=self.tree.root)
         else:
             parent_node = self._get_node(parent)
-            super(Scene, self).add(node, parent=parent_node)
+            self.tree.add(node, parent=parent_node)
 
         return sceneobject
+    
+    def remove(self, sceneobject):
+        node = self._get_node(sceneobject)
+        self.tree.remove_node(node)
 
     def _get_node(self, sceneobject):
-        for node in self.nodes:
+        for node in self.tree.nodes:
             if 'sceneobject' in node.attributes:
                 if node.attributes['sceneobject'] == sceneobject:
                     return node
         raise Exception('Scene object not in scene')
 
     def redraw(self):
-        for node in self.nodes:
-            if 'sceneobject' in node.attributes:
-                node.attributes['sceneobject'].draw()
+        for sceneobject in self.sceneobjects:
+            sceneobject.draw()
