308768c635a63ab8782b1053ff348d83c9415b4d
tomvanmele <tom.v.mele@gmail.com>
2023-11-24 00:27:38 +0100

    return type hints and shortcuts for scale, rotatet, translate

--- src/compas/geometry/geometry.py
+++ src/compas/geometry/geometry.py
@@ -2,6 +2,13 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+try:
+    from typing import TypeVar  # noqa: F401
+except ImportError:
+    pass
+else:
+    G = TypeVar("G", bound="Geometry")
+
 from compas.data import Data
 
 
@@ -33,7 +40,7 @@ class Geometry(Data):
         """
         raise NotImplementedError
 
-    def transformed(self, transformation):
+    def transformed(self, transformation):  # type: (...) -> G
         """Returns a transformed copy of this geometry.
 
         Parameters
@@ -47,6 +54,153 @@ class Geometry(Data):
             The transformed geometry.
 
         """
-        geometry = self.copy()
+        geometry = self.copy()  # type: Geometry
         geometry.transform(transformation)
-        return geometry
+        return geometry  # type: ignore
+
+    def scale(self, x, y=None, z=None):
+        """Scale the geometry.
+
+        Parameters
+        ----------
+        x : float
+            The scaling factor in the x-direction.
+        y : float, optional
+            The scaling factor in the y-direction.
+            Defaults to ``x``.
+        z : float, optional
+            The scaling factor in the z-direction.
+            Defaults to ``x``.
+
+        Returns
+        -------
+        None
+
+        """
+        from compas.geometry import Scale
+
+        if y is None:
+            y = x
+
+        if z is None:
+            z = x
+
+        self.transform(Scale.from_factors([x, y, z]))
+
+    def scaled(self, x, y=None, z=None):  # type: (...) -> G
+        """Returns a scaled copy of this geometry.
+
+        Parameters
+        ----------
+        x : float
+            The scaling factor in the x-direction.
+        y : float, optional
+            The scaling factor in the y-direction.
+            Defaults to ``x``.
+        z : float, optional
+            The scaling factor in the z-direction.
+            Defaults to ``x``.
+
+        Returns
+        -------
+        :class:`Geometry`
+            The scaled geometry.
+
+        """
+        from compas.geometry import Scale
+
+        if y is None:
+            y = x
+
+        if z is None:
+            z = x
+
+        return self.transformed(Scale.from_factors([x, y, z]))
+
+    def translate(self, vector):
+        """Translate the geometry.
+
+        Parameters
+        ----------
+        vector : :class:`compas.geometry.Vector`
+            The vector used to translate the geometry.
+
+        Returns
+        -------
+        None
+
+        """
+        from compas.geometry import Translation
+
+        self.transform(Translation.from_vector(vector))
+
+    def translated(self, vector):  # type: (...) -> G
+        """Returns a translated copy of this geometry.
+
+        Parameters
+        ----------
+        vector : :class:`compas.geometry.Vector`
+            The vector used to translate the geometry.
+
+        Returns
+        -------
+        :class:`Geometry`
+            The translated geometry.
+
+        """
+        from compas.geometry import Translation
+
+        return self.transformed(Translation.from_vector(vector))
+
+    def rotate(self, angle, axis=None, point=None):
+        """Rotate the geometry.
+
+        Parameters
+        ----------
+        angle : float
+            The angle of rotation in radians.
+        axis : :class:`compas.geometry.Vector`, optional
+            The axis of rotation.
+            Defaults to the z-axis.
+        point : :class:`compas.geometry.Point`, optional
+            The base point of the rotation axis.
+            Defaults to the origin.
+
+        Returns
+        -------
+        None
+
+        """
+        from compas.geometry import Rotation
+
+        if axis is None:
+            axis = [0.0, 0.0, 1.0]
+
+        self.transform(Rotation.from_axis_and_angle(axis, angle, point))
+
+    def rotated(self, angle, axis=None, point=None):  # type: (...) -> G
+        """Returns a rotated copy of this geometry.
+
+        Parameters
+        ----------
+        angle : float
+            The angle of rotation in radians.
+        axis : :class:`compas.geometry.Vector`, optional
+            The axis of rotation.
+            Defaults to the z-axis.
+        point : :class:`compas.geometry.Point`, optional
+            The base point of the rotation axis.
+            Defaults to the origin.
+
+        Returns
+        -------
+        :class:`Geometry`
+            The rotated geometry.
+
+        """
+        from compas.geometry import Rotation
+
+        if axis is None:
+            axis = [0.0, 0.0, 1.0]
+
+        return self.transformed(Rotation.from_axis_and_angle(axis, angle, point))
