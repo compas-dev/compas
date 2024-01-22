270d431d30287e8b5b96bc3978f83090f318776e
tomvanmele <tom.v.mele@gmail.com>
2024-01-22 13:53:03 +0100

    install with pip

--- src/compas_rhino/install_with_pip.py
+++ src/compas_rhino/install_with_pip.py
@@ -2,8 +2,8 @@ import subprocess
 import compas_rhino
 
 
-def install(package, *args):
-    subprocess.check_call([compas_rhino._get_default_rhino_cpython_path, "-m", "pip", "install", package] + list(args))
+def install(args):
+    subprocess.check_call([compas_rhino._get_default_rhino_cpython_path("8.0"), "-m", "pip", "install"] + args)
 
 
 if __name__ == "__main__":
@@ -11,15 +11,9 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument(
-        "-v",
-        "--version",
-        choices=["8.0"],
-        default="8.0",
-        help="The version of Rhino to install the packages in.",
-    )
+    parser.add_argument("pipargs", help="Arguments to be passed on to pip as a string")
 
     args = parser.parse_args()
-    compas_rhino.INSTALLATION_ARGUMENTS = args
+    pipargs = args.pipargs.split()
 
-    install("compas")
+    install(pipargs)
