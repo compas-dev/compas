c3abef9ad5125a6ec844516ba7c192e0e7829f98
Gonzalo Casas <casas@arch.ethz.ch>
2024-02-06 12:14:02 +0100

    Bump version to 2.0.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ optional_requirements = {}
 
 setup(
     name="COMPAS",
-    version="2.0.1",
+    version="2.0.2",
     description="The COMPAS framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
