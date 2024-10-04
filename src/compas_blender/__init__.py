007077a6ed4fa7945fef52b89adee0fbe1e0d6d3
Chen Kasirer <ckasirer@ethz.ch>
2024-10-04 10:26:55 +0200

    Bump version to 2.4.3

--- src/compas_blender/__init__.py
+++ src/compas_blender/__init__.py
@@ -11,7 +11,7 @@ except ImportError:
     pass
 
 
-__version__ = "2.4.2"
+__version__ = "2.4.3"
 
 
 INSTALLABLE_PACKAGES = ["compas", "compas_blender"]
--- src/compas_rhino/__init__.py
+++ src/compas_rhino/__init__.py
@@ -7,7 +7,7 @@ import sys
 import compas
 import compas._os
 
-__version__ = "2.4.2"
+__version__ = "2.4.3"
 
 
 PURGE_ON_DELETE = True
