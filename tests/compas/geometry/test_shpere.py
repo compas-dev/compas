f68d0ea943c79217045efc45ac94630c5151bf0f
Chen Kasirer <ckasirer@ethz.ch>
2024-11-01 14:50:02 +0100

    some unittests

--- tests/compas/geometry/test_shpere.py
+++ tests/compas/geometry/test_shpere.py
@@ -10,6 +10,10 @@ def sphere():
 
 
 def test_sphere_discretization(sphere):
-    assert len(sphere.edges) == 496
-    assert len(sphere.faces) == 256
-    assert len(sphere.vertices) == 242
+    expected_face_count = sphere.resolution_v * sphere.resolution_u
+    expected_vertex_count = (sphere.resolution_v - 1) * sphere.resolution_u + 2
+    expected_edge_count = expected_face_count * 2 - sphere.resolution_u
+
+    assert len(sphere.edges) == expected_edge_count
+    assert len(sphere.faces) == expected_face_count
+    assert len(sphere.vertices) == expected_vertex_count
