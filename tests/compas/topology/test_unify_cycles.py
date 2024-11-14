9bb9e916b3bac5ea177d221d8f1535132705348f
Romana Rust <romana.rust@gmail.com>
2024-11-14 13:29:06 +0100

    skip ipy

--- tests/compas/topology/test_unify_cycles.py
+++ tests/compas/topology/test_unify_cycles.py
@@ -6,6 +6,8 @@ HERE = os.path.dirname(__file__)
 
 
 def test_unify_cycles():
+    if compas.IPY:
+        return
     test_data = compas.json_load(os.path.join(HERE, "..", "fixtures", "topology", "vertices_faces.json"))
     vertices = test_data["vertices"]
     faces = test_data["faces"]
