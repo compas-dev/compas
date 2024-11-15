155147bafa225adc14f9709309e1939f3a7d2ad0
Romana Rust <r.rust@vyzn.tech>
2024-11-15 15:52:24 +0100

    adding meshgrid test

--- tests/compas/topology/test_unify_cycles.py
+++ tests/compas/topology/test_unify_cycles.py
@@ -1,5 +1,10 @@
+import math
 import os
+import random
+
 import compas
+from compas.datastructures import Mesh
+from compas.tolerance import TOL
 from compas.topology import unify_cycles
 
 HERE = os.path.dirname(__file__)
@@ -13,3 +18,17 @@ def test_unify_cycles():
     faces = test_data["faces"]
     unify_cycles(vertices, faces)
     unify_cycles(vertices, faces, nmax=29, radius=22.4)  # anything below won't work
+
+
+def test_face_adjacency():
+    if compas.IPY:
+        return
+    for _ in range(10):
+        nx = random.randint(5, 20)
+        ny = random.randint(5, 20)
+        dx = 10
+        dy = 10
+        mesh = Mesh.from_meshgrid(dx, nx, dy, ny)
+        vertices, faces = mesh.to_vertices_and_faces()
+        radius = math.sqrt((dx / nx) ** 2 + (dy / ny) ** 2) + TOL.absolute
+        unify_cycles(vertices, faces, radius=radius)
