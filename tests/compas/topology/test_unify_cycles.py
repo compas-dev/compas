fc9e736d87eec1796b58390843fbf84ad6f81a90
Romana Rust <romana.rust@gmail.com>
2024-12-13 07:52:10 +0100

    Update test_unify_cycles.py

--- tests/compas/topology/test_unify_cycles.py
+++ tests/compas/topology/test_unify_cycles.py
@@ -4,6 +4,7 @@ import random
 
 import compas
 from compas.datastructures import Mesh
+from compas.geometry import volume_polyhedron
 from compas.tolerance import TOL
 from compas.topology import unify_cycles
 
@@ -16,11 +17,26 @@ def test_unify_cycles():
     test_data = compas.json_load(os.path.join(HERE, "..", "fixtures", "topology", "vertices_faces.json"))
     vertices = test_data["vertices"]
     faces = test_data["faces"]
+
+    max_edge_length = 22.386
+    max_nbrs = 29
+    volume = 1121.146165  # only correct if cycles are unified
+
+    # no parameters
     unify_cycles(vertices, faces)
-    unify_cycles(vertices, faces, nmax=29, max_distance=22.4)  # anything below won't work
+    assert TOL.is_close(volume, volume_polyhedron((vertices, faces)))
+    # only max_nbrs
+    unify_cycles(vertices, faces, nmax=max_nbrs)
+    assert TOL.is_close(volume, volume_polyhedron((vertices, faces)))
+    # only max_distance
+    unify_cycles(vertices, faces, max_distance=max_edge_length)
+    assert TOL.is_close(volume, volume_polyhedron((vertices, faces)))
+    # both parameters
+    unify_cycles(vertices, faces, nmax=max_nbrs, max_distance=max_edge_length)
+    assert TOL.is_close(volume, volume_polyhedron((vertices, faces)))
 
 
-def test_face_adjacency():
+def test_face_adjacency_and_unify_cycles():
     if compas.IPY:
         return
     for _ in range(10):
@@ -32,3 +48,6 @@ def test_face_adjacency():
         vertices, faces = mesh.to_vertices_and_faces()
         max_distance = math.sqrt((dx / nx) ** 2 + (dy / ny) ** 2) + TOL.absolute
         unify_cycles(vertices, faces, max_distance=max_distance)
+        mesh = Mesh.from_vertices_and_faces(vertices, faces)
+        for face in mesh.faces():
+            assert TOL.is_allclose(mesh.face_normal(face), [0, 0, 1])
