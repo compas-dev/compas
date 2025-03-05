4d526f3da18a88ca183df019a619ffe0c13239d7
Licini <leochen1990@gmail.com>
2025-03-05 14:30:55 +0100

    docstring

--- src/compas/scene/group.py
+++ src/compas/scene/group.py
@@ -4,7 +4,30 @@ from .sceneobject import SceneObject
 
 
 class Group(SceneObject):
-    """A group of scene objects."""
+    """A group of scene objects.
+
+    Parameters
+    ----------
+    name : str, optional
+        The name of the group.
+
+    Examples
+    --------
+    >>> from compas.scene import Scene
+    >>> from compas.scene import Group
+    >>> from compas.geometry import Point
+    >>> scene = Scene()
+    >>> group = Group(name="My Group")
+    >>> scene.add(group)
+    >>> point = Point(0, 0, 0)
+    >>> group.add(point)
+    >>> print(scene)
+    <Tree with 3 nodes>
+    └── <TreeNode: ROOT>
+        └── <Group: My Group>
+            └── <GeometryObject: Point>
+
+    """
 
     def __new__(cls, *args, **kwargs):
         return object.__new__(cls)
