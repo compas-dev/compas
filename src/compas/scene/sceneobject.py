fc52d3d42639bfffd69c021031cb6d2f82793a31
Licini <leochen1990@gmail.com>
2025-03-06 18:26:03 +0100

    small tweak

--- src/compas/scene/sceneobject.py
+++ src/compas/scene/sceneobject.py
@@ -101,7 +101,7 @@ class SceneObject(TreeNode):
         **kwargs  # type: dict
     ):  # fmt: skip
         # type: (...) -> None
-        if not isinstance(item, (Data, type(None))):
+        if item and not isinstance(item, Data):
             raise ValueError("The item assigned to this scene object should be a data object: {}".format(type(item)))
 
         name = name or item.name
