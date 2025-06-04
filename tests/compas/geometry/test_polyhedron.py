c13e0e475eaa686a90fc6ae0fa116b4c5b5df64b
tomvanmele <tom.v.mele@gmail.com>
2025-06-04 15:56:29 +0200

    polyhedron edges and faces test

--- tests/compas/geometry/test_polyhedron.py
+++ tests/compas/geometry/test_polyhedron.py
@@ -11,6 +11,8 @@ def test_polyhedron():
     assert polyhedron.vertices == vertices
     assert polyhedron.faces == faces
     assert polyhedron.name == name
+    assert all(u < len(polyhedron.vertices) and v < len(polyhedron.vertices) for u, v in polyhedron.edges)
+    assert all(v < len(polyhedron.vertices) for face in polyhedron.faces for v in face)
     assert polyhedron.points == vertices
     assert polyhedron.lines == [(a, b) for a, b in pairwise(vertices[-1:] + vertices)]
     assert polyhedron.points[0] == vertices[0]
