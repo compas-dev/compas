b75d2d75d12a24636b6a19bb47a773fe08b0f08f
obucklin <obucklin@gmail.com>
2025-06-11 13:15:57 +0200

    changed to raise ValueEror

--- src/compas/geometry/_core/angles.py
+++ src/compas/geometry/_core/angles.py
@@ -156,7 +156,7 @@ def angle_vectors_projected(u, v, normal, deg=False, tol=None):
     v_cross = cross_vectors(v, normal)
 
     if TOL.is_allclose(u_cross, [0.0, 0.0, 0.0]) or TOL.is_allclose(v_cross, [0.0, 0.0, 0.0]):
-        return None
+        raise ValueError("Cannot compute angle between vectors projected onto a plane defined by the normal vector. One of the vectors is parallel to the normal vector.")
 
     return angle_vectors_signed(u_cross, v_cross, normal, deg, tol)
 
