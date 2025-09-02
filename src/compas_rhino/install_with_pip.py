21cb8654137eda6738fcf9f28f79ba23fd8d4a64
tomvanmele <tom.v.mele@gmail.com>
2025-09-02 11:40:58 +0200

    update install script

--- src/compas_rhino/install_with_pip.py
+++ src/compas_rhino/install_with_pip.py
@@ -1,20 +1,75 @@
+import pathlib
+import random
+import string
 import subprocess
 
-import compas_rhino
+import click
+
+rhinocode = pathlib.Path().home() / ".rhinocode"
+rhinopython = rhinocode / "py39-rh8" / "python3.9"
+site_envs = rhinocode / "py39-rh8" / "site-envs"
 
 
-def install(args):
-    subprocess.check_call([compas_rhino._get_default_rhino_cpython_path("8.0"), "-m", "pip", "install"] + args)
+def random_string(length=8) -> str:
+    return "".join(random.choices(string.ascii_letters + string.digits, k=length))
 
+
+def find_full_env_name(name: str) -> str:
+    for filepath in site_envs.iterdir():
+        if filepath.stem.startswith(name):
+            return filepath.stem
+    raise ValueError(f"No environment with this name exists: {name}")
+
+
+def default_env_name() -> str:
+    return find_full_env_name("default")
+
+
+def ensure_site_env(name: str) -> str:
+    try:
+        fullname = find_full_env_name(name)
+    except ValueError:
+        fullname = f"{name}-{random_string()}"
+    return fullname
+
+
+@click.command()
+@click.argument("package")
+@click.option("--env", default="default", help="Name of the site env, without the random suffix...")
+@click.option("--upgrade/--no-upgrade", default=False)
+@click.option("--deps/--no-deps", default=True)
+def install_package(package, env, upgrade, deps):
+    if package == ".":
+        package = pathlib.Path().cwd()
+    elif package == "..":
+        package = pathlib.Path().cwd().parent
+
+    target = site_envs / ensure_site_env(env or "default")
+    target.mkdir(exist_ok=True)
+
+    args = [
+        str(rhinopython),
+        "-m",
+        "pip",
+        "install",
+        package,
+        "--target",
+        target,
+        "--no-warn-script-location",
+    ]
+
+    if upgrade:
+        args.append("--upgrade")
+
+    if not deps:
+        args.append("--no-deps")
+
+    return subprocess.check_call(args)
+
+
+# =============================================================================
+# Main
+# =============================================================================
 
 if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("pipargs", help="Arguments to be passed on to pip as a string")
-
-    args = parser.parse_args()
-    pipargs = args.pipargs.split()
-
-    install(pipargs)
+    install_package()
