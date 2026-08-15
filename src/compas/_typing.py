e1883c388e9eb20a9a6999c79641c3913a2e2a55
tomvanmele <tom.v.mele@gmail.com>
2026-08-15 08:37:10 +0200

    generalised typing

---
+++ src/compas/_typing.py
@@ -0,0 +1,26 @@
+from typing import Iterator
+from typing import Protocol
+from typing import Sequence
+from typing import Union
+
+
+class Coordinate(Protocol):
+    def __len__(self) -> int: ...
+
+    def __getitem__(self, key: int) -> float: ...
+
+    def __iter__(self) -> Iterator[float]: ...
+
+
+CoordinateType = Union[Coordinate, Sequence[float]]
+
+
+class Coordinates(Protocol):
+    def __len__(self) -> int: ...
+
+    def __getitem__(self, key: int) -> CoordinateType: ...
+
+    def __iter__(self) -> Iterator[CoordinateType]: ...
+
+
+CoordinatesType = Union[Coordinates, Sequence[CoordinateType]]
--- src/compas/geometry/_typing.py
+++ src/compas/geometry/_typing.py
@@ -1,8 +1,8 @@
 from typing import TYPE_CHECKING
-from typing import Iterator
-from typing import Protocol
 from typing import Sequence
 from typing import Union
+
+from compas._typing import CoordinateType
 
 if TYPE_CHECKING:
     from compas.geometry import Line
@@ -11,25 +11,6 @@ if TYPE_CHECKING:
     from compas.geometry import Polyline
     from compas.geometry import Transformation
 
-
-class Coordinate(Protocol):
-    def __getitem__(self, key: int) -> float: ...
-
-    def __iter__(self) -> Iterator[float]: ...
-
-
-CoordinateType = Union[Coordinate, Sequence[float]]
-
-
-class Coordinates(Protocol):
-    def __len__(self) -> int: ...
-
-    def __getitem__(self, key: int) -> CoordinateType: ...
-
-    def __iter__(self) -> Iterator[CoordinateType]: ...
-
-
-CoordinatesType = Union[Coordinates, Sequence[CoordinateType]]
 
 LineType = Union["Line", Sequence[CoordinateType]]
 PlaneType = Union["Plane", Sequence[CoordinateType]]
